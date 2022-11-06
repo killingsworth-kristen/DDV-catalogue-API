df5bede98a7fcab377ff568d2c3b9f179018e3da
Kristen <killingsworth_kristen@yahoo.com>
2022-11-05 22:42:09 -0400

    Sleeping beauty clothing data added

--- seeds/clothing/sleepingBeauty.js
+++ seeds/clothing/sleepingBeauty.js
@@ -4,13 +4,13 @@ const Tag = require(`../../models`)
 // 1 item
 
 const sleepingBeautyClothing = [
-    // {
-    //     clothingName: ``,
-    //     price: ,
-    //     obtainedBy: "Scrooge's Store",
-    //     tagId: [],
-    //     setId: 23
-    // },
+    {
+        clothingName: `Malevolent Fairy Horns`,
+        price: null,
+        obtainedBy: "Villain's Star Path",
+        tagId: [29],
+        setId: 23
+    },
 ]
 
 module.exports=sleepingBeautyClothing
