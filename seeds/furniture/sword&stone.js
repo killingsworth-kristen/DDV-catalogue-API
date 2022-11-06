ea707e7b249099cf02438181175e3da833edc6a2
Kristen <killingsworth_kristen@yahoo.com>
2022-11-05 20:37:45 -0400

    sword and stone furniture seed data added

--- seeds/furniture/sword&stone.js
+++ seeds/furniture/sword&stone.js
@@ -2,14 +2,118 @@ const Set = require(`../../models`)
 const Tag = require(`../../models`)
 
 const SwordStoneFurniture =[
-    // {
-    //     furnitureName: ``,
-    //     price: ,
-    //     obtainedBy: "Scrooge's Store",
-    //     craftingMaterials: null,
-    //     tagId: [],
-    //     setId: 2
-    // },
+    {
+        furnitureName: `Bag of Tricks`,
+        price: 3200,
+        obtainedBy: "Merlin Friendship Level 2",
+        craftingMaterials: null,
+        tagId: [8,11,19],
+        setId: 2
+    },
+    {
+        furnitureName: `Big Pile of Books`,
+        price: 6400,
+        obtainedBy: "Scrooge's Store",
+        craftingMaterials: null,
+        tagId: [8,11,19],
+        setId: 2
+    },
+    {
+        furnitureName: `Book Pile`,
+        price: 900,
+        obtainedBy: "Scrooge's Store",
+        craftingMaterials: null,
+        tagId: [7,11,13,19],
+        setId: 2
+    },
+    {
+        furnitureName: `Book Piles`,
+        price: 3200,
+        obtainedBy: "Scrooge's Store",
+        craftingMaterials: null,
+        tagId: [8,11,19],
+        setId: 2
+    },
+    {
+        furnitureName: `Line of Books`,
+        price: 480,
+        obtainedBy: "Scrooge's Store",
+        craftingMaterials: null,
+        tagId: [],
+        setId: 2
+    },
+    {
+        furnitureName: `Melted Candles`,
+        price: 360,
+        obtainedBy: "Scrooge's Store",
+        craftingMaterials: null,
+        tagId: [7,11,13,19],
+        setId: 2
+    },
+    {
+        furnitureName: `Mortar 'n' Pestle`,
+        price: 360,
+        obtainedBy: "Scrooge's Store",
+        craftingMaterials: null,
+        tagId: [7,8,9,19],
+        setId: 2
+    },
+    {
+        furnitureName: `Old Armchair`,
+        price: 900,
+        obtainedBy: "Scrooge's Store",
+        craftingMaterials: null,
+        tagId: [2,8,16,18,19],
+        setId: 2
+    },
+    {
+        furnitureName: `Open Book`,
+        price: 1200,
+        obtainedBy: "Scrooge's Store",
+        craftingMaterials: null,
+        tagId: [7,11,13,19],
+        setId: 2
+    },
+    {
+        furnitureName: `Orntate Curtain`,
+        price: 1200,
+        obtainedBy: "Scrooge's Store",
+        craftingMaterials: null,
+        tagId: [16,19],
+        setId: 2
+    },
+    {
+        furnitureName: `Phial Holder`,
+        price: 1200,
+        obtainedBy: "Merlin Friendship Level 5",
+        craftingMaterials: null,
+        tagId: [7,8,11,18,19],
+        setId: 2
+    },
+    {
+        furnitureName: `Potion Cupboard`,
+        price: 6400,
+        obtainedBy: "Merlin Friendship Level 8",
+        craftingMaterials: null,
+        tagId: [8,9,11,18,19],
+        setId: 2
+    },
+    {
+        furnitureName: `Round-Bottomed Flask and Holder`,
+        price: 900,
+        obtainedBy: "Scrooge's Store",
+        craftingMaterials: null,
+        tagId: [7,8,18,19],
+        setId: 2
+    },
+    {
+        furnitureName: `Small Book Pile`,
+        price: 360,
+        obtainedBy: "Scrooge's Store",
+        craftingMaterials: null,
+        tagId: [7,11,13,19],
+        setId: 2
+    }
 ]
 
 module.exports=SwordStoneFurniture
