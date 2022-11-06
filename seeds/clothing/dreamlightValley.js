3aa5a490898c1857b89505a1303c37daa505f1ec
Kristen <killingsworth_kristen@yahoo.com>
2022-11-05 22:45:36 -0400

    dreamlight valley clothing data added

--- seeds/clothing/dreamlightValley.js
+++ seeds/clothing/dreamlightValley.js
@@ -4,13 +4,20 @@ const Tag = require(`../../models`)
 // 2 items
 
 const dreamlightValleyClothing =[
-    // {
-    //     clothingName: ``,
-    //     price: ,
-    //     obtainedBy: "Scrooge's Store",
-    //     tagId: [],
-    //     setId: 3
-    // },  
+    {
+        clothingName: `Dreamlight Valley Jersey`,
+        price: null,
+        obtainedBy: "Avatar Designer Tool import",
+        tagId: [1,2,22],
+        setId: 4
+    }, 
+       {
+        clothingName: `Lenses of Shadows`,
+        price: null,
+        obtainedBy: "The Treasure Hunt Part 2 (Scrooge McDuck Level 10)",
+        tagId: [1,6,26],
+        setId: 4
+    }
 ]
 
 module.exports=dreamlightValleyClothing
