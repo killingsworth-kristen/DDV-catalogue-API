09fd6e68f6bd640de2983e9efc4f2204fc257291
Kristen <killingsworth_kristen@yahoo.com>
2022-11-07 02:48:24 -0500

    Moana and Little Mermaid clothing seed data added

--- seeds/clothing/littleMermaid.js
+++ seeds/clothing/littleMermaid.js
@@ -4,13 +4,160 @@ const Tag = require(`../../models`)
 // 22 items
 
 const littleMermaidClothing = [
-    // {
-    //     clothingName: ``,
-    //     price: ,
-    //     obtainedBy: "Scrooge's Store",
-    //     tagId: [],
-    //     setId: 11
-    // },
+    {
+        clothingName: `Ariel's Seafoam Gown`,
+        price: null,
+        obtainedBy: "The Ancient Doorway (Ariel Level 10)",
+        tagId: [4,24],
+        setId: 11
+    },
+    {
+        clothingName: `Areil's Seafoam Suit`,
+        price: null,
+        obtainedBy: "The Ancient Doorway (Ariel Level 10)",
+        tagId: [4,6,23],
+        setId: 11
+    },
+    {
+        clothingName: `Blue Button-Up Vest`,
+        price: 2400,
+        obtainedBy: "Scrooge's Store",
+        tagId: [2,4,36],
+        setId: 11
+    },
+    {
+        clothingName: `Blue Cummerbund Pants`,
+        price: 2600,
+        obtainedBy: "Scrooge's Store",
+        tagId: [4,11,32],
+        setId: 11
+    },
+    {
+        clothingName: `Buttoned Boots`,
+        price: 2000,
+        obtainedBy: "Scrooge's Store",
+        tagId: [33],
+        setId: 11
+    },
+    {
+        clothingName: `Cream Silk Gloves`,
+        price: 4000,
+        obtainedBy: "Scrooge's Store",
+        tagId: [4,11,27],
+        setId: 11
+    },
+    {
+        clothingName: `Dark Blue Button-Up Vest`,
+        price: null,
+        obtainedBy: "Ursula Friendship Level 2",
+        tagId: [2,4,36],
+        setId: 11
+    },
+    {
+        clothingName: `Gold Mariner's Watch`,
+        price: 12000,
+        obtainedBy: "Scrooge's Store",
+        tagId: [1,4,10,21],
+        setId: 11
+    },
+    {
+        clothingName: `Gray Button-Up Vest`,
+        price: 2400,
+        obtainedBy: "Scrooge's Store",
+        tagId: [2,4,36],
+        setId: 11
+    },
+    {
+        clothingName: `Green Off-the-Shoulder Shell Shirt`,
+        price: null,
+        obtainedBy: "Ariel Friendship Level 5",
+        tagId: [2,3,6,36],
+        setId: 11
+    },
+    {
+        clothingName: `Pink Off-the-Shoulder Shell Shirt`,
+        price: 12000,
+        obtainedBy: "Scrooge's Store",
+        tagId: [2,3,6,36],
+        setId: 11
+    },
+    {
+        clothingName: `Purple and Tan Seashell Satchel`,
+        price: 4400,
+        obtainedBy: "Scrooge's Store",
+        tagId: [3,4,8,20],
+        setId: 11
+    },
+    {
+        clothingName: `Purple Scaled Stilettos`,
+        price: 300,
+        obtainedBy: "Scrooge's Store",
+        tagId: [4,8,33],
+        setId: 11
+    },
+    {
+        clothingName: `Red Cummerbund Pants`,
+        price: 2600,
+        obtainedBy: "Scrooge's Store",
+        tagId: [4,11,32],
+        setId: 11
+    },
+    {
+        clothingName: `Sea Witch's Gown`,
+        price: null,
+        obtainedBy: "A Deal with Ursula",
+        tagId: [1,4,8,24],
+        setId: 11
+    },
+    {
+        clothingName: `Seaside Prince Side Part`,
+        price: null,
+        obtainedBy: "Prince Eric Friendship Level 2",
+        tagId: [28],
+        setId: 11
+    },
+    {
+        clothingName: `Sea-Swept Pearl Pinned Hairdo`,
+        price: 2000,
+        obtainedBy: "Scrooge's Store",
+        tagId: [28],
+        setId: 11
+    },
+    {
+        clothingName: `Shimmering Pink Scaled Pants`,
+        price: 13000,
+        obtainedBy: "Scrooge's Store",
+        tagId: [3,6,32],
+        setId: 11
+    },
+    {
+        clothingName: `Silver Mariner's Watch`,
+        price: 12000,
+        obtainedBy: "Scrooge's Store",
+        tagId: [1,4,10,21],
+        setId: 11
+    },
+    {
+        clothingName: `White and Blue Officer Jacket`,
+        price: null,
+        obtainedBy: "Prince Eric Friendship Level 8",
+        tagId: [4,7,11,22],
+        setId: 11
+    },
+    {
+        clothingName: `White and Red Officer Jacket`,
+        price: 20000,
+        obtainedBy: "Scrooge's Store",
+        tagId: [4,7,11,22],
+        setId: 11
+    },
+    {
+        clothingName: `White Silk Gloves`,
+        price: 4000,
+        obtainedBy: "Scrooge's Store",
+        tagId: [4,11,27],
+        setId: 11
+    },
 ]
 
 module.exports=littleMermaidClothing
--- seeds/clothing/moana.js
+++ seeds/clothing/moana.js
@@ -4,13 +4,153 @@ const Tag = require(`../../models`)
 // 21 items
 
 const moanaClothing = [
-    // {
-    //     clothingName: ``,
-    //     price: ,
-    //     obtainedBy: "Scrooge's Store",
-    //     tagId: [],
-    //     setId: 4
-    // },
+    {
+        clothingName: `Blue Beach Shorts`,
+        price: 3400,
+        obtainedBy: "Scrooge's Store",
+        tagId: [2,34],
+        setId: 5
+    },
+    {
+        clothingName: `Blue Gradient Shades`,
+        price: 2800,
+        obtainedBy: "Scrooge's Store",
+        tagId: [2,6,26],
+        setId: 5
+    },  
+    {
+        clothingName: `Blue Tassel Earrings`,
+        price: 3800,
+        obtainedBy: "Scrooge's Store",
+        tagId: [2,4,25],
+        setId: 5
+    },  
+    {
+        clothingName: `Blue Woven Top`,
+        price: 2400,
+        obtainedBy: "Scrooge's Store",
+        tagId: [2,9,36],
+        setId: 5
+    },  
+    {
+        clothingName: `Green Beach Shorts`,
+        price: 3400,
+        obtainedBy: "Scrooge's Store",
+        tagId: [2,34],
+        setId: 5
+    },  
+    {
+        clothingName: `Green Gradient Shades`,
+        price: 2800,
+        obtainedBy: "Scrooge's Store",
+        tagId: [2,6,26],
+        setId: 5
+    },    
+    {
+        clothingName: `Green Tassel Earrings`,
+        price: 3800,
+        obtainedBy: "Scrooge's Store",
+        tagId: [2,4,25],
+        setId: 5
+    },   
+    {
+        clothingName: `Green Woven Shorts`,
+        price: 6800,
+        obtainedBy: "Scrooge's Store",
+        tagId: [2,3,9,34],
+        setId: 5
+    },
+    {
+        clothingName: `Green Woven Top`,
+        price: 2400,
+        obtainedBy: "Scrooge's Store",
+        tagId: [2,9,36],
+        setId: 5
+    },  
+    {
+        clothingName: `Hooked Curly Topknot`,
+        price: 1000,
+        obtainedBy: "Scrooge's Store",
+        tagId: [1,8,9,28],
+        setId: 5
+    },
+    {
+        clothingName: `Lava Pleather Jacket`,
+        price: 4000,
+        obtainedBy: "Scrooge's Store",
+        tagId: [1,2,6,22],
+        setId: 5
+    },
+    {
+        clothingName: `Lush Blue Earrings`,
+        price: 19000,
+        obtainedBy: "Scrooge's Store",
+        tagId: [1,3,25],
+        setId: 5
+    },  
+    {
+        clothingName: `Lush Green Earrings`,
+        price: 19000,
+        obtainedBy: "Scrooge's Store",
+        tagId: [1,3,25],
+        setId: 5
+    },  
+    {
+        clothingName: `Orange "Heart of Te Fiti" Shirt`,
+        price: 900,
+        obtainedBy: "Scrooge's Store",
+        tagId: [2,3,36],
+        setId: 5
+    },  
+    {
+        clothingName: `Orange Gradient Shades`,
+        price: 2800,
+        obtainedBy: "Scrooge's Store",
+        tagId: [2,6,26],
+        setId: 5
+    }, 
+    {
+        clothingName: `Pink Woven Top`,
+        price: null,
+        obtainedBy: "Moana Friendship Level 2",
+        tagId: [2,9,36], //simple,playful,delicate,wondrous
+        setId: 5
+    },  
+    {
+        clothingName: `Purple Woven Shorts`,
+        price: 6800,
+        obtainedBy: "Scrooge's Store",
+        tagId: [2,3,9,34],
+        setId: 5
+    }, 
+    {
+        clothingName: `Straw Sunhat with Yellow and Red Band`,
+        price: null,
+        obtainedBy: "Remembering Old Times (Minnie Mouse Level 7",
+        tagId: [29],
+        setId: 5
+    },  
+    {
+        clothingName: `Tan "Heart of Te Fiti" Shirt`,
+        price: 900,
+        obtainedBy: "Scrooge's Store",
+        tagId: [2,3,36],
+        setId: 5
+    },  
+    {
+        clothingName: `Tan Beach Shorts`,
+        price: null,
+        obtainedBy: "Maui Freindship Level 2",
+        tagId: [2,6,34], //simple, playful, delicate, wondrous
+        setId: 5
+    },
+    {
+        clothingName: `Turquiose Woven Shorts`,
+        price: 6800,
+        obtainedBy: "Scrooge's Store",
+        tagId: [2,3,9,34],
+        setId: 5
+    }, 
 ]
 
 module.exports=moanaClothing
