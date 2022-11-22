0eaeff7d7c90e27935d477f7bb4ed20bfd5b84ee
killingsworth.kristen <killingsworth_kristen@yahoo.com>
2022-11-22 15:49:15 -0800

    hercuules seed format changed

--- seeds/clothing/hercules.js
+++ seeds/clothing/hercules.js
@@ -1,13 +1,14 @@
-const Set = require(`../../models`)
-const Tag = require(`../../models`)
+// none as of 11/22/22
 
 const herculesClothing = [
     // {
     //     clothingName: ``,
     //     price: ,
     //     obtainedBy: "Scrooge's Store",
-    //     tagId: [],
-    //     setId: 18
+    //     style: [],
+    //     clothingCategory: "",
+    //     color: [],
+    //     setId: "Hercules"
     // },
 ]
 
