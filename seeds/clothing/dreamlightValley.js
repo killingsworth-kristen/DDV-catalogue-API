3df02e77985103cdaf9219ed6c701c2c663244ee
killingsworth.kristen <killingsworth_kristen@yahoo.com>
2022-11-22 15:28:48 -0800

    dreamlight valley seeds updated

--- seeds/clothing/dreamlightValley.js
+++ seeds/clothing/dreamlightValley.js
@@ -1,22 +1,22 @@
-const Set = require(`../../models`)
-const Tag = require(`../../models`)
 
 // 2 items
 
 const dreamlightValleyClothing =[
     {
         clothingName: `Dreamlight Valley Jersey`,
-        price: null,
         obtainedBy: "Avatar Designer Tool import",
-        tagId: [1,2,22],
-        setId: 4
+        style: ["Bold","Casual"],
+        clothingCategory: "Coats",
+        color: [],
+        setId: "Dreamlight Valley"
     }, 
     {
         clothingName: `Lenses of Shadows`,
-        price: null,
         obtainedBy: "The Treasure Hunt Part 2 (Scrooge McDuck Level 10)",
-        tagId: [1,6,26],
-        setId: 4
+        style: ["Bold","Modern"],
+        clothingCategory: "Glasses",
+        color: [],
+        setId: "Dreamlight Valley"
     }
 ]
 
