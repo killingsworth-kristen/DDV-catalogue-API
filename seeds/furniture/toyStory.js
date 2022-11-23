af9117c821495bf0c0c29b513fef37c2b8301e66
killingsworth.kristen <killingsworth_kristen@yahoo.com>
2022-11-23 11:49:31 -0800

    Toy Story furniture seed updated

--- seeds/furniture/toyStory.js
+++ seeds/furniture/toyStory.js
@@ -1,5 +1,3 @@
-const Set = require(`../../models`)
-const Tag = require(`../../models`)
 
 // 26 pieces of furniture
 
@@ -16,175 +14,208 @@ const toyStoryFurniture = [
     {
         furnitureName: `Alien Guessing Booth`,
         price: 6400,
-        obtainedBy: "Scrooge's Store",
-        tagId: [5,8,18],
+        style: ["Futuristic","Quirky"],
+        furnitureCategory: ["Rec Room"],
+        color: [],
         universe: "Toy Story"
     },
     {
         furnitureName: `Chest Coffee Table`,
         price: 3520,
-        obtainedBy: "Scrooge's Store",
-        tagId: [8,9,16,18],
+        style: ["Quirky","Rustic"],
+        furnitureCategory: ["Living Room","Rec Room"],
+        color: [],
         universe: "Toy Story"
     },
     {
         furnitureName: `Cloudy Island`,
         price: 3520,
-        obtainedBy: "Scrooge's Store",
-        tagId: [3,18,19],
+        style: ["Cute"],
+        furnitureCategory: ["Rec Room","Study"],
+        color: [],
         universe: "Toy Story"
     },
     {
         furnitureName: `Cloudy Lamp`,
         price: 2720,
-        obtainedBy: "Scrooge's Store",
-        tagId: [2,3,13,18,19],
+        style: ["Casual","Cute"],
+        furnitureCategory: ["Bedroom","Rec Room","Study"],
+        color: [],
         universe: "Toy Story"
     },
     {
         furnitureName: `Coffee Cart`,
         price: 7040,
-        obtainedBy: "Scrooge's Store",
-        tagId: [2,3,16,19],
+        style: ["Casual","Cute"],
+        furnitureCategory: ["Living Room","Study"],
+        color: [],
         universe: "Toy Story"
     },
     {
         furnitureName: `Colorful L Couch`,
         price: 6000,
         obtainedBy: "Founder's Pack: Deluxe Edition",
-        tagId: [],
+        style: [],
+        furnitureCategory: ["Seating"],
+        color: [],
         universe: "Toy Story"
     },
     {
         furnitureName: `Entertainment System`,
         price: 7200,
         obtainedBy: "Founder's Pack: Deluxe Edition",
-        tagId: [],
+        style: [],
+        furnitureCategory: ["Decor"],
+        color: [],
         universe: "Toy Story"
     },
     {
         furnitureName: `Geometric Side Table`,
         price: 880,
-        obtainedBy: "Scrooge's Store",
-        tagId: [6,8,10,13,16],
+        style: ["Modern","Quirky","Sporty"],
+        furnitureCategory: ["Bedroom","Living Room"],
+        color: [],
         universe: "Toy Story"
     },
     {
         furnitureName: `Graphic Sofa`,
         price: 3000,
         obtainedBy: "Founder's Pack: Deluxe Edition",
-        tagId: [],
+        style: [],
+        furnitureCategory: ["Seating"],
+        color: [],
         universe: "Toy Story"
-    },{
+    },
+    {
         furnitureName: `Half-Arch Window Left`,
         price: 8000,
         obtainedBy: "Founder's Pack: Deluxe Edition",
-        tagId: [],
+        style: [],
+        furnitureCategory: ["Decor"],
+        color: [],
         universe: "Toy Story"
     },
     {
         furnitureName: `Half-Arch Window Right`,
         price: 8000,
         obtainedBy: "Founder's Pack: Deluxe Edition",
-        tagId: [],
+        style: [],
+        furnitureCategory: ["Decor"],
+        color: [],
         universe: "Toy Story"
     },
     {
         furnitureName: `Half-Cup Chair`,
         price: 4800,
-        obtainedBy: "Scrooge's Store",
-        tagId: [3,8,16,18],
+        style: ["Cute","Quirky"],
+        furnitureCategory: ["Living Room","Rec Room"],
+        color: [],
         universe: "Toy Story"
     },
     {
         furnitureName: `Hydrangea Bundle`,
         price: 5760,
-        obtainedBy: "Scrooge's Store",
-        tagId: [2,3,13,14,16],
+        style: ["Casual","Cute"],
+        furnitureCategory: ["Bedroom","Dining Room","Living Room"],
+        color: [],
         universe: "Toy Story"
     },
     {
         furnitureName: `Lively Coffee Table`,
         price: 3520,
         obtainedBy: "Founder's Pack: Deluxe Edition",
-        tagId: [],
+        style: [],
+        furnitureCategory: ["Tables"],
+        color: [],
         universe: "Toy Story"
     },
     {
         furnitureName: `Photo Booth`,
         price: 6400,
-        obtainedBy: "Scrooge's Store",
-        tagId: [6,8,17,18],
+        style: ["Modern","Quirky"],
+        furnitureCategory: ["Outdoors","Rec Room"],
+        color: [],
         universe: "Toy Story"
     },
     {
         furnitureName: `Popcorn Machine`,
         price: 7360,
-        obtainedBy: "Scrooge's Store",
-        tagId: [8,11,15,17,18],
+        style: ["Quirky","Traditional"],
+        furnitureCategory: ["Kitchen","Outdoors","Rec Room"],
+        color: [],
         universe: "Toy Story"
     },
     {
         furnitureName: `Red Drapery`,
         price: 1200,
-        obtainedBy: "Scrooge's Store",
-        tagId: [8,11,13,18],
+        style: ["Quirky","Traitional"],
+        furnitureCategory: ["Bedroom","Rec Room"],
+        color: [],
         universe: "Toy Story"
     },
     {
         furnitureName: `Rocket Painting`,
         price: 2720,
-        obtainedBy: "Scrooge's Store",
-        tagId: [3,5,13,16,18,19],
+        style: ["Cute","Casual"],
+        furnitureCategory: ["Bedroom","Rec Room","Study"],
+        color: [],
         universe: "Toy Story"
     },
     {
         furnitureName: `Stage Curtain Left`,
         price: 1200,
-        obtainedBy: "Scrooge's Store",
-        tagId: [8,11,13,18],
+        style: ["Quirky","Traditional"],
+        furnitureCategory: ["Bedroom","Rec Room"],
+        color: [],
         universe: "Toy Story"
     },
     {
         furnitureName: `Stage Curtain Right`,
         price: 1200,
-        obtainedBy: "Scrooge's Store",
-        tagId: [8,11,13,18],
+        style: ["Quirky","Traditional"],
+        furnitureCategory: ["Bedroom","Rec Room"],
+        color: [],
         universe: "Toy Story"
     },
     {
         furnitureName: `Starry Chair`,
         price: 2400,
-        obtainedBy: "Scrooge's Store",
-        tagId: [2,3,18,19],
+        style: ["Casual","Cute"],
+        furnitureCategory: ["Rec Room","Study"],
+        color: [],
         universe: "Toy Story"
     },
     {
         furnitureName: `Starry Stool`,
         price: 1800,
-        obtainedBy: "Scrooge's Store",
-        tagId: [2,3,15,16,19],
+        style: ["Casual","Cute"],
+        furnitureCategory: ["Kitchen","Living Room","Study"],
+        color: [],
         universe: "Toy Story"
     },
     {
         furnitureName: `Star-Shaped Neon Decoration`,
         price: 2720,
-        obtainedBy: "Scrooge's Store",
-        tagId: [3,8,13,16,18],
+        style: ["Cute","Quirky"],
+        furnitureCategory: ["Bedroom","Living Room","Rec Room"],
+        color: [],
         universe: "Toy Story"
     },
     {
         furnitureName: `Step-Up Stool`,
         price: 2400,
-        obtainedBy: "Scrooge's Store",
-        tagId: [3,8,17,18],
+        style: ["Cute","Quirky"],
+        furnitureCategory: ["Outdoors","Rec Room"],
+        color: [],
         universe: "Toy Story"
     },
     {
         furnitureName: `Vibrant Wall Planter`,
         price: 2800,
         obtainedBy: "Founder's Pack: Deluxe Edition",
-        tagId: [],
+        style: [],
+        furnitureCategory: ["Decor"],
+        color: [],
         universe: "Toy Story"
     },
 ]
