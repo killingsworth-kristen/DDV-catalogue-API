4a15755ddde1d6699ff00853ed44e4bc1f78770d
killingsworth.kristen <killingsworth_kristen@yahoo.com>
2022-11-23 12:43:59 -0800

    Ratatouille furniture added

--- seeds/furniture/ratatouille.js
+++ seeds/furniture/ratatouille.js
@@ -2,15 +2,260 @@
 // 31 pieces of furniture
 
 const ratatouilleFurniture = [
-    // {
-    //     furnitureName: ``,
-    //     price: ,
-    //     obtainedBy: "Scrooge's Store",
-    //     style: [],
-    //     furnitureCategory: [""],
-    //     color: [],
-    //     universe: "Ratatouille"
-    // },
+    {
+        furnitureName: `Basic Tableware`,
+        price: 900,
+        style: ["Casual","Quirky"],
+        furnitureCategory: ["Dining Room","Kitchen","Decor"],
+        color: [],
+        universe: "Ratatouille"
+    },
+    {
+        furnitureName: `Cheese Poster`,
+        price: 5440,
+        style: ["Rustic","Traditional"],
+        furnitureCategory: ["Bathroom","Dining Room","Kitchen","Decor"],
+        color: [],
+        universe: "Ratatouille"
+    },
+    {
+        furnitureName: `Counter Trolly`,
+        price: 1320,
+        style: ["Professional","Traditional"],
+        furnitureCategory: ["Kitchen","Tables"],
+        color: [],
+        universe: "Ratatouille"
+    },
+    {
+        furnitureName: `Cushoined Dining Chair`,
+        price: 900,
+        style: ["Elegant","Traditional"],
+        furnitureCategory: ["Dining Room","Study","Seating"],
+        color: [],
+        universe: "Ratatouille"
+    },
+    {
+        furnitureName: `Fridge Covered in Notes`,
+        price: 3680,
+        style: ["Casual","Quirky"],
+        furnitureCategory: ["Kitchen","Decor"],
+        color: [],
+        universe: "Ratatouille"
+    },
+    {
+        furnitureName: `Fruits Poster`,
+        price: 5440,
+        obtainedBy: "Remy Friendship Level 5",
+        style: ["Rustic","Traditional"],
+        furnitureCategory: ["Bathroom","Dining Room","Kitchen","Decor"],
+        color: [],
+        universe: "Ratatouille"
+    },
+    {
+        furnitureName: `Herbs de Provence`,
+        price: 3200,
+        style: ["Rustic","Traditional"],
+        furnitureCategory: ["Bathroom","Kitchen","Decor"],
+        color: [],
+        universe: "Ratatouille"
+    },
+    {
+        furnitureName: `Hot Plate`,
+        price: 3680,
+        style: ["Casual","Quirky"],
+        furnitureCategory: ["Kitchen","Cooking Station"],
+        color: [],
+        universe: "Ratatouille"
+    },
+    {
+        furnitureName: `Kitchen Shelf`,
+        price: 3200,
+        style: ["Casual","Quirky"],
+        furnitureCategory: ["Kitchen","Decor"],
+        color: [],
+        universe: "Ratatouille"
+    },
+    {
+        furnitureName: `Large Pot`,
+        price: 360,
+        style: ["Rustic","Traditional"],
+        furnitureCategory: ["Kitchen","Decor"],
+        color: [],
+        universe: "Ratatouille"
+    },
+    {
+        furnitureName: `Metal Kitchen Counter`,
+        price: 1320,
+        style: [],
+        furnitureCategory: ["Tables"],
+        color: [],
+        universe: "Ratatouille"
+    },
+    {
+        furnitureName: `Minimalistic Ceiling Lights`,
+        price: 2720,
+        style: ["Elegant","Modern"],
+        furnitureCategory: ["Dining Room","Kitchen","Rec Room","Lighting"],
+        color: [],
+        universe: "Ratatouille"
+    },
+    {
+        furnitureName: `Old TV`,
+        price: 2880,
+        style: ["Casual","Quirky"],
+        furnitureCategory: ["Bedroom","Living Room","Rec Room"],
+        color: [],
+        universe: "Ratatouille"
+    },
+    {
+        furnitureName: `Oven and Hood`,
+        price: 18400,
+        obtainedBy: "Remy Friendship Level 2",
+        style: ["Rustic","Traditional"],
+        furnitureCategory: ["Kitchen","Cooking Station"],
+        color: [],
+        universe: "Ratatouille"
+    },
+    {
+        furnitureName: `Pan Hanger`,
+        price: 3200,
+        style: ["Professional","Rustic","Traditional"],
+        furnitureCategory: ["Kitchen","Decor"],
+        color: [],
+        universe: "Ratatouille"
+    },
+    {
+        furnitureName: `Radiator`,
+        price: 1200,
+        style: ["Casual","Quirky"],
+        furnitureCategory: ["Bathroom","Bedroom","Decor"],
+        color: [],
+        universe: "Ratatouille"
+    },
+    {
+        furnitureName: `Remy's "Best Chef" Shelf`,
+        price: 16000,
+        obtainedBy: "The Unknown Flavor (Remy Level 10)",
+        style: ["Professional","Rustic"],
+        furnitureCategory: ["Kitchen","Decor"],
+        color: [],
+        universe: "Ratatouille"
+    },
+    {
+        // not tracked in collection
+        furnitureName: `Restaurant Furniture Kit`,
+        price: 2000,
+        obtainedBy: "A Restaurant Makeover",
+        style: [],
+        furnitureCategory: ["Decor"],
+        color: [],
+        universe: "Ratatouille"
+    },
+    {
+        furnitureName: `Retro Fan`,
+        price: 900,
+        style: ["Casual","Quirky"],
+        furnitureCategory: ["Kitchen","Rec Room","Decor"],
+        color: [],
+        universe: "Ratatouille"
+    },
+    {
+        furnitureName: `Rotary Phone`,
+        price: 2160,
+        style: ["Casual","Quirky"],
+        furnitureCategory: ["Kitchen","Rec Room","Decor"],
+        color: [],
+        universe: "Ratatouille"
+    },
+    {
+        furnitureName: `Round Dining Table`,
+        price: 500,
+        style: ["Elegant","Traditional"],
+        furnitureCategory: ["Dining Room","Kitchen","Outdoors","Tables"],
+        color: [],
+        universe: "Ratatouille"
+    },
+    {
+        furnitureName: `Simple Lamp`,
+        price: 765,
+        style: ["Elegant","Traditional"],
+        furnitureCategory: ["Dining Room","Study","Lighting"],
+        color: [],
+        universe: "Ratatouille"
+    },
+    {
+        furnitureName: `Simple Wall Lamp`,
+        price: 2720,
+        style: ["Elegant","Traditional"],
+        furnitureCategory: ["Bathroom","Dining Room","Study","Lighting"],
+        color: [],
+        universe: "Ratatouille"
+    },
+    {
+        furnitureName: `Sink and Mirror`,
+        price: 1200,
+        style: ["Casual","Quirky"],
+        furnitureCategory: ["Bathroom","Kitchen","Decor"],
+        color: [],
+        universe: "Ratatouille"
+    },
+    {
+        furnitureName: `Sink and Tiled Wall`,
+        price: 3200,
+        obtainedBy: "Foodception",
+        style: ["Rustic","Traditional"],
+        furnitureCategory: ["Bathroom","Kitchen","Decor"],
+        color: [],
+        universe: "Ratatouille"
+    },
+    {
+        furnitureName: `Small Round Dining Table`,
+        price: 500,
+        style: ["Elegant","Traditional"],
+        furnitureCategory: ["Dining Room","Kitchen","Outdoors","Tables"],
+        color: [],
+        universe: "Ratatouille"
+    },
+    {
+        furnitureName: `Tableware`,
+        price: 2400,
+        style: ["Elegant","Traditional"],
+        furnitureCategory: ["Dining Room","Kitchen","Decor"],
+        color: [],
+        universe: "Ratatouille"
+    },
+    {
+        furnitureName: `Tiled-Top Corner Counter`,
+        price: 3520,
+        style: ["Cute","Rustic","Traditional"],
+        furnitureCategory: ["Bathroom","Kitchen","Tables"],
+        color: [],
+        universe: "Ratatouille"
+    },
+    {
+        furnitureName: `Tiled-Top Counter`,
+        price: 3520,
+        style: ["Cute","Rustic","Traditional"],
+        furnitureCategory: ["Bathroom","Kitchen","Tables"],
+        color: [],
+        universe: "Ratatouille"
+    },
+    {
+        furnitureName: `Tiled-Top Cubby Counter`,
+        price: 3520,
+        style: ["Cute","Rustic","Traditional"],
+        furnitureCategory: ["Bathroom","Kitchen","Tables"],
+        color: [],
+        universe: "Ratatouille"
+    },
+    {
+        furnitureName: `Window to Paris`,
+        price: 3200,
+        style: [],
+        furnitureCategory: ["Decor"],
+        color: [],
+        universe: "Ratatouille"
+    },
 ]
 
 module.exports=ratatouilleFurniture
