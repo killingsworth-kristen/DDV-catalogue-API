0ae4cfb31ac34e070880f6f6ef248c23097095f3
killingsworth.kristen <killingsworth_kristen@yahoo.com>
2022-11-23 11:35:50 -0800

    Tangled furniture seed updated

--- seeds/furniture/tangled.js
+++ seeds/furniture/tangled.js
@@ -1,158 +1,158 @@
-const Set = require(`../../models`)
-const Tag = require(`../../models`)
 
 const tangledFurniture = [
     {
         furnitureName: `Barrel Table`,
         price: 3520,
-        obtainedBy: "Scrooge's Store",
-        
-        tagId: [1,9,13,16,19],
+        style: ["Bold","Rustic"],
+        furnitureCategory: ["Bedroom","Living Room","Study"],
+        color: [],
         universe:"Tangled"
     },
     {
         furnitureName: `Bear Armchair`,
         price: 2400,
-        obtainedBy: "Scrooge's Store",
-        
-        tagId: [1,14,16,18,19],
+        style: ["Bold"],
+        furnitureCategory: ["Dining Room","Living Room","Rec Room","Study"],
+        color: [],
         universe:"Tangled"
     },
     {
         furnitureName: `Cozy Couch`,
         price: 3000,
-        obtainedBy: "Scrooge's Store",
-        
-        tagId: [2,3,8,13,16,18],
+        style: ["Casual","Cute","Quirky"],
+        furnitureCategory: ["Bedroom","Living Room","Rec Room"],
+        color: [],
         universe:"Tangled"
     },
     {
         furnitureName: `Day & Night Paintings`,
         price: 1275,
-        obtainedBy: "Scrooge's Store",
-        
-        tagId: [2,3,8,18,19],
+        style: ["Casual","Cute","Quirky"],
+        furnitureCategory: ["Rec Room","Study"],
+        color: [],
         universe:"Tangled"
     },
     {
         furnitureName: `Easel`,
         price: 2720,
-        obtainedBy: "Scrooge's Store",
-        
-        tagId: [2,3,8,18,19],
+        style: ["Casual","Cute","Quirky"],
+        furnitureCategory: ["Rec Room","Study"],
+        color: [],
         universe:"Tangled"
     },
     {
         furnitureName: `Guitar`,
         price: 2400,
-        obtainedBy: "Scrooge's Store",
-        
-        tagId: [2,3,8,18,19],
+        style: ["Casual","Cute","Quirky"],
+        furnitureCategory: ["Rec Room","Study"],
+        color: [],
         universe:"Tangled"
     },
     {
         furnitureName: `Helmet Lamp`,
         price: 2720,
-        obtainedBy: "Scrooge's Store",
-        
-        tagId: [1,9,16,18,19],
+        style: ["Bold","Rustic"],
+        furnitureCategory: ["Living Room","Rec Room","Study"],
+        color: [],
         universe:"Tangled"
     },
     {
         furnitureName: `Homey Rug`,
         price: 4800,
-        obtainedBy: "Scrooge's Store",
-        
-        tagId: [1,9,13,16,19],
+        style: ["Bold","Rustic"],
+        furnitureCategory: ["Bedroom","Living Room","Study"],
+        color: [],
         universe:"Tangled"
     },
     {
         furnitureName: `Horned Tavern Fireplace`,
         price: 600,
-        obtainedBy: "Scrooge's Store",
-        
-        tagId: [1,9,14,16,18],
+        style: ["Bold","Rustic"],
+        furnitureCategory: ["Dining Room","Living Room","Rec Room"],
+        color: [],
         universe:"Tangled"
     },
     {
         furnitureName: `Lantern Garland`,
         price: 2720,
-        obtainedBy: "Scrooge's Store",
-        
-        tagId: [2,3,8,13,18],
+        style: ["Casual","Cute","Quirky"],
+        furnitureCategory: ["Bedroom","Rec Room"],
+        color: [],
         universe:"Tangled"
     },
     {
         furnitureName: `Lounge Pillows`,
         price: 8000,
-        obtainedBy: "Scrooge's Store",
-        
-        tagId: [2,3,8,13,16,18],
+        style: ["Casual","Cute","Quirky"],
+        furnitureCategory: ["Bedroom","Living Room","Rec Room"],
+        color: [],
         universe:"Tangled"
     },
     {
         furnitureName: `Mother Gothel's Standing Mirror`,
         price: 16000,
         obtainedBy: "Village Project: Restoring the Sunstone (Mother Gothel Level 10)",
-        
-        tagId: [4,11,12,13],
+        style: ["Elegant","Traditional"],
+        furnitureCategory: ["Bathroom","Bedroom"],
+        color: [],
         universe:"Tangled"
     },
-    // needs pricing data
+    
     {
         furnitureName: `Painting Table`,
-        
+        // needs pricing data
         obtainedBy: "Scrooge's Store",
-        
-        tagId: [2,3,8,18,19],
+        style: ["Casual","Cutre","Quirky"],
+        furnitureCategory: ["Rec Room","Study"],
+        color: [],
         universe:"Tangled"
     },
     {
         furnitureName: `Plain Bed`,
         price: 4600,
-        obtainedBy: "Scrooge's Store",
-        
-        tagId: [1,9,13],
+        style: ["Bold","Rustic"],
+        furnitureCategory: ["Bedroom"],
+        color: [],
         universe:"Tangled"
     },
     {
         furnitureName: `Provincial Window`,
         price: 3200,
-        obtainedBy: "Scrooge's Store",
-        
-        tagId: [1,9,13,15,16],
+        style: ["Bold","Rustic"],
+        furnitureCategory: ["Bedroom","Kitchen","Living Room"],
+        color: [],
         universe:"Tangled"
     },
     {
         furnitureName: `Pub Sign`,
         price: 2720,
-        obtainedBy: "Scrooge's Store",
-        
-        tagId: [3,8,9,12,15,18],
+        style: ["Cute","Quirky","Rustic"],
+        furnitureCategory: ["Bathroom","Kitchen","Rec Room"],
+        color: [],
         universe:"Tangled"
     },
     {
         furnitureName: `Rustic Chandelier`,
         price: 2720,
-        obtainedBy: "Scrooge's Store",
-        
-        tagId: [1,9,14,15,16],
+        style: ["Bold","Rustic"],
+        furnitureCategory: ["Dining Room","Kitchen","Living Room"],
+        color: [],
         universe:"Tangled"
     },
     {
         furnitureName: `Simple Stool`,
         price: 1800,
-        obtainedBy: "Scrooge's Store",
-        
-        tagId: [1,9,14,16,19],
+        style: ["Bold","Rustic"],
+        furnitureCategory: ["Dining Room","Living Room","Study"],
+        color: [],
         universe:"Tangled"
     },
     {
         furnitureName: `Spray-Painted Window`,
         price: 4800,
-        obtainedBy: "Scrooge's Store",
-        
-        tagId: [2,3,8,18,19],
+        style: ["Casual","Cute","Quirky"],
+        furnitureCategory: ["Rec Room","Study"],
+        color: [],
         universe:"Tangled"
     }
 ]
