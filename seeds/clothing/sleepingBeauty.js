cea1db4bf217b6a99dd2055f427e8794d06c0b81
killingsworth.kristen <killingsworth_kristen@yahoo.com>
2022-11-22 19:07:19 -0800

    Sleeping beauty clothign seed updated

--- seeds/clothing/sleepingBeauty.js
+++ seeds/clothing/sleepingBeauty.js
@@ -1,15 +1,14 @@
-const Set = require(`../../models`)
-const Tag = require(`../../models`)
 
 // 1 item
 
 const sleepingBeautyClothing = [
     {
         clothingName: `Malevolent Fairy Horns`,
-        price: null,
         obtainedBy: "Villain's Star Path",
-        tagId: [29],
-        setId: 23
+        style: [],
+        clothingCategory: "Hats",
+        color: [],
+        set: "Sleeping Beauty"
     },
 ]
 
