4aac3243f253abcc76b3b7fad391bf0a75521fab
killingsworth.kristen <killingsworth_kristen@yahoo.com>
2022-11-23 12:17:12 -0800

    Moana furniture seed added

--- seeds/furniture/moana.js
+++ seeds/furniture/moana.js
@@ -1,16 +1,262 @@
 
-// 30 pieces of furniture
+// 31 pieces of furniture
 
 const moanaFurniture = [
-    // {
-    //     furnitureName: ``,
-    //     price: ,
-    //     obtainedBy: "Scrooge's Store",
-    //     style: [],
-    //     furnitureCategory: [""],
-    //     color: [],
-    //     universe:"Moana"
-    // },
+    {
+        furnitureName: `Beach Banquet`,
+        price: 20000,
+        style: ["Casual","Elegant","Rustic"],
+        furnitureCategory: ["Outdoors","Decor"],
+        color: [],
+        universe:"Moana"
+    },
+    {
+        furnitureName: `Beadlet Anemones`,
+        price: 2160,
+        style: ["Bold","Quirky"],
+        furnitureCategory: ["Outdoors","Decor"],
+        color: [],
+        universe:"Moana"
+    },
+    {
+        furnitureName: `Bioluminescent Palm`,
+        price: 7200,
+        style: ["Bold","Quirky"],
+        furnitureCategory: ["Outdoors","Decor"],
+        color: [],
+        universe:"Moana"
+    },
+    {
+        furnitureName: `Canopied Couch`,
+        price: 4800,
+        obtainedBy: "Furniture Pouch",
+        style: ["Casual","Cute"],
+        furnitureCategory: ["Outdoors","Seating"],
+        color: [],
+        universe:"Moana"
+    },
+    {
+        furnitureName: `Circular Rocky Pool`,
+        price: 24000,
+        style: ["Casual","Elegant","Rustic"],
+        furnitureCategory: ["Outdoors","Decor"],
+        color: [],
+        universe:"Moana"
+    },
+    {
+        furnitureName: `Coconut Beverage`,
+        price: 900,
+        style: ["Casual","Cute"],
+        furnitureCategory: ["Kitchen","Outdoors","Decor"],
+        color: [],
+        universe:"Moana"
+    },
+    {
+        furnitureName: `Coconut Setting`,
+        price: 900,
+        style: ["Rustic","Traditional"],
+        furnitureCategory: ["Dining Room","Kitchen","Outdoors","Decor"],
+        color: [],
+        universe:"Moana"
+    },
+    {
+        furnitureName: `Conch Shell`,
+        price: 900,
+        obtainedBy: "Moana Friendship Level 8",
+        style: ["Elegant","Rustic","Traditional"],
+        furnitureCategory: ["Bathroom","Outdoors","Study","Decor"],
+        color: [],
+        universe:"Moana"
+    },
+    {
+        furnitureName: `Cooking Fire Pit`,
+        price: 6400,
+        obtainedBy: "Moana Friendship Level 5",
+        style: ["Rustic","Traditional"],
+        furnitureCategory: ["Outdoors","Decor"],
+        color: [],
+        universe:"Moana"
+    },
+    {
+        furnitureName: `Coral, Shells, and Rock Pillar`,
+        price: 6400,
+        style: ["Bold","Quirky"],
+        furnitureCategory: ["Outdoors","Decor"],
+        color: [],
+        universe:"Moana"
+    },
+    {
+        furnitureName: `Corkscrew Anemone`,
+        price: 2800,
+        style: ["Bold","Quirky"],
+        furnitureCategory: ["Outdoors","Decor"],
+        color: [],
+        universe:"Moana"
+    },
+    {
+        furnitureName: `Engraved Stone Partition`,
+        // price: not available,
+        obtainedBy: "Furniture Pouch",
+        style: [],
+        furnitureCategory: ["Decor"],
+        color: [],
+        universe:"Moana"
+    },
+    {
+        furnitureName: `Fan Coral`,
+        price: 2880,
+        style: ["Bold","Quirky"],
+        furnitureCategory: ["Outdoors","Decor"],
+        color: [],
+        universe:"Moana"
+    },
+    {
+        furnitureName: `Floral Tapestry`,
+        price: 2720,
+        style: ["Rustic","Traditional"],
+        furnitureCategory: ["Bedroom","Living Room","Study"],
+        color: [],
+        universe:"Moana"
+    },
+    {
+        furnitureName: `Giant Clam`,
+        price: 6800,
+        style: ["Bold","Quirky"],
+        furnitureCategory: ["Outdoors","Decor"],
+        color: [],
+        universe:"Moana"
+    },
+    {
+        furnitureName: `Handwoven Palm Basket`,
+        price: 900,
+        style: ["Rustic","Traditional"],
+        furnitureCategory: ["Kitchen","Outdoors","Decor"],
+        color: [],
+        universe:"Moana"
+    },
+    {
+        furnitureName: `Hanging Wicker Light`,
+        price: 2720,
+        style: ["Modern","Rustic"],
+        furnitureCategory: ["Dining Room","Kitchen","Living Room","Lighting"],
+        color: [],
+        universe:"Moana"
+    },
+    {
+        furnitureName: `Heat Vent`,
+        price: 3200,
+        style: ["Bold","Quirky"],
+        furnitureCategory: ["Outdoors","Decor"],
+        color: [],
+        universe:"Moana"
+    },
+    {
+        furnitureName: `Island Drawings`,
+        price: 2720,
+        obtainedBy: "Maui Friendship Level 5",
+        style: ["Cute","Rustic","Traditional"],
+        furnitureCategory: ["Kitchen","Rec Room","Decor"],
+        color: [],
+        universe:"Moana"
+    },
+    {
+        furnitureName: `Maui's #1 Fan`,
+        price: 1,
+        obtainedBy: "The Heart of Dreamlight Valley (Maui Level 10)",
+        style: ["Bold","Traditional"],
+        furnitureCategory: ["Outdoors","Decor"],
+        color: [],
+        universe:"Moana"
+    },
+    {
+        furnitureName: `Medium Fern Coral`,
+        price: 2880,
+        style: ["Bold","Quirky"],
+        furnitureCategory: ["OUtdoors","Deor"],
+        color: [],
+        universe:"Moana"
+    },
+    {
+        furnitureName: `Ornamental Palm Fronds`,
+        price: 2160,
+        style: ["Elegant","Modern"],
+        furnitureCategory: ["Bathroom","Kitchen","Living Room","Decor"],
+        color: [],
+        universe:"Moana"
+    },
+    {
+        furnitureName: `Round Side Table`,
+        price: 3520,
+        style: ["Casual","Elegant","Rustic"],
+        furnitureCategory: ["Living Room","Outdoors","Tables"],
+        color: [],
+        universe:"Moana"
+    },
+    {
+        furnitureName: `Sea Snail Shell`,
+        price: 2040,
+        style: ["Bold","Quirky"],
+        furnitureCategory: ["Outdoors","Decor"],
+        color: [],
+        universe:"Moana"
+    },
+    {
+        furnitureName: `Small Fern Coral`,
+        price: 2880,
+        style: ["Bold","Quirky"],
+        furnitureCategory: ["Outdoors","Decor"],
+        color: [],
+        universe:"Moana"
+    },
+    {
+        furnitureName: `Stone Sink`,
+        price: 6400,
+        style: ["Elegant","Modern","Rustic"],
+        furnitureCategory: ["Kitchen","Tables"],
+        color: [],
+        universe:"Moana"
+    },
+    {
+        furnitureName: `Tall Torch`,
+        price: 2040,
+        style: ["Rustic","Traditional"],
+        furnitureCategory: ["Outdoors","Lighting"],
+        color: [],
+        universe:"Moana"
+    },
+    {
+        furnitureName: `Three-Panel Windows`,
+        price: 4800,
+        style: ["Casual","Elegant","Modern"],
+        furnitureCategory: ["Dining Room","Kitchen","Living Room","Decor"],
+        color: [],
+        universe:"Moana"
+    },
+    {
+        furnitureName: `Tropical Fruit Bowl`,
+        price: 2400,
+        obtainedBy: "Maui Friendship Level 8",
+        style: ["Casual","Elegant","Modern"],
+        furnitureCategory: ["Kitchen","Decor"],
+        color: [],
+        universe:"Moana"
+    },
+    {
+        furnitureName: `Voyager Tapestry`,
+        price: 2720,
+        style: ["Rustic","Traditional"],
+        furnitureCategory: ["Bedroom","Living Room","Study","Decor"],
+        color: [],
+        universe:"Moana"
+    },
+    {
+        furnitureName: `Wicker Bar Stool`,
+        price: 1800,
+        style: ["Elegant","Modern","Rustic"],
+        furnitureCategory: ["Kitchen","Outdoors","Seating"],
+        color: [],
+        universe:"Moana"
+    },
 
 ]
 
