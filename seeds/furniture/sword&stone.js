da4303525c26e6e9eb2402fa56eaf7d3005e07bf
killingsworth.kristen <killingsworth_kristen@yahoo.com>
2022-11-23 11:22:53 -0800

    Sword and stone furniture seed updated

--- seeds/furniture/sword&stone.js
+++ seeds/furniture/sword&stone.js
@@ -1,117 +1,118 @@
-const Set = require(`../../models`)
-const Tag = require(`../../models`)
 
 const SwordStoneFurniture =[
     {
         furnitureName: `Bag of Tricks`,
         price: 3200,
         obtainedBy: "Merlin Friendship Level 2",
-        
-        tagId: [8,11,19],
+        style: ["Quirky","Traditional"],
+        furnitureCategory: ["Study"],
+        color: [],
         universe: "The Sword and the Stone"
     },
     {
         furnitureName: `Big Pile of Books`,
         price: 6400,
-        obtainedBy: "Scrooge's Store",
-        
-        tagId: [8,11,19],
+        style: ["Quirky","Traditional"],
+        furnitureCategory: ["Study"],
+        color: [],
         universe: "The Sword and the Stone"
     },
     {
         furnitureName: `Book Pile`,
         price: 900,
-        obtainedBy: "Scrooge's Store",
-        
-        tagId: [7,11,13,19],
+        style: ["Professional","Traditional"],
+        furnitureCategory: ["Bedroom","Study"],
+        color: [],
         universe: "The Sword and the Stone"
     },
     {
         furnitureName: `Book Piles`,
         price: 3200,
-        obtainedBy: "Scrooge's Store",
-        
-        tagId: [8,11,19],
+        style: ["Quirky","Traditional"],
+        furnitureCategory: ["Study"],
+        color: [],
         universe: "The Sword and the Stone"
     },
     {
         furnitureName: `Line of Books`,
         price: 480,
-        obtainedBy: "Scrooge's Store",
-        
-        tagId: [],
+        style: [],
+        furnitureCategory: ["Decor"],
+        color: [],
         universe: "The Sword and the Stone"
     },
     {
         furnitureName: `Melted Candles`,
         price: 360,
-        obtainedBy: "Scrooge's Store",
-        
-        tagId: [7,11,13,19],
+        style: ["Professional","Traditional"],
+        furnitureCategory: ["Bedroom","Study"],
+        color: [],
         universe: "The Sword and the Stone"
     },
     {
         furnitureName: `Mortar 'n' Pestle`,
         price: 360,
-        obtainedBy: "Scrooge's Store",
-        
-        tagId: [7,8,9,19],
+        style: ["Professional","Quirky","Rustic"],
+        furnitureCategory: ["Study"],
+        color: [],
         universe: "The Sword and the Stone"
     },
     {
         furnitureName: `Old Armchair`,
         price: 900,
-        obtainedBy: "Scrooge's Store",
-        
-        tagId: [2,8,16,18,19],
+        style: ["Casual","Quirky"],
+        furnitureCategory: ["Living Room","Rec Room","Study"],
+        color: [],
         universe: "The Sword and the Stone"
     },
     {
         furnitureName: `Open Book`,
         price: 1200,
-        obtainedBy: "Scrooge's Store",
-        
-        tagId: [7,11,13,19],
+        style: ["Professional","Traditional"],
+        furnitureCategory: ["Bedroom","Study"],
+        color: [],
         universe: "The Sword and the Stone"
     },
     {
         furnitureName: `Orntate Curtain`,
         price: 1200,
-        obtainedBy: "Scrooge's Store",
-        
-        tagId: [16,19],
+        style: [],
+        furnitureCategory: ["Living Room","Study"],
+        color: [],
         universe: "The Sword and the Stone"
     },
     {
         furnitureName: `Phial Holder`,
         price: 1200,
         obtainedBy: "Merlin Friendship Level 5",
-        
-        tagId: [7,8,11,18,19],
+        style: ["Professional","Quirky","Traditional"],
+        furnitureCategory: ["Rec Room","Study"],
+        color: [],
         universe: "The Sword and the Stone"
     },
     {
         furnitureName: `Potion Cupboard`,
         price: 6400,
         obtainedBy: "Merlin Friendship Level 8",
-        
-        tagId: [8,9,11,18,19],
+        style: ["Quirky","Rustic","Traditional"],
+        furnitureCategory: ["Rec Room","Study"],
+        color: [],
         universe: "The Sword and the Stone"
     },
     {
         furnitureName: `Round-Bottomed Flask and Holder`,
         price: 900,
-        obtainedBy: "Scrooge's Store",
-        
-        tagId: [7,8,18,19],
+        style: ["Professional","Quirky"],
+        furnitureCategory: ["Rec Room","Study"],
+        color: [],
         universe: "The Sword and the Stone"
     },
     {
         furnitureName: `Small Book Pile`,
         price: 360,
-        obtainedBy: "Scrooge's Store",
-        
-        tagId: [7,11,13,19],
+        style: ["Professional","Traditional"],
+        furnitureCategory: ["Bedroom","Study"],
+        color: [],
         universe: "The Sword and the Stone"
     }
 ]
