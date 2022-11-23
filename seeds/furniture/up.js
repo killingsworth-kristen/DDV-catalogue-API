98201fab6989a4b8f07917bcf0a945d82b9cd573
killingsworth.kristen <killingsworth_kristen@yahoo.com>
2022-11-23 11:50:35 -0800

    up furniture seed updated

--- seeds/furniture/up.js
+++ seeds/furniture/up.js
@@ -1,21 +1,21 @@
-const Set = require(`../../models`)
-const Tag = require(`../../models`)
 
 const upFurniture = [
     {
         furnitureName: `"Adventure is Out There!" Balloon Basket`,
         price: 720,
         obtainedBy: "Pixar Fest Star Path",
-        
-        tagId: [3,8,17],
+        style: ["Cute","Quirky"],
+        furnitureCategory: ["Outdoors"],
+        color: [],
         universe: "Up"
     },
     {
         furnitureName: `Fredricksen Fireplace`,
         price: 480,
         obtainedBy: "Pixar Fest Star Path",
-        
-        tagId: [2,3,9,14,16,19],
+        style: ["Casual","Cute","Rustic"],
+        furnitureCategory: ["Dining Room","Living Room","Study"],
+        color: [],
         universe: "Up"
     }
 ]
