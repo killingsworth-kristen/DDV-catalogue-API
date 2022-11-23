82b8c41a13d2f7421dc5a092733c3f434232805a
killingsworth.kristen <killingsworth_kristen@yahoo.com>
2022-11-22 19:29:34 -0800

    Wreck it ralph clothing seed updated

--- seeds/clothing/wreckItRalph.js
+++ seeds/clothing/wreckItRalph.js
@@ -1,5 +1,3 @@
-const Set = require(`../../models`)
-const Tag = require(`../../models`)
 
 // 27 items
 
@@ -7,193 +5,221 @@ const wreckItRaplhClothing = [
     {
         clothingName: `Amazing Algorithim Pompadour`,
         price: 650,
-        obtainedBy: "Scrooge's Store",
-        tagId: [28],
-        setId: 8
+        style: [],
+        clothingCategory: "Hairstyle",
+        color: [],
+        set: "Wreck it Ralph"
     },
     {
         clothingName: `Black BFF T-Shirt`,
         price: 360,
-        obtainedBy: "Scrooge's Store",
+        style: ["Modern"],
+        clothingCategory: "Tops",
+        color: [],
         tagId: [6,36],
-        setId: 8
+        set: "Wreck it Ralph"
     },
     {
         clothingName: `Black Lace-Up Combat Boots`,
         // green version said 1000
         price: 4000,
-        obtainedBy: "Scrooge's Store",
-        tagId: [1,10,33],
-        setId: 8
+        style: ["Bold","Sporty"],
+        clothingCategory: "Shoes",
+        color: [],
+        set: "Wreck it Ralph"
     },
     {
         clothingName: `Blue and Gold Tech Bracelets`,
         price: 2400,
-        obtainedBy: "Scrooge's Store",
-        tagId: [1,5,21],
-        setId: 8
+        style: ["Bold","Futuristic"],
+        clothingCategory: "Bracelets",
+        color: [],
+        set: "Wreck it Ralph"
     },
     {
         clothingName: `Candy Racer Helmet`,
         price: 10000,
-        obtainedBy: "Scrooge's Store",
-        tagId: [1,8,10,29],
-        setId: 8
+        style: ["Bold","Quirky","Sporty"],
+        clothingCategory: "Hats",
+        color: [],
+        set: "Wreck it Ralph"
     },
     {
         clothingName: `Candy-Encrusted Pigtails`,
         price: 1500,
-        obtainedBy: "Scrooge's Store",
-        tagId: [28],
-        setId: 8
+        style: [],
+        clothingCategory: "Hairstyle",
+        color: [],
+        set: "Wreck it Ralph"
     },
     {
         clothingName: `Dark Green Cropped Camo Hoodie`,
         price: 4800,
-        obtainedBy: "Scrooge's Store",
-        tagId: [1,10,36],
-        setId: 8
+        style: ["Bold","Sporty"],
+        clothingCategory: "Tops",
+        color: [],
+        set: "Wreck it Ralph"
     },
     {
         clothingName: `Dark Green Pixel Camo Pants`,
         price: 5200,
-        obtainedBy: "Scrooge's Store",
-        tagId: [1,10,32],
-        setId: 8
+        style: ["Bold","Sporty"],
+        clothingCategory: "Pants",
+        color: [],
+        set: "Wreck it Ralph"
     },
     {
         clothingName: `Green "What's the Glitch" Shirt`,
         price: 360,
-        obtainedBy: "Scrooge's Store",
-        tagId: [3,36],
-        setId: 8
+        style: ["Cute"],
+        clothingCategory: "Tops",
+        color: [],
+        set: "Wreck it Ralph"
     },
     {
         clothingName: `Green Cropped Camo Hoodie`,
         price: 4800,
-        obtainedBy: "Scrooge's Store",
-        tagId: [1,10,36],
-        setId: 8
+        style: ["Bold","Sporty"],
+        clothingCategory: "Tops",
+        color: [],
+        set: "Wreck it Ralph"
     },
     {
         clothingName: `Green Dog Tags`,
         price: 2700,
-        obtainedBy: "Scrooge's Store",
-        tagId: [31],
-        setId: 8
+        style: [],
+        clothingCategory: "Neckwear",
+        color: [],
+        set: "Wreck it Ralph"
     },
     {
         clothingName: `Green Lace-Up Combat Boots`,
         // black version said 4000
         price: 1000,
-        obtainedBy: "Scrooge's Store",
-        tagId: [1,10,33],
-        setId: 8
+        style: ["Bold","Sporty"],
+        clothingCategory: "Shoes",
+        color: [],
+        set: "Wreck it Ralph"
     },
     {
         clothingName: `Green Pixel Camo Pants`,
         pprice: 5200,
-        obtainedBy: "Scrooge's Store",
-        tagId: [1,10,32],
-        setId: 8
+        style: ["Bold","Sporty"],
+        clothingCategory: "Pants",
+        color: [],
+        set: "Wreck it Ralph"
     },
     {
         clothingName: `Orange Dog Tags`,
         price: 2700,
-        obtainedBy: "Scrooge's Store",
-        tagId: [31],
-        setId: 8
+        style: [],
+        clothingCategory: "Neckwear",
+        color: [],
+        set: "Wreck it Ralph"
     },
     {
         clothingName: `Pink "What's the Glitch" Shirt`,
         price: 360,
-        obtainedBy: "Scrooge's Store",
-        tagId: [3,36],
-        setId: 8
+        style: ["Cute"],
+        clothingCategory: "Tops",
+        color: [],
+        set: "Wreck it Ralph"
     },
     {
         clothingName: `Pink Candy-Laden Skirt`,
         price: 25000,
-        obtainedBy: "Scrooge's Store",
-        tagId: [3,8,35],
-        setId: 8
+        style: ["Cute","Quirky"],
+        clothingCategory: "Skirts",
+        color: [],
+        set: "Wreck it Ralph"
     },
     {
         clothingName: `Pink Ita Bag`,
         price: 11000,
-        obtainedBy: "Scrooge's Store",
-        tagId: [3,6,8,20],
-        setId: 8
+        style: ["Cute","Modern","Quirky"],
+        clothingCategory: "Back",
+        color: [],
+        set: "Wreck it Ralph"
     },
     {
         clothingName: `Purple Candy-Laden Skirt`,
         price: 25000,
-        obtainedBy: "Scrooge's Store",
-        tagId: [3,8,35],
-        setId: 8
+        style: ["Cute","Quirky"],
+        clothingCategory: "Skirts",
+        color: [],
+        set: "Wreck it Ralph"
     },
     {
         clothingName: `Purple Ita Bag`,
         price: 11000,
-        obtainedBy: "Scrooge's Store",
-        tagId: [3,6,8,20],
-        setId: 8
+        style: ["Cute","Modern","Quirky"],
+        clothingCategory: "Back",
+        color: [],
+        set: "Wreck it Ralph"
     },
     {
         clothingName: `Ralph's Sturdy Overalls`,
         price: 40000,
-        obtainedBy: "Scrooge's Store",
-        tagId: [1,2,6,23],
-        setId: 8
+        style: ["Bold","Casual","Modern"],
+        clothingCategory: "Costumes",
+        color: [],
+        set: "Wreck it Ralph"
     },
     {
         clothingName: `Shank's Squad Jacket`,
         price: 20000,
-        obtainedBy: "Scrooge's Store",
-        tagId: [1,2,22],
-        setId: 8
+        style: ["Bold","Casual"],
+        clothingCategory: "Coats",
+        color: [],
+        set: "Wreck it Ralph"
     },
     {
         clothingName: `Silver and Gold Tech Bracelets`,
         price: 2400,
-        obtainedBy: "Scrooge's Store",
-        tagId: [1,5,21],
-        setId: 8
+        style: ["Bold","Futuristic"],
+        clothingCategory: "Bracelets",
+        color: [],
+        set: "Wreck it Ralph"
     },
     {
         clothingName: `Teal Cargo Shorts`,
         price: 3400,
-        obtainedBy: "Scrooge's Store",
-        tagId: [1,10,34],
-        setId: 8
+        style: ["Bold","Sporty"],
+        clothingCategory: "Shorts",
+        color: [],
+        set: "Wreck it Ralph"
     },
     {
         clothingName: `Turquiose Candy-Laden Skirt`,
         price: 25000,
-        obtainedBy: "Scrooge's Store",
-        tagId: [3,8,35],
-        setId: 8
+        style: ["Cute","Quirky"],
+        clothingCategory: "Skirts",
+        color: [],
+        set: "Wreck it Ralph"
     },
     {
         clothingName: `Turquiose Ita Bag`,
         price: 11000,
-        obtainedBy: "Scrooge's Store",
-        tagId: [3,6,8,20],
-        setId: 8
+        style: ["Cute","Modern","Quirky"],
+        clothingCategory: "Back",
+        color: [],
+        set: "Wreck it Ralph"
     },
     {
         clothingName: `White BFF Shirt`,
         price: 360,
-        obtainedBy: "Scrooge's Store",
-        tagId: [6,36],
-        setId: 8
+        style: ["Modern"],
+        clothingCategory: "Tops",
+        color: [],
+        set: "Wreck it Ralph"
     },
     {
         clothingName: `Yesss's "Run the Net" Dress`,
         price: 40000,
-        obtainedBy: "Scrooge's Store",
-        tagId: [1,4,5,24],
-        setId: 8
+        style: ["Bold","Elegant","Futuristic"],
+        clothingCategory: "Dresses",
+        color: [],
+        set: "Wreck it Ralph"
     },
 
     
