ae375b7f550459a4b498680b983934cf36ae6dd6
killingsworth.kristen <killingsworth_kristen@yahoo.com>
2022-11-22 18:43:35 -0800

    Lion king clothing seed updated

--- seeds/clothing/lionKing.js
+++ seeds/clothing/lionKing.js
@@ -1,5 +1,3 @@
-const Set = require(`../../models`)
-const Tag = require(`../../models`)
 
 // 27 items
 
@@ -7,190 +5,217 @@ const lionKingClothing = [
     {
         clothingName: `Black and Gold Claw Shoes`,
         price: 10000,
-        obtainedBy: "Scrooge's Store",
-        tagId: [1,4,8,33],
-        setId: 10
+        style: ["Bold","Elegant","Quirky"],
+        clothingCategory: "Shoes",
+        color: [],
+        set: "The Lion King"
     },
     {
         clothingName: `Black and Gray Claw Shoes`,
         price: 10000,
-        obtainedBy: "Scrooge's Store",
-        tagId: [1,4,8,33],
-        setId: 10
+        style: ["Bold","Elegant","Quirky"],
+        clothingCategory: "Shoes",
+        color: [],
+        set: "The Lion King"
     },
     {
         clothingName: `Black Claw-Tipped Gloves`,
         price: 20000,
-        obtainedBy: "Scrooge's Store",
-        tagId: [1,4,27],
-        setId: 10
+        style: ["Bold","Elegant"],
+        clothingCategory: "Gloves",
+        color: [],
+        set: "The Lion King"
     },
     {
         clothingName: `Blue Baby-Simba Crop Top`,
         price: 2400,
-        obtainedBy: "Scrooge's Store",
-        tagId: [2,3,36],
-        setId: 10
+        style: ["Casual","Cute"],
+        clothingCategory: "Tops",
+        color: [],
+        set: "The Lion King"
     },
     {
         clothingName: `Blue Earrings`,
         price: 3800,
-        obtainedBy: "Scrooge's Store",
-        tagId: [1,2,11,25],
-        setId: 10
+        style: ["Bold","Casual","Traditional"],
+        clothingCategory: "Earrings",
+        color: [],
+        set: "The Lion King"
     },
     {
         clothingName: `Blue Silk Shirt`,
         price: 4800,
-        obtainedBy: "Scrooge's Store",
-        tagId: [4,8,36],
-        setId: 10
+        style: ["Elegant","Quirky"],
+        clothingCategory: "Tops",
+        color: [],
+        set: "The Lion King"
     },
     {
         clothingName: `Blue Sunrise Bracelet`,
         price: 4800,
-        obtainedBy: "Scrooge's Store",
-        tagId: [1,4,6,21],
-        setId: 10
+        style: ["Bold","Elegant","Modern"],
+        clothingCategory: "Bracelets",
+        color: [],
+        set: "The Lion King"
     },
     {
         clothingName: `Brown Claw-Tipped Gloves`,
         price: 20000,
-        obtainedBy: "Scrooge's Store",
-        tagId: [1,4,27],
-        setId: 10
+        style: ["Bold","Elegant"],
+        clothingCategory: "Gloves",
+        color: [],
+        set: "The Lion King"
     },
     {
         clothingName: `Brown Faux-Fur-Trimmed Jacket`,
         price: 8000,
-        obtainedBy: "Scrooge's Store",
-        tagId: [1,22],
-        setId: 10
+        style: ["Bold"],
+        clothingCategory: "Coats",
+        color: [],
+        set: "The Lion King"
     },
     {
         clothingName: `Classy Blue Dress Shoes`,
         price: 4000,
-        obtainedBy: "Scrooge's Store",
-        tagId: [4,6,7,33],
-        setId: 10
+        style: ["Elegant","Modern","Professional"],
+        clothingCategory: "Shoes",
+        color: [],
+        set: "The Lion King"
     },{
         clothingName: `Classy Purple Dress Shoes`,
         price: 4000,
-        obtainedBy: "Scrooge's Store",
-        tagId: [4,6,7,33],
-        setId: 10
+        style: ["Elegant","Modern","Professional"],
+        clothingCategory: "Shoes",
+        color: [],
+        set: "The Lion King"
     },
     {
         clothingName: `Gold and Purple Watch`,
         price: 4800,
-        obtainedBy: "Scrooge's Store",
-        tagId: [21],
-        setId: 10
+        style: [],
+        clothingCategory: "Bracelets",
+        color: [],
+        set: "The Lion King"
     },
     {
         clothingName: `Gray Claw Top`,
         price: 8000,
-        obtainedBy: "Scrooge's Store",
-        tagId: [1,22],
-        setId: 10
+        style: ["Bold"],
+        clothingCategory: "Coats",
+        color: [],
+        set: "The Lion King"
     },
     {
         clothingName: `Green Claw Top`,
         price: 8000,
-        obtainedBy: "Scrooge's Store",
-        tagId: [1,22],
-        setId: 10
+        style: ["Bold"],
+        clothingCategory: "Coats",
+        color: [],
+        set: "The Lion King"
     },
     {
         clothingName: `Orange "Time Flies" Shirt`,
         price: 360,
-        obtainedBy: "Scrooge's Store",
-        tagId: [6,36],
-        setId: 10
+        style: ["Modern"],
+        clothingCategory: "Tops",
+        color: [],
+        set: "The Lion King"
     },
     {
         clothingName: `Orange Earrings`,
         price: 3800,
-        obtainedBy: "Scrooge's Store",
-        tagId: [1,2,11,25],
-        setId: 10
+        style: ["Bold","Casual","Traditional"],
+        clothingCategory: "Earrings",
+        color: [],
+        set: "The Lion King"
     },
     {
         clothingName: `Orange Sunrise Bracelet`,
         price: 4800,
-        obtainedBy: "Scrooge's Store",
-        tagId: [1,4,6,21],
-        setId: 10
+        style: ["Bold","Elegant","Modern"],
+        clothingCategory: "Bracelets",
+        color: [],
+        set: "The Lion King"
     },
     {
         clothingName: `Purple and Gray Silk Shirt`,
         price: 4800,
-        obtainedBy: "Scrooge's Store",
-        tagId: [4,8,36],
-        setId: 10
+        style: ["Elegant","Quirky"],
+        clothingCategory: "Tops",
+        color: [],
+        set: "The Lion King"
     },
     {
         clothingName: `Purple Baby-Simba Crop Top`,
         price: 2400,
-        obtainedBy: "Scrooge's Store",
-        tagId: [2,3,36],
-        setId: 10
+        style: ["Casual","Cute"],
+        clothingCategory: "Tops",
+        color: [],
+        set: "The Lion King"
     },
     {
         clothingName: `Purple Silk Shirt`,
         price: 4800,
-        obtainedBy: "Scrooge's Store",
-        tagId: [4,8,36],
-        setId: 10
+        style: ["Elegant","Quirky"],
+        clothingCategory: "Tops",
+        color: [],
+        set: "The Lion King"
     },
     {
         clothingName: `Red Faux-Fur-Trimmed Jacket`,
         price: 8000,
-        obtainedBy: "Scrooge's Store",
-        tagId: [1,22],
-        setId: 10
+        style: ["Bold"],
+        clothingCategory: "Coats",
+        color: [],
+        set: "The Lion King"
     },
     {
         clothingName: `Red Headwrap`,
         price: 2000,
-        obtainedBy: "Scrooge's Store",
-        tagId: [1,6,29],
-        setId: 10
+        style: ["Bold","Modern"],
+        clothingCategory: "Hats",
+        color: [],
+        set: "The Lion King"
     },
     {
         clothingName: `Silver and Purple Watch`,
         price: 4800,
-        obtainedBy: "Scrooge's Store",
-        tagId: [21],
-        setId: 10
+        style: [],
+        clothingCategory: "Bracelets",
+        color: [],
+        set: "The Lion King"
     },
     {
         clothingName: `Tan Sunny Slacks`,
         price: 2600,
-        obtainedBy: "Scrooge's Store",
-        tagId: [6,7,32],
-        setId: 10
+        style: ["Modern","Professional"],
+        clothingCategory: "Pants",
+        color: [],
+        set: "The Lion King"
     },
     {
         clothingName: `White Sunny Slacks`,
         price: 2600,
-        obtainedBy: "Scrooge's Store",
-        tagId: [6,7,32],
-        setId: 10
+        style: ["Modern","Professional"],
+        clothingCategory: "Pants",
+        color: [],
+        set: "The Lion King"
     },
     {
         clothingName: `Yellow "Time Flies" Shirt`,
         price: 360,
-        obtainedBy: "Scrooge's Store",
-        tagId: [6,36],
-        setId: 10
+        style: ["Modern"],
+        clothingCategory: "Tops",
+        color: [],
+        set: "The Lion King"
     },
     {
         clothingName: `Yellow Headwrap`,
         price: 2000,
-        obtainedBy: "Scrooge's Store",
-        tagId: [1,6,29],
-        setId: 10
+        style: ["Bold","Modern"],
+        clothingCategory: "Hats",
+        color: [],
+        set: "The Lion King"
     },
 ]
 
