92a43bef2b85bc8529f64767fe2a7cb13322c1a9
killingsworth.kristen <killingsworth_kristen@yahoo.com>
2022-11-22 19:49:50 -0800

    Cinderella clothing seed added

--- seeds/clothing/cinderella.js
+++ seeds/clothing/cinderella.js
@@ -1,16 +1,239 @@
 
 // 29 items
 
-const cinderllaClothing = [
-    // {
-    //     clothingName: ``,
-    //     price: ,
-    //     obtainedBy: "",
-    //     style: [],
-    //     clothingCategory: "",
-    //     color: [],
-    //     set: "Cinderella"
-    // },
+const cinderellaClothing = [
+    {
+        clothingName: `Blue Mice-Sewn Pearl-Strung Gown`,
+        price: 40000,
+        style: ["Cute","Elegant"],
+        clothingCategory: "Dresses",
+        color: [],
+        set: "Cinderella"
+    },
+    {
+        clothingName: `Blue-Green Bandana`,
+        price: 750,
+        style: ["Casual","cute","Professional"],
+        clothingCategory: "Hats",
+        color: [],
+        set: "Cinderella"
+    },
+    {
+        clothingName: `Burgundy Gold Lock Skirt`,
+        price: 10000,
+        style: ["Elegant","Quirky"],
+        clothingCategory: "Skirts",
+        color: [],
+        set: "Cinderella"
+    },
+    {
+        clothingName: `Enchanting Blue Pumpkin Ring`,
+        price: 1800,
+        style: ["Traditional","Elegant","Cute"],
+        clothingCategory: "Gloves",
+        color: [],
+        set: "Cinderella"
+    },
+    {
+        clothingName: `Enchanting Pick Pumpkin Ring`,
+        price: 1800,
+        style: ["Traditional","Elegant","Cute"],
+        clothingCategory: "Gloves",
+        color: [],
+        set: "Cinderella"
+    },
+    {
+        clothingName: `Enchanting White Pumpkin RIng`,
+        price: 1800,
+        style: ["Traditional","Elegant","Cute"],
+        clothingCategory: "Gloves",
+        color: [],
+        set: "Cinderella"
+    },
+    {
+        clothingName: `Fancy Black and Orange Hat`,
+        price: 2000,
+        style: ["Elegant","Traditional"],
+        clothingCategory: "Hats",
+        color: [],
+        set: "Cinderella"
+    },
+    {
+        clothingName: `Fancy Black Hat`,
+        price: 2000,
+        style: ["Elegant","Traditional"],
+        clothingCategory: "Hats",
+        color: [],
+        set: "Cinderella"
+    },
+    {
+        clothingName: `Fancy Bronze Bracelet`,
+        price: 900,
+        style: ["Elegant","Traditional"],
+        clothingCategory: "Bracelets",
+        color: [],
+        set: "Cinderella"
+    },
+    {
+        clothingName: `Fancy Gold Bracelet`,
+        price: 900,
+        style: ["Elegant","Traditional"],
+        clothingCategory: "Bracelets",
+        color: [],
+        set: "Cinderella"
+    },
+    {
+        clothingName: `Fancy Gold Jeweled Gloves`,
+        price: 8000,
+        style: ["Bold","Elegant","Traditional"],
+        clothingCategory: "Gloves",
+        color: [],
+        set: "Cinderella"
+    },
+    {
+        clothingName: `Fancy Silver Bracelet`,
+        price: 900,
+        style: ["Elegant","Traditional"],
+        clothingCategory: "Bracelets",
+        color: [],
+        set: "Cinderella"
+    },
+    {
+        clothingName: `Fancy Silver Jeweled Gloves`,
+        price: 8000,
+        style: ["Bold","Elegant","Traditional"],
+        clothingCategory: "Gloves",
+        color: [],
+        set: "Cinderella"
+    },
+    {
+        clothingName: `Green Bandana`,
+        price: 750,
+        style: ["Casual","cute","Professional"],
+        clothingCategory: "Hats",
+        color: [],
+        set: "Cinderella"
+    },
+    {
+        clothingName: `Green Pearls of Freedom Necklace`,
+        price: 6000,
+        style: ["Cute","Elegant"],
+        clothingCategory: "Neckwear",
+        color: [],
+        set: "Cinderella"
+    },
+    {
+        clothingName: `Green Silver Lock Skirt`,
+        price: 10000,
+        style: ["Elegant","Quirky"],
+        clothingCategory: "Skirts",
+        color: [],
+        set: "Cinderella"
+    },
+    {
+        clothingName: `Once Upon a Ball Gown`,
+        obtainedBy: "Founder's Pack: Ultimate Edition",
+        style: [],
+        clothingCategory: "Dresses",
+        color: [],
+        set: "Cinderella"
+    },
+    {
+        clothingName: `Pastel Blue Bandana`,
+        price: 750,
+        style: ["Casual","cute","Professional"],
+        clothingCategory: "Hats",
+        color: [],
+        set: "Cinderella"
+    },
+    {
+        clothingName: `Pearl-Laced Updo`,
+        price: 2000,
+        style: [],
+        clothingCategory: "Hairstyle",
+        color: [],
+        set: "Cinderella"
+    },
+    {
+        clothingName: `Pink Bandana`,
+        price: 750,
+        style: ["Casual","cute","Professional"],
+        clothingCategory: "Hats",
+        color: [],
+        set: "Cinderella"
+    },
+    {
+        clothingName: `Pink Jewel-Collared Button-Up`,
+        price: 4800,
+        style: ["Elegant","Traditional"],
+        clothingCategory: "Tops",
+        color: [],
+        set: "Cinderella"
+    },
+    {
+        clothingName: `Pink Mice-Sewn Pearl-Strung Gown`,
+        price: 40000,
+        style: ["Cute","Elegant"],
+        clothingCategory: "Dresses",
+        color: [],
+        set: "Cinderella"
+    },
+    {
+        clothingName: `Pink Pearls of Freedom Necklace`,
+        price: 6000,
+        style: ["Cute","Elegant"],
+        clothingCategory: "Neckwear",
+        color: [],
+        set: "Cinderella"
+    },
+    {
+        clothingName: `Purple Jewel-Collared Button-Up`,
+        price: 4800,
+        style: ["Elegant","Traditional"],
+        clothingCategory: "Tops",
+        color: [],
+        set: "Cinderella"
+    },
+    {
+        clothingName: `Sturdy Blue Overalls`,
+        price: 8000,
+        style: ["Casual","Cute"],
+        clothingCategory: "Costumes",
+        color: [],
+        set: "Cinderella"
+    },
+    {
+        clothingName: `Sturdy Brown Overalls`,
+        price: 8000,
+        style: ["Casual","Cute"],
+        clothingCategory: "Costumes",
+        color: [],
+        set: "Cinderella"
+    },
+    {
+        clothingName: `Sturdy White Overalls`,
+        price: 8000,
+        style: ["Casual","Cute"],
+        clothingCategory: "Costumes",
+        color: [],
+        set: "Cinderella"
+    },
+    {
+        clothingName: `Till the Stroke of Midnight Ensemble`,
+        obtainedBy: "Founder's Pack: Ultimate Edition",
+        style: [],
+        clothingCategory: "Costumes",
+        color: [],
+        set: "Cinderella"
+    },
+    {
+        clothingName: `White Pearls of Freedom Necklace`,
+        price: 6000,
+        style: ["Cute","Elegant"],
+        clothingCategory: "Neckwear",
+        color: [],
+        set: "Cinderella"
+    }
 ]
 
-module.exports=cinderllaClothing
+module.exports=cinderellaClothing
