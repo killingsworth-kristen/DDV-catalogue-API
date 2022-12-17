94aed66ad6da552bc46a45c8e5ab21581d3a868c
killingsworth.kristen <killingsworth_kristen@yahoo.com>
2022-12-16 21:21:08 -0500

    Monsters inc clothing seeded

--- seeds/clothing/MonstersInc.js
+++ seeds/clothing/MonstersInc.js
@@ -2,15 +2,295 @@
 // 32 items
 
 const monstersIncClothing = [
-//     {
-//         clothingName: ``,
-//         price: ,
-//         obtainedBy: "Scrooge's Store",
-//         style: [],
-        // clothingCategory: "",
-        // color: [],
-//         universe: "Monsters Inc."
-//     },
+    {
+        clothingName: `"Scarer Sulley" Slippers`,
+        price: 10000,
+        obtainedBy: "Scrooge's Store",
+        style: ["Casual","Quirky","Modern"],
+        clothingCategory: "Shoes",
+        color: [],
+        universe: "Monsters Inc."
+    },
+    {
+        clothingName: `Vlack and Yellow Rubber Gloves`,
+        price: 1500,
+        obtainedBy: "Scrooge's Store",
+        style: ["Modern","Professional","Quirky"],
+        clothingCategory: "Gloves",
+        color: [],
+        universe: "Monsters Inc."
+    },
+    {
+        clothingName: `Black Goggles`,
+        price: 5600,
+        obtainedBy: "Scrooge's Store",
+        style: ["Bold","Quirky","Futuristic"],
+        clothingCategory: "Glasses",
+        color: [],
+        universe: "Monsters Inc."
+    },
+    {
+        clothingName: `Black Rain Jacket`,
+        price: 4000,
+        obtainedBy: "Scrooge's Store",
+        style: ["Bold","Quirky","Futuristic"],
+        clothingCategory: "coats",
+        color: [],
+        universe: "Monsters Inc."
+    },
+    {
+        clothingName: `Black Sweats`,
+        price: 975,
+        obtainedBy: "Scrooge's Store",
+        style: ["Bold","Modern"],
+        clothingCategory: "Pants",
+        color: [],
+        universe: "Monsters Inc."
+    },
+    {
+        clothingName: `Blue Goggles`,
+        price: 5600,
+        obtainedBy: "Scrooge's Store",
+        style: ["Bold","Quirky","Futuristic"],
+        clothingCategory: "Glasses",
+        color: [],
+        universe: "Monsters Inc."
+    },
+    {
+        clothingName: `Blue Headphones`,
+        price: 6000,
+        obtainedBy: "Scrooge's Store",
+        style: ["Bold","Modern"],
+        clothingCategory: "Neckwear",
+        color: [],
+        universe: "Monsters Inc."
+    },
+    {
+        clothingName: `Blue Mike Bag`,
+        price: 825,
+        obtainedBy: "Scrooge's Store",
+        style: ["Bold","Modern"],
+        clothingCategory: "Back",
+        color: [],
+        universe: "Monsters Inc."
+    },
+    {
+        clothingName: `Blue Rain Jacket`,
+        price: 4000,
+        obtainedBy: "Scrooge's Store",
+        style: ["Bold","Quirky","Futuristic"],
+        clothingCategory: "coats",
+        color: [],
+        universe: "Monsters Inc."
+    },
+    {
+        clothingName: `Blue Sweats`,
+        price: 975,
+        obtainedBy: "Scrooge's Store",
+        style: ["Bold","Modern"],
+        clothingCategory: "Pants",
+        color: [],
+        universe: "Monsters Inc."
+    },
+    {
+        clothingName: `Chunky Sneakers With Blue Highlights`,
+        price: 4000,
+        obtainedBy: "Scrooge's Store",
+        style: ["Bold","Modern"],
+        clothingCategory: "Shoes",
+        color: [],
+        universe: "Monsters Inc."
+    },
+    {
+        clothingName: `Chunky Sneakers With Green Highlights`,
+        price: 4000,
+        obtainedBy: "Scrooge's Store",
+        style: ["Bold","Modern"],
+        clothingCategory: "Shoes",
+        color: [],
+        universe: "Monsters Inc."
+    },
+    {
+        clothingName: `Chunky Sneakers With Red Highlights`,
+        price: 200,
+        // price: 4000,
+        obtainedBy: "Scrooge's Store",
+        style: ["Bold","Modern"],
+        clothingCategory: "Shoes",
+        color: [],
+        universe: "Monsters Inc."
+    },
+    {
+        clothingName: `Decontamination Pack`,
+        price: 2200,
+        obtainedBy: "Scrooge's Store",
+        style: ["Futuristic","Bold","Quirky"],
+        clothingCategory: "Back",
+        color: [],
+        universe: "Monsters Inc."
+    },
+    {
+        clothingName: `Gray Jean Jacket WIth Patches`,
+        price: 4000,
+        obtainedBy: "Scrooge's Store",
+        style: ["Bold","Modern"],
+        clothingCategory: "coats",
+        color: [],
+        universe: "Monsters Inc."
+    },
+    {
+        clothingName: `Green Headphones`,
+        price: 6000,
+        obtainedBy: "Scrooge's Store",
+        style: ["Bold","Modern"],
+        clothingCategory: "Neckwear",
+        color: [],
+        universe: "Monsters Inc."
+    },
+    {
+        clothingName: `Green Mike Bag`,
+        price: 825,
+        obtainedBy: "Scrooge's Store",
+        style: ["Bold","Modern"],
+        clothingCategory: "Back",
+        color: [],
+        universe: "Monsters Inc."
+    },
+    {
+        clothingName: `Green Tufted Horn`,
+        price: 4000,
+        obtainedBy: "Scrooge's Store",
+        style: ["Bold","Quirky"],
+        clothingCategory: "Hats",
+        color: [],
+        universe: "Monsters Inc."
+    },
+    {
+        clothingName: `Navy Blue Jean Jacket with Patches`,
+        price: 4000,
+        obtainedBy: "Scrooge's Store",
+        style: ["Bold","Modern"],
+        clothingCategory: "coats",
+        color: [],
+        universe: "Monsters Inc."
+    },
+    {
+        clothingName: `Pale Blue Jean Jacket with Patches`,
+        price: 4000,
+        obtainedBy: "Scrooge's Store",
+        style: ["Bold","Modern"],
+        clothingCategory: "coats",
+        color: [],
+        universe: "Monsters Inc."
+    },
+    {
+        clothingName: `Puffy Blue Jacket`,
+        price: 1500,
+        obtainedBy: "Scrooge's Store",
+        style: ["Modern","Casual","Sporty"],
+        clothingCategory: "Coats",
+        color: [],
+        universe: "Monsters Inc."
+    },
+    {
+        clothingName: `Puffy Green Jacket`,
+        price: 1500,
+        obtainedBy: "Scrooge's Store",
+        style: ["Modern","Casual","Sporty"],
+        clothingCategory: "Coats",
+        color: [],
+        universe: "Monsters Inc."
+    },
+    {
+        clothingName: `Puffy Red Jacket`,
+        price: 1500,
+        obtainedBy: "Scrooge's Store",
+        style: ["Modern","Casual","Sporty"],
+        clothingCategory: "Coats",
+        color: [],
+        universe: "Monsters Inc."
+    },
+    {
+        clothingName: `Purple Tufted Horn`,
+        price: 4000,
+        obtainedBy: "Scrooge's Store",
+        style: ["Bold","Quirky"],
+        clothingCategory: "Hats",
+        color: [],
+        universe: "Monsters Inc."
+    },
+    {
+        clothingName: `Red Headphones`,
+        price: 6000,
+        obtainedBy: "Scrooge's Store",
+        style: ["Bold","Modern"],
+        clothingCategory: "Neckwear",
+        color: [],
+        universe: "Monsters Inc."
+    },
+    {
+        clothingName: `Red Mike Bag`,
+        price: 825,
+        obtainedBy: "Scrooge's Store",
+        style: ["Bold","Modern"],
+        clothingCategory: "Back",
+        color: [],
+        universe: "Monsters Inc."
+    },
+    {
+        clothingName: `Red Sweats`,
+        price: 975,
+        obtainedBy: "Scrooge's Store",
+        style: ["Bold","Modern"],
+        clothingCategory: "Pants",
+        color: [],
+        universe: "Monsters Inc."
+    },
+    {
+        clothingName: `Rubber Boots`,
+        price: 2000,
+        obtainedBy: "Scrooge's Store",
+        style: ["Modern","Casual"],
+        clothingCategory: "Shoes",
+        color: [],
+        universe: "Monsters Inc."
+    },
+    {
+        clothingName: `White Goggles`,
+        price: 5600,
+        obtainedBy: "Scrooge's Store",
+        style: ["Bold","Quirky","Futuristic"],
+        clothingCategory: "Glasses",
+        color: [],
+        universe: "Monsters Inc."
+    },
+    {
+        clothingName: `White Rain Jacket`,
+        price: 4000,
+        obtainedBy: "Scrooge's Store",
+        style: ["Bold","Quirky","Futuristic"],
+        clothingCategory: "coats",
+        color: [],
+        universe: "Monsters Inc."
+    },
+    {
+        clothingName: `Yellow Goggles`,
+        price: 5600,
+        obtainedBy: "Scrooge's Store",
+        style: ["Bold","Quirky","Futuristic"],
+        clothingCategory: "Glasses",
+        color: [],
+        universe: "Monsters Inc."
+    },
+    {
+        clothingName: `Yellow Rain Jacket`,
+        price: 4000,
+        obtainedBy: "Scrooge's Store",
+        style: ["Bold","Quirky","Futuristic"],
+        clothingCategory: "coats",
+        color: [],
+        universe: "Monsters Inc."
+    },
 ]
 
 module.exports=monstersIncClothing
