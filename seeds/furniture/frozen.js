daeed4103c0d39bf8ba159d3c3661134c1cd457c
killingsworth.kristen <killingsworth_kristen@yahoo.com>
2022-12-17 00:57:37 -0500

    Frozen furniture seed added

--- seeds/furniture/frozen.js
+++ seeds/furniture/frozen.js
@@ -2,15 +2,410 @@
 // 45 pieces of furniture
 
 const frozenFurniture = [
-    // {
-    //     furnitureName: ``,
-    //     price: ,
-    //     obtainedBy: "Scrooge's Store",
-    //     style: [],
-    //     furnitureCategory: [""],
-    //     color: [],
-    //     universe:"Frozen"
-    // },
+    {
+        furnitureName: `"Spring Reborn" Vertical Tapestry`,
+        price: 2720,
+        style: ["Rustic","Traditional"],
+        furnitureCategory: ["Living Room","Rec Room","Study","Decor"],
+        color: [],
+        universe:"Frozen"
+    },
+    {
+        furnitureName: `Anna Statue`,
+        // price: 1,
+        tracked: false,
+        style: ["Elegant"],
+        furnitureCategory: ["Outdoors","Decor"],
+        color: [],
+        universe:"Frozen"
+    },
+    {
+        furnitureName: `Anna's Picnic Set`,
+        price: 8000,
+        style: ["Traditional","Rustic","Casual"],
+        furnitureCategory: ["Outdoors","Decor"],
+        color: [],
+        universe:"Frozen"
+    },
+    {
+        furnitureName: `Banner`,
+        price: 3200,
+        obtainedBy: "Anna Friendship Level 8",
+        style: ["Traditional","Elegant"],
+        furnitureCategory: ["Dining Room","Living Room","Study","Decor"],
+        color: [],
+        universe:"Frozen"
+    },
+    {
+        furnitureName: `Birch L-shaped Bench`,
+        price: 3600,
+        style: ["Elegant","Rustic"],
+        furnitureCategory: ["Rec Room","Bathroom","Seating"],
+        color: [],
+        universe:"Frozen"
+    },
+    {
+        furnitureName: `Birch Toiletry Basket`,
+        price: 1200,
+        style: ["Elegant","Rustic"],
+        furnitureCategory: ["Rec Room","Bathroom","Decor"],
+        color: [],
+        universe:"Frozen"
+    },
+    {
+        furnitureName: `Birch Tub`,
+        price: 20000,
+        style: ["Elegant","Rustic"],
+        furnitureCategory: ["Rec Room","Bathroom","Decor"],
+        color: [],
+        universe:"Frozen"
+    },
+    {
+        furnitureName: `Birch Water Bucket`,
+        price: 360,
+        style: ["Elegant","Rustic"],
+        furnitureCategory: ["Rec Room","Bathroom","Decor"],
+        color: [],
+        universe:"Frozen"
+    },
+    {
+        furnitureName: `Blue Robe Hanger`,
+        price: 6400,
+        style: ["Elegant","Rustic"],
+        furnitureCategory: ["Rec Room","Bathroom","Decor"],
+        color: [],
+        universe:"Frozen"
+    },
+    {
+        furnitureName: `Blue Robe Shelf`,
+        price: 6400,
+        style: ["Elegant","Rustic"],
+        furnitureCategory: ["Rec Room","Bathroom","Decor"],
+        color: [],
+        universe:"Frozen"
+    },
+    {
+        furnitureName: `Candle`,
+        price: 306,
+        style: ["Rustic","Traditional"],
+        furnitureCategory: ["Living Room","Study","Decor"],
+        color: [],
+        universe:"Frozen"
+    },
+    {
+        furnitureName: `Chandelier`,
+        price: 5440,
+        style: ["Traditional","Elegant"],
+        furnitureCategory: ["Dining Room","Living Room","Lighting"],
+        color: [],
+        universe:"Frozen"
+    },
+    {
+        furnitureName: `Chilled Chair`,
+        price: 4800,
+        style: ["Bold","Elegant"],
+        furnitureCategory: ["Dining Room","Study","Seating"],
+        color: [],
+        universe:"Frozen"
+    },
+    {
+        furnitureName: `Four-Poster Bed`,
+        price: 3680,
+        obtainedBy: "Anna Friendship Level 5",
+        style: ["Traditional","Elegant"],
+        furnitureCategory: ["Bedroom","Beds"],
+        color: [],
+        universe:"Frozen"
+    },
+    {
+        furnitureName: `Frost Chandelier`,
+        price: 5440,
+        style: ["Bold","Elegant"],
+        furnitureCategory: ["Bedroom","Dining Room","Living Room","Decor"],
+        color: [],
+        universe:"Frozen"
+    },
+    {
+        furnitureName: `Frozen Portal`,
+        price: 6400,
+        style: ["Bold","Elegant"],
+        furnitureCategory: ["Bedroom","Dining Room","Living Room","Decor"],
+        color: [],
+        universe:"Frozen"
+    },
+    {
+        furnitureName: `Gallant Painting`,
+        // price: ,
+        style: ["Traditional","Elegant"],
+        furnitureCategory: ["Dining Room","Living Room","Study","Decor"],
+        color: [],
+        universe:"Frozen",
+        added: 1.1
+    },
+    {
+        furnitureName: `Gold Hot Steam Rocks`,
+        price: 3200,
+        style: ["Elegant","Rustic"],
+        furnitureCategory: ["Bathroom","Rec Room","Decor"],
+        color: [],
+        universe:"Frozen"
+    },
+    {
+        furnitureName: `Grandfather Clock`,
+        price: 6400,
+        style: ["Traditional","Elegant"],
+        furnitureCategory: ["Dining Room","Study","Living Room","Decor"],
+        color: [],
+        universe:"Frozen"
+    },
+    {
+        furnitureName: `Green Window to the Mountains`,
+        price: 20000,
+        style: ["Elegant","Rustic"],
+        furnitureCategory: ["Rec Room","Bathroom","Windows"],
+        color: [],
+        universe:"Frozen"
+    },
+    {
+        furnitureName: `Hot Cocoa Stand`,
+        price: 480,
+        obtainedBy:"Festive Star Path",
+        style: ["Tradtional","Rustic"],
+        furnitureCategory: ["Outdoors","Decor"],
+        color: [],
+        universe:"Frozen",
+        added: 1.2
+    },
+    {
+        furnitureName: `Ice Castle Vertical Tapestry`,
+        price: 2720,
+        style: ["Rustic","Traditional"],
+        furnitureCategory: ["Living Room","Rec Room","Study","Decor"],
+        color: [],
+        universe:"Frozen"
+    },
+    {
+        furnitureName: `Ice Glazed Couch`,
+        price: 6000,
+        style: ["Bold","Elegant"],
+        furnitureCategory: ["Bedroom","Living Room","Dining Room","Seating"],
+        color: [],
+        universe:"Frozen"
+    },
+    {
+        furnitureName: `Ice Throne`,
+        price: 210,
+        obtainedBy: "Elsa Friendship Level 8",
+        style: ["Bold","Elegant"],
+        furnitureCategory: ["Study","Rec Room","Outdoors","Seating"],
+        color: [],
+        universe:"Frozen"
+    },
+    {
+        furnitureName: `Icicle Dining Table`,
+        price: 4400,
+        style: ["Bold","Elegant"],
+        furnitureCategory: ["Dining Room","Tables"],
+        color: [],
+        universe:"Frozen"
+    },
+    {
+        furnitureName: `Icy Fountain`,
+        price: 8000,
+        style: ["Bold","Elegant"],
+        furnitureCategory: ["Rec Room","Bathroom","Decor"],
+        color: [],
+        universe:"Frozen"
+    },
+    {
+        furnitureName: `Icy Window`,
+        price: 3200,
+        style: ["Rustic","Traditional"],
+        furnitureCategory: ["Kitchen","Dining Room","Living Room","Windows"],
+        color: [],
+        universe:"Frozen"
+    },
+    {
+        furnitureName: `Kristoff's Sled`,
+        price: 20000,
+        obtainedBy: "A Broken Sled (Kristoff Level 10)",
+        style: ["Traditional","Rustic"],
+        furnitureCategory: ["Outdoors","Decor"],
+        color: [],
+        universe:"Frozen"
+    },
+    {
+        furnitureName: `Library Side Table`,
+        price: 1320,
+        style: ["Rustic","Traditional"],
+        furnitureCategory: ["Living Room","Study","Tables"],
+        color: [],
+        universe:"Frozen"
+    },
+    {
+        furnitureName: `Long Dining Table`,
+        price: 8800,
+        style: ["Traditional","Elegant"],
+        furnitureCategory: ["Dining Room","Tables"],
+        color: [],
+        universe:"Frozen"
+    },
+    {
+        furnitureName: `Miniature Snow Castle`,
+        price: 2720,
+        style: ["Elegant","Quirky"],
+        furnitureCategory: ["Outdoors","Decor"],
+        color: [],
+        universe:"Frozen"
+    },
+    {
+        furnitureName: `Norwegian Spruce L-shaped Bench`,
+        price: 3600,
+        style: ["Elegant","Rustic"],
+        furnitureCategory: ["Rec Room","Bathroom","Seating"],
+        color: [],
+        universe:"Frozen"
+    },
+    {
+        furnitureName: `Norwegian Spruce Robe Hanger`,
+        price: 6400,
+        style: ["Elegant","Rustic"],
+        furnitureCategory: ["Rec Room","Bathroom","Decor"],
+        color: [],
+        universe:"Frozen"
+    },
+    {
+        furnitureName: `Norwegian Spruce Robe Shelf`,
+        price: 6400,
+        style: ["Elegant","Rustic"],
+        furnitureCategory: ["Rec Room","Bathroom","Decor"],
+        color: [],
+        universe:"Frozen"
+    },
+    {
+        furnitureName: `Norwegian Spruce Toiletry Basket`,
+        price: 1200,
+        style: ["Elegant","Rustic"],
+        furnitureCategory: ["Rec Room","Bathroom","Decor"],
+        color: [],
+        universe:"Frozen"
+    },
+    {
+        furnitureName: `Norwegian Spruce Tub`,
+        price: 20000,
+        obtainedBy: "Kristoff Friendship Level 8",
+        style: ["Elegant","Rustic"],
+        furnitureCategory: ["Rec Room","Bathroom","Decor"],
+        color: [],
+        universe:"Frozen"
+    },
+    {
+        furnitureName: `Norwegian Spruce Water Bucket`,
+        price: 360,
+        style: ["Elegant","Rustic"],
+        furnitureCategory: ["Rec Room","Bathroom","Decor"],
+        color: [],
+        universe:"Frozen"
+    },
+    {
+        furnitureName: `Norwegian Spruce Window to the Mountains`,
+        price: 20000,
+        style: ["Elegant","Rustic"],
+        furnitureCategory: ["Rec Room","Bathroom","Windows"],
+        color: [],
+        universe:"Frozen"
+    },
+    {
+        furnitureName: `Ornate Dining Chair`,
+        price: 2400,
+        style: ["Traditional","Elegant"],
+        furnitureCategory: ["Dining Room","Seating"],
+        color: [],
+        universe:"Frozen"
+    },
+    {
+        furnitureName: `Painted Double Door`,
+        price: 3200,
+        obtainedBy: "Anna Friendship Level 2",
+        style: ["Traditional","Elegant"],
+        furnitureCategory: ["Dining Room","Bedroom","Study","Living Room", "Decor"],
+        color: [],
+        universe:"Frozen"
+    },
+    {
+        furnitureName: `Pillar`,
+        price: 3200,
+        style: ["Bold","Elegant"],
+        furnitureCategory: ["Dining Room","Living Room","Outdoors"],
+        color: [],
+        universe:"Frozen"
+    },
+    {
+        furnitureName: `Royal Bookshelf with Bell`,
+        price: 6400,
+        style: ["Rustic","Traditional"],
+        furnitureCategory: ["Study","Rec Room","Living Room","Decor"],
+        color: [],
+        universe:"Frozen"
+    },
+    {
+        furnitureName: `Royal Bookshelf with Greenery`,
+        price: 6400,
+        style: ["Rustic","Traditional"],
+        furnitureCategory: ["Study","Rec Room","Living Room","Decor"],
+        color: [],
+        universe:"Frozen"
+    },
+    {
+        furnitureName: `Snowflake Bed`,
+        price: 9200,
+        style: ["Bold","Elegant"],
+        furnitureCategory: ["Bedroom","Beds"],
+        color: [],
+        universe:"Frozen"
+    },
+    {
+        furnitureName: `Swing Painting`,
+        price: 3400,
+        style: ["Traditional","Elegant"],
+        furnitureCategory: ["Dining Room","Living Room","Study","Decor"],
+        color: [],
+        universe:"Frozen"
+    },
+    {
+        furnitureName: `Triangular Stained Glass Window`,
+        price: 8000,
+        style: ["Traditional","Elegant"],
+        furnitureCategory: ["Dining Room","Bathroom","Kitchen","Living Room","Windows"],
+        color: [],
+        universe:"Frozen"
+    },
+    {
+        furnitureName: `Vintage Phonograph`,
+        price: 6400,
+        style: ["Rustic","Traditional"],
+        furnitureCategory: ["Living Room","Rec Room","Study","Decor"],
+        color: [],
+        universe:"Frozen"
+    },
+    {
+        furnitureName: `Wine-Colored Loveseat`,
+        price: 2400,
+        obtainedBy: "Furniture pouch",
+        style: ["Traditional","Rustic"],
+        furnitureCategory: ["Rec Room","Study","Bedroom","Seating"],
+        color: [],
+        universe:"Frozen"
+    },
+    {
+        furnitureName: `Yule Goat`,
+        obtainedBy: "crafting",
+        style: [],
+        furnitureCategory: ["Decor"],
+        color: [],
+        universe:"Frozen",
+        added: 1.2
+    }
 ]
 
 module.exports=frozenFurniture
