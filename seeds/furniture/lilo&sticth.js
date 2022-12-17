a2fb235d78c486998aeb7a4497d48895c4a32368
killingsworth.kristen <killingsworth_kristen@yahoo.com>
2022-12-16 21:49:48 -0500

    wreck it ralph furniture seeded

--- seeds/furniture/lilo&sticth.js
+++ seeds/furniture/lilo&sticth.js
@@ -2,15 +2,312 @@
 // 34 pieces of furniture
 
 const liloStitchFurniture = [
-    // {
-    //     furnitureName: ``,
-    //     price: ,
-    //     obtainedBy: "Scrooge's Store",
-    //     style: [],
-    //     furnitureCategory: "",
-    //     color: [],
-    //     universe: "Lilo & Stitch"
-    // },
+    {
+        furnitureName: ``,
+        price: ,
+        obtainedBy: "Scrooge's Store",
+        style: [],
+        furnitureCategory: "",
+        color: [],
+        universe: "Lilo & Stitch"
+    },
+    {
+        furnitureName: ``,
+        price: ,
+        obtainedBy: "Scrooge's Store",
+        style: [],
+        furnitureCategory: "",
+        color: [],
+        universe: "Lilo & Stitch"
+    },
+    {
+        furnitureName: ``,
+        price: ,
+        obtainedBy: "Scrooge's Store",
+        style: [],
+        furnitureCategory: "",
+        color: [],
+        universe: "Lilo & Stitch"
+    },
+    {
+        furnitureName: ``,
+        price: ,
+        obtainedBy: "Scrooge's Store",
+        style: [],
+        furnitureCategory: "",
+        color: [],
+        universe: "Lilo & Stitch"
+    },
+    {
+        furnitureName: ``,
+        price: ,
+        obtainedBy: "Scrooge's Store",
+        style: [],
+        furnitureCategory: "",
+        color: [],
+        universe: "Lilo & Stitch"
+    },
+    {
+        furnitureName: ``,
+        price: ,
+        obtainedBy: "Scrooge's Store",
+        style: [],
+        furnitureCategory: "",
+        color: [],
+        universe: "Lilo & Stitch"
+    },
+    {
+        furnitureName: ``,
+        price: ,
+        obtainedBy: "Scrooge's Store",
+        style: [],
+        furnitureCategory: "",
+        color: [],
+        universe: "Lilo & Stitch"
+    },
+    {
+        furnitureName: ``,
+        price: ,
+        obtainedBy: "Scrooge's Store",
+        style: [],
+        furnitureCategory: "",
+        color: [],
+        universe: "Lilo & Stitch"
+    },
+    {
+        furnitureName: ``,
+        price: ,
+        obtainedBy: "Scrooge's Store",
+        style: [],
+        furnitureCategory: "",
+        color: [],
+        universe: "Lilo & Stitch"
+    },
+    {
+        furnitureName: ``,
+        price: ,
+        obtainedBy: "Scrooge's Store",
+        style: [],
+        furnitureCategory: "",
+        color: [],
+        universe: "Lilo & Stitch"
+    },
+    {
+        furnitureName: ``,
+        price: ,
+        obtainedBy: "Scrooge's Store",
+        style: [],
+        furnitureCategory: "",
+        color: [],
+        universe: "Lilo & Stitch"
+    },
+    {
+        furnitureName: ``,
+        price: ,
+        obtainedBy: "Scrooge's Store",
+        style: [],
+        furnitureCategory: "",
+        color: [],
+        universe: "Lilo & Stitch"
+    },
+    {
+        furnitureName: ``,
+        price: ,
+        obtainedBy: "Scrooge's Store",
+        style: [],
+        furnitureCategory: "",
+        color: [],
+        universe: "Lilo & Stitch"
+    },
+    {
+        furnitureName: ``,
+        price: ,
+        obtainedBy: "Scrooge's Store",
+        style: [],
+        furnitureCategory: "",
+        color: [],
+        universe: "Lilo & Stitch"
+    },
+    {
+        furnitureName: ``,
+        price: ,
+        obtainedBy: "Scrooge's Store",
+        style: [],
+        furnitureCategory: "",
+        color: [],
+        universe: "Lilo & Stitch"
+    },
+    {
+        furnitureName: ``,
+        price: ,
+        obtainedBy: "Scrooge's Store",
+        style: [],
+        furnitureCategory: "",
+        color: [],
+        universe: "Lilo & Stitch"
+    },
+    {
+        furnitureName: ``,
+        price: ,
+        obtainedBy: "Scrooge's Store",
+        style: [],
+        furnitureCategory: "",
+        color: [],
+        universe: "Lilo & Stitch"
+    },
+    {
+        furnitureName: ``,
+        price: ,
+        obtainedBy: "Scrooge's Store",
+        style: [],
+        furnitureCategory: "",
+        color: [],
+        universe: "Lilo & Stitch"
+    },
+    {
+        furnitureName: ``,
+        price: ,
+        obtainedBy: "Scrooge's Store",
+        style: [],
+        furnitureCategory: "",
+        color: [],
+        universe: "Lilo & Stitch"
+    },
+    {
+        furnitureName: ``,
+        price: ,
+        obtainedBy: "Scrooge's Store",
+        style: [],
+        furnitureCategory: "",
+        color: [],
+        universe: "Lilo & Stitch"
+    },
+    {
+        furnitureName: ``,
+        price: ,
+        obtainedBy: "Scrooge's Store",
+        style: [],
+        furnitureCategory: "",
+        color: [],
+        universe: "Lilo & Stitch"
+    },
+    {
+        furnitureName: ``,
+        price: ,
+        obtainedBy: "Scrooge's Store",
+        style: [],
+        furnitureCategory: "",
+        color: [],
+        universe: "Lilo & Stitch"
+    },
+    {
+        furnitureName: ``,
+        price: ,
+        obtainedBy: "Scrooge's Store",
+        style: [],
+        furnitureCategory: "",
+        color: [],
+        universe: "Lilo & Stitch"
+    },
+    {
+        furnitureName: ``,
+        price: ,
+        obtainedBy: "Scrooge's Store",
+        style: [],
+        furnitureCategory: "",
+        color: [],
+        universe: "Lilo & Stitch"
+    },
+    {
+        furnitureName: ``,
+        price: ,
+        obtainedBy: "Scrooge's Store",
+        style: [],
+        furnitureCategory: "",
+        color: [],
+        universe: "Lilo & Stitch"
+    },
+    {
+        furnitureName: ``,
+        price: ,
+        obtainedBy: "Scrooge's Store",
+        style: [],
+        furnitureCategory: "",
+        color: [],
+        universe: "Lilo & Stitch"
+    },
+    {
+        furnitureName: ``,
+        price: ,
+        obtainedBy: "Scrooge's Store",
+        style: [],
+        furnitureCategory: "",
+        color: [],
+        universe: "Lilo & Stitch"
+    },
+    {
+        furnitureName: ``,
+        price: ,
+        obtainedBy: "Scrooge's Store",
+        style: [],
+        furnitureCategory: "",
+        color: [],
+        universe: "Lilo & Stitch"
+    },
+    {
+        furnitureName: ``,
+        price: ,
+        obtainedBy: "Scrooge's Store",
+        style: [],
+        furnitureCategory: "",
+        color: [],
+        universe: "Lilo & Stitch"
+    },
+    {
+        furnitureName: ``,
+        price: ,
+        obtainedBy: "Scrooge's Store",
+        style: [],
+        furnitureCategory: "",
+        color: [],
+        universe: "Lilo & Stitch"
+    },
+    {
+        furnitureName: ``,
+        price: ,
+        obtainedBy: "Scrooge's Store",
+        style: [],
+        furnitureCategory: "",
+        color: [],
+        universe: "Lilo & Stitch"
+    },
+    {
+        furnitureName: ``,
+        price: ,
+        obtainedBy: "Scrooge's Store",
+        style: [],
+        furnitureCategory: "",
+        color: [],
+        universe: "Lilo & Stitch"
+    },
+    {
+        furnitureName: ``,
+        price: ,
+        obtainedBy: "Scrooge's Store",
+        style: [],
+        furnitureCategory: "",
+        color: [],
+        universe: "Lilo & Stitch"
+    },
+    {
+        furnitureName: ``,
+        price: ,
+        obtainedBy: "Scrooge's Store",
+        style: [],
+        furnitureCategory: "",
+        color: [],
+        universe: "Lilo & Stitch"
+    },
 ]
 
 module.exports=liloStitchFurniture
--- seeds/furniture/wreckItRalph.js
+++ seeds/furniture/wreckItRalph.js
@@ -2,16 +2,322 @@
 // 34 pieces of furniture
 
 const wreckItRaplhFurniture = [
-    // {
-    //     furnitureName: ``,
-    //     price: ,
-    //     obtainedBy: "Scrooge's Store",
-    //     style: [],
-    //     furnitureCategory: [""],
-    //     color: [],
-    //     tagId: [],
-    //     universe:"Wreck it Ralph"
-    // },
+    {
+        furnitureName: `Abstract Chandelier`,
+        price: 1020,
+        obtainedBy: "Scrooge's Store",
+        style: ["Professional","Modern","Quirky"],
+        furnitureCategory: ["Study","Bathroom","Dining Room","Lighting"],
+        color: [],
+        universe:"Wreck it Ralph"
+    },
+    {
+        furnitureName: `Abstract Neon Sign`,
+        price: 2720,
+        obtainedBy: "Scrooge's Store",
+        style: ["Modern","Casual"],
+        furnitureCategory: ["Rec Room","Bedroom","Living Room","Decor"],
+        color: [],
+        universe:"Wreck it Ralph"
+    },
+    {
+        furnitureName: `Accent Chair`,
+        price: 2400,
+        obtainedBy: "Scrooge's Store",
+        style: ["Professional","Modern","Quirky"],
+        furnitureCategory: ["Study","Living Room","Seating"],
+        color: [],
+        universe:"Wreck it Ralph"
+    },
+    {
+        furnitureName: `Air Hockey Table`,
+        price: 6400,
+        obtainedBy: "Scrooge's Store",
+        style: ["Modern","Casual"],
+        furnitureCategory: ["Rec Room","Decor"],
+        color: [],
+        universe:"Wreck it Ralph"
+    },
+    {
+        furnitureName: `Awesome Online Awards`,
+        price: 3200,
+        obtainedBy: "Scrooge's Store",
+        style: ["Professional","Modern","Quirky"],
+        furnitureCategory: ["Study","Bedroom","Rec Room","Decor"],
+        color: [],
+        universe:"Wreck it Ralph"
+    },
+    {
+        furnitureName: `Bamboo Shoots`,
+        price: 2880,
+        obtainedBy: "Scrooge's Store",
+        style: ["Modern"],
+        furnitureCategory: ["Living Room","Bathroom","Study","Decor"],
+        color: [],
+        universe:"Wreck it Ralph"
+    },
+    {
+        furnitureName: `Candy Bed`,
+        price: 9200,
+        obtainedBy: "Scrooge's Store",
+        style: ["Modern","Cute"],
+        furnitureCategory: ["Bedroom","Beds"],
+        color: [],
+        universe:"Wreck it Ralph"
+    },
+    {
+        furnitureName: `Coin-op Game`,
+        price: 210,
+        obtainedBy: "Scrooge's Store",
+        style: ["Modern","Casual"],
+        furnitureCategory: ["Rec Room","Decor"],
+        color: [],
+        universe:"Wreck it Ralph"
+    },
+    {
+        furnitureName: `Counter Seat`,
+        price: 900,
+        obtainedBy: "Scrooge's Store",
+        style: ["Modern"],
+        furnitureCategory: ["Living Room","Study","Seating"],
+        color: [],
+        universe:"Wreck it Ralph"
+    },
+    {
+        furnitureName: `Curvy Couch`,
+        price: 6000,
+        obtainedBy: "Scrooge's Store",
+        style: ["Professional","Modern","Quirky"],
+        furnitureCategory: ["Study","Living Room","Seating"],
+        color: [],
+        universe:"Wreck it Ralph"
+    },
+    {
+        furnitureName: `Desktop Computer`,
+        price: 288,
+        obtainedBy: "Scrooge's Store",
+        style: ["Modern","Cute"],
+        furnitureCategory: ["Bedroom","Study","Electronics"],
+        color: [],
+        universe:"Wreck it Ralph"
+    },
+    {
+        furnitureName: `Donut Chair`,
+        price: 4800,
+        obtainedBy: "Scrooge's Store",
+        style: ["Modern","Cute"],
+        furnitureCategory: ["Bedroom","Living Room","Rec Room","Seating"],
+        color: [],
+        universe:"Wreck it Ralph"
+    },
+    {
+        furnitureName: `Drink Cabinet`,
+        price: 3200,
+        obtainedBy: "Scrooge's Store",
+        style: ["Modern"],
+        furnitureCategory: ["Living Room","Study","Decor"],
+        color: [],
+        universe:"Wreck it Ralph"
+    },
+    {
+        furnitureName: `Exec Desk`,
+        price: 8800,
+        obtainedBy: "Scrooge's Store",
+        style: ["Professional","Modern","Quirky"],
+        furnitureCategory: ["Study","Tables"],
+        color: [],
+        universe:"Wreck it Ralph"
+    },
+    {
+        furnitureName: `Fancy Desk Chair`,
+        price: 2400,
+        obtainedBy: "Scrooge's Store",
+        style: ["Professional","Modern","Quirky"],
+        furnitureCategory: ["Study","Dining Room","Seating"],
+        color: [],
+        universe:"Wreck it Ralph"
+    },
+    {
+        furnitureName: `Food Stall Counter`,
+        price: 20000,
+        obtainedBy: "Scrooge's Store",
+        style: ["Modern","Casual"],
+        furnitureCategory: ["Rec Room","Kitchen","Decor"],
+        color: [],
+        universe:"Wreck it Ralph"
+    },
+    {
+        furnitureName: `Freestanding Fireplace`,
+        price: 6400,
+        obtainedBy: "Scrooge's Store",
+        style: ["Modern","Bold"],
+        furnitureCategory: ["Living Room","Study","Dining Room","Decor"],
+        color: [],
+        universe:"Wreck it Ralph"
+    },
+    {
+        furnitureName: `Game Machine`,
+        price: 16000,
+        obtainedBy: "Scrooge's Store",
+        style: [],
+        furnitureCategory: ["Decor"],
+        color: [],
+        universe:"Wreck it Ralph"
+    },
+    {
+        furnitureName: `Gamer Desk`,
+        price: 3520,
+        obtainedBy: "Scrooge's Store",
+        style: ["Modern","Cute"],
+        furnitureCategory: ["Bedroom","Study","Tables"],
+        color: [],
+        universe:"Wreck it Ralph"
+    },
+    {
+        furnitureName: `Geometric Coffee Table`,
+        price: 1320,
+        obtainedBy: "Scrooge's Store",
+        style: ["Professional","Modern","Quirky"],
+        furnitureCategory: ["Study","Living Room","Tables"],
+        color: [],
+        universe:"Wreck it Ralph"
+    },
+    {
+        furnitureName: `Glass Shelves`,
+        price: 3200,
+        obtainedBy: "Scrooge's Store",
+        style: ["Modern"],
+        furnitureCategory: ["Living Room","Study","Decor"],
+        color: [],
+        universe:"Wreck it Ralph"
+    },
+    {
+        furnitureName: `Holographic Bar Stool`,
+        price: 1800,
+        obtainedBy: "Scrooge's Store",
+        style: ["Modern","Casual"],
+        furnitureCategory: ["Rec Room","Kitchen","Seating"],
+        color: [],
+        universe:"Wreck it Ralph"
+    },
+    {
+        furnitureName: `Indoor Vertical Fountain`,
+        price: 6400,
+        obtainedBy: "Scrooge's Store",
+        style: ["Bold","Modern"],
+        furnitureCategory: ["Bathroom","Living Room","Study","Decor"],
+        color: [],
+        universe:"Wreck it Ralph"
+    },
+    {
+        furnitureName: `L-Counter`,
+        price: 4400,
+        obtainedBy: "Scrooge's Store",
+        style: ["Modern"],
+        furnitureCategory: ["Living Room","Study","Tables"],
+        color: [],
+        universe:"Wreck it Ralph"
+    },
+    {
+        furnitureName: `Modern Wall Sconce`,
+        price: 765,
+        obtainedBy: "Scrooge's Store",
+        style: ["Modern"],
+        furnitureCategory: ["Living Room","Study","Dining Room","Lighting"],
+        color: [],
+        universe:"Wreck it Ralph"
+    },
+    {
+        furnitureName: `Nightlight`,
+        price: 2040,
+        obtainedBy: "Scrooge's Store",
+        style: ["Modern","Cute"],
+        furnitureCategory: ["Bedroom","Lighting"],
+        color: [],
+        universe:"Wreck it Ralph"
+    },
+    {
+        furnitureName: `Octagonal Mirror`,
+        price: 3200,
+        obtainedBy: "Scrooge's Store",
+        style: ["Professional","Modern","Quirky"],
+        furnitureCategory: ["Study","Bathroom","Bedroom","Decor"],
+        color: [],
+        universe:"Wreck it Ralph"
+    },
+    {
+        furnitureName: `Octagonal Side Table`,
+        price: 3520,
+        obtainedBy: "Scrooge's Store",
+        style: ["Prfessional","Modern","Quirky"],
+        furnitureCategory: ["Study","Living Room","Bedroom","Tables"],
+        color: [],
+        universe:"Wreck it Ralph"
+    },
+    {
+        furnitureName: `Pink Sweet Tooth Rug`,
+        price: 1200,
+        obtainedBy: "Scrooge's Store",
+        style: ["Cute","Casual"],
+        furnitureCategory: ["Bathroom","Bedroom","Study","Rugs"],
+        color: [],
+        universe:"Wreck it Ralph"
+    },
+    {
+        furnitureName: `Pixelated Window`,
+        price: 3200,
+        obtainedBy: "Scrooge's Store",
+        style: ["Modern"],
+        furnitureCategory: ["Living Room","Study","Bedroom","Windows"],
+        color: [],
+        universe:"Wreck it Ralph"
+    },
+    {
+        furnitureName: `Shelving Unit`,
+        price: 3200,
+        obtainedBy: "Scrooge's Store",
+        style: ["Modern"],
+        furnitureCategory: ["Living Room","Study","Bedroom","Decor"],
+        color: [],
+        universe:"Wreck it Ralph"
+    },
+    {
+        furnitureName: `Sugar Rush Plushie Shelf`,
+        price: 6400,
+        obtainedBy: "Scrooge's Store",
+        style: ["Modern","Cute"],
+        furnitureCategory: ["Bedroom","Decor","Wall"],
+        color: [],
+        universe:"Wreck it Ralph"
+    },
+    {
+        furnitureName: `Vanellope Portraits`,
+        price: 2720,
+        obtainedBy: "Scrooge's Store",
+        style: ["Modern"],
+        furnitureCategory: ["Living Room","Study","Dining Room","Decor"],
+        color: [],
+        universe:"Wreck it Ralph"
+    },
+    {
+        furnitureName: `Virtual Desktop`,
+        price: 5760,
+        obtainedBy: "Scrooge's Store",
+        style: ["Modern","Professional","Quirky"],
+        furnitureCategory: ["Study","Bedroom","Decor"],
+        color: [],
+        universe:"Wreck it Ralph"
+    },
+    // says decor on wiki
+    {
+        furnitureName: `Winner's Gamer Chair`,
+        price: 4800,
+        obtainedBy: "Scrooge's Store",
+        style: ["Modern","Cute"],
+        furnitureCategory: ["Bedroom","Study","Seating"],
+        color: [],
+        universe:"Wreck it Ralph"
+    },
 ]
 
 module.exports=wreckItRaplhFurniture
