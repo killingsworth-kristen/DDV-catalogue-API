37c81ff5c0c33907f0e7f0b54cd1a27051b80e99
killingsworth.kristen <killingsworth_kristen@yahoo.com>
2022-12-20 01:02:48 -0500

    beauty and beast furniture seeds

--- seeds/furniture/beauty&beast.js
+++ seeds/furniture/beauty&beast.js
@@ -2,15 +2,404 @@
 // 41 pieces of furniture
 
 const beatuyBeastFurniture = [
-    // {
-    //     furnitureName: ``,
-    //     price: ,
-    //     style: [],
-    //     furnitureCategory: "",
-    //     color: [],
-    //     universe: "Beauty & the Beast",
-    //     added: 1.0
-    // },
+    {
+        furnitureName: `Black and Gold Angelic Harp`,
+        price: 1000,
+        style: ["Traditional","Elegant"],
+        furnitureCategory: ["Study","Rec Room","Decor"],
+        color: [],
+        universe: "Beauty & the Beast",
+        added: 1.0,
+        updated: 1.1
+    },
+    {
+        furnitureName: `Bridge Painting`,
+        price: 1020,
+        style: ["Traditional","Elegant"],
+        furnitureCategory: ["Study","Living Room","Dining Room","Decor"],
+        color: [],
+        universe: "Beauty & the Beast",
+        added: 1.0
+    },
+    {
+        furnitureName: `Bronze and Gold Angelic Harp`,
+        price: 1000,
+        style: ["Traditional","Elegant"],
+        furnitureCategory: ["Study","Rec Room"],
+        color: [],
+        universe: "Beauty & the Beast",
+        added: 1.0,
+        updated: 1.1
+    },
+    {
+        furnitureName: `Coat Hanger`,
+        price: 3200,
+        style: ["Traditional","Elegant"],
+        furnitureCategory: ["Study","Living Room","Bedroom","Decor"],
+        color: [],
+        universe: "Beauty & the Beast",
+        added: 1.0
+    },
+    {
+        furnitureName: `Cushioned Seat`,
+        price: 2400,
+        style: ["Traditional","Elegant"],
+        furnitureCategory: ["Outdoors","Kitchen","Dining Room","Seating"],
+        color: [],
+        universe: "Beauty & the Beast",
+        added: 1.0
+    },
+    {
+        furnitureName: `Decorative Shield`,
+        price: 1200,
+        style: ["Traditional","Elegant"],
+        furnitureCategory: ["Dining Room","Study","Rec Room","Decor"],
+        color: [],
+        universe: "Beauty & the Beast",
+        added: 1.0
+    },
+    {
+        furnitureName: `Elegant Armchair`,
+        price: 900,
+        style: ["Traditional","Elegant"],
+        furnitureCategory: ["Living Room","Study","Seating"],
+        color: [],
+        universe: "Beauty & the Beast",
+        added: 1.0,
+        updated: 1.1
+    },
+    {
+        furnitureName: `Elegant Nightstand`,
+        price: 3520,
+        style: ["Traditional","Elegant","Professional"],
+        furnitureCategory: ["Study","Living Room","Bedroom","Tables"],
+        color: [],
+        universe: "Beauty & the Beast",
+        added: 1.0
+    },
+    {
+        furnitureName: `Engraved Round Table`,
+        price: 3520,
+        style: ["Traditional","Elegant"],
+        furnitureCategory: ["Kitchen","Dining Room","Outdoor","Tables"],
+        color: [],
+        universe: "Beauty & the Beast",
+        added: 1.0
+    },
+    {
+        furnitureName: `Fancy Dining Chair`,
+        price: 2400,
+        style: ["Traditional","Elegant"],
+        furnitureCategory: ["Dining Room","Study","Seating"],
+        color: [],
+        universe: "Beauty & the Beast",
+        added: 1.0
+    },
+    {
+        furnitureName: `Gaston's Antler CHair`,
+        price: 12000,
+        style: ["Bold","Rustic"],
+        furnitureCategory: ["Living Room","Study","Seating"],
+        color: [],
+        universe: "Beauty & the Beast",
+        added: 1.0
+    },
+    {
+        furnitureName: `Globe`,
+        price: 6400,
+        style: ["Traditional","Elegant"],
+        furnitureCategory: ["Study","Rec Room","Decor"],
+        color: [],
+        universe: "Beauty & the Beast",
+        added: 1.0
+    },
+    {
+        furnitureName: `Golden Bathtub`,
+        // price: ,
+        style: [],
+        furnitureCategory: ["Bathroom"],
+        color: [],
+        universe: "Beauty & the Beast",
+        added: 1.0
+    },
+    {
+        furnitureName: `Golden Bed`,
+        price: 23000,
+        style: ["Traditional","Elegant"],
+        furnitureCategory: ["Bedroom","Beds"],
+        color: [],
+        universe: "Beauty & the Beast",
+        added: 1.0
+    },
+    {
+        furnitureName: `Golden Vanity`,
+        price: 17600,
+        style: ["Traditional","Elegant"],
+        furnitureCategory: ["Bedroom","Bathroom","Tables"],
+        color: [],
+        universe: "Beauty & the Beast",
+        added: 1.0
+    },
+    {
+        furnitureName: `Grand Double Doors`,
+        price: 3200,
+        style: ["Traditional","Elegant"],
+        furnitureCategory: ["Dining Room","Bedroom","Study","Decor"],
+        color: [],
+        universe: "Beauty & the Beast",
+        added: 1.0
+    },
+    {
+        furnitureName: `Green and Bronze Angelic Harp`,
+        price: 1000,
+        style: ["Traditional","Elegant"],
+        furnitureCategory: ["Study",'Rec Room',"Decor"],
+        color: [],
+        universe: "Beauty & the Beast",
+        added: 1.0,
+        updated: 1.1
+    },
+    {
+        furnitureName: `Large Coffee Table`,
+        price: 3520,
+        style: ["Traditional","Elegant"],
+        furnitureCategory: ["Living Room","Study","Tables"],
+        color: [],
+        universe: "Beauty & the Beast",
+        added: 1.0
+    },
+    {
+        furnitureName: `Lion Statue`,
+        price: 6800,
+        style: ["Traditional","Elegant"],
+        furnitureCategory: ["Outdoors","Study","Decor"],
+        color: [],
+        universe: "Beauty & the Beast",
+        added: 1.0
+    },
+    {
+        furnitureName: `Loveseat`,
+        price: 6000,
+        style: ["Traditional","Elegant"],
+        furnitureCategory: ["Bedroom","Living Room","Study","Seating"],
+        color: [],
+        universe: "Beauty & the Beast",
+        added: 1.0
+    },
+    {
+        furnitureName: `Mohogany Table`,
+        price: 4400,
+        style: ["Traditional","Elegant"],
+        furnitureCategory: ["Dining ROom","Tables"],
+        color: [],
+        universe: "Beauty & the Beast",
+        added: 1.0
+    },
+    {
+        furnitureName: `Monumental Marble Vase`,
+        price: 1020,
+        style: ["Traditional","Elegant"],
+        furnitureCategory: ["Study","Dining Room","Bathroom","Decor"],
+        color: [],
+        universe: "Beauty & the Beast",
+        added: 1.0
+    },
+    {
+        furnitureName: `Painiting Arrangement`,
+        price: 2720,
+        style: ["Traditional","Elegant"],
+        furnitureCategory: ["Living Room","Dining Room","Study","Decor"],
+        color: [],
+        universe: "Beauty & the Beast",
+        added: 1.0
+    },
+    {
+        furnitureName: `Picnic Blanket`,
+        price: 8000,
+        style: ["Traditional","Elegant"],
+        furnitureCategory: ["Outdoors","Decor"],
+        color: [],
+        universe: "Beauty & the Beast",
+        added: 1.0,
+        updated: 1.1
+    },
+    {
+        furnitureName: `Quill and Ink`,
+        price: 2400,
+        style: ["Traditional","Elegant"],
+        furnitureCategory: ["Study","Decor"],
+        color: [],
+        universe: "Beauty & the Beast",
+        added: 1.0
+    },
+    {
+        furnitureName: `Red and Gold Angelic Harp`,
+        price: 1000,
+        style: ["Traditional","Elegant"],
+        furnitureCategory: ["Study","Rec Room","Decor"],
+        color: [],
+        universe: "Beauty & the Beast",
+        added: 1.0,
+        updated: 1.1
+    },
+    {
+        furnitureName: `Romantic Painting`,
+        price: 1020,
+        style: ["Traditional","Elegant"],
+        furnitureCategory: ["Living Room","Dining Room","Study","Decor"],
+        color: [],
+        universe: "Beauty & the Beast",
+        added: 1.0
+    },
+    {
+        furnitureName: `Room Divider`,
+        price: 6400,
+        style: ["Traditional","Elegant"],
+        furnitureCategory: ["Bedroom","Study","Bathroom","Decor"],
+        color: [],
+        universe: "Beauty & the Beast",
+        added: 1.0
+    },
+    {
+        furnitureName: `Rose Four-Poster Bed`,
+        price: 9200,
+        style: ["Traditional","Elegant"],
+        furnitureCategory: ["Bedroom","Beds"],
+        color: [],
+        universe: "Beauty & the Beast",
+        added: 1.0,
+        updated: 1.2
+    },
+    {
+        furnitureName: `Row of Books`,
+        price: 1200,
+        style: ["Traditional","Professional"],
+        furnitureCategory: ["Study","Bedroom","Decor"],
+        color: [],
+        universe: "Beauty & the Beast",
+        added: 1.0
+    },
+    {
+        furnitureName: `Scenic Painting`,
+        price: 1020,
+        style: ["Traditional","Elegant"],
+        furnitureCategory: ["Living Room","Dining Room","Study","Decor"],
+        color: [],
+        universe: "Beauty & the Beast",
+        added: 1.0
+    },
+    {
+        furnitureName: `Stained Glass Lamp`,
+        price: 4080,
+        style: ["Traditional","Elegant"],
+        furnitureCategory: ["Bedroom","Living Room","Study","Lighting"],
+        color: [],
+        universe: "Beauty & the Beast",
+        added: 1.0
+    },
+    {
+        furnitureName: `Stained Glass Window`,
+        price: 16000,
+        style: ["Traditional","Elegant"],
+        furnitureCategory: ["Living Room","Study","Bedroom","Windows"],
+        color: [],
+        universe: "Beauty & the Beast",
+        added: 1.0
+    },
+    {
+        furnitureName: `Tall Arched Window`,
+        price: 3200,
+        style: ["Traditional","Elegant"],
+        furnitureCategory: ["Living Room","Study","Dining Room","Windows"],
+        color: [],
+        universe: "Beauty & the Beast",
+        added: 1.0
+    },
+    {
+        furnitureName: `Tea Trolley`,
+        price: 200,
+        style: ["Traditional","Elegant"],
+        furnitureCategory: ["Kitchen","Living Room","Dining Room","Study","Decor"],
+        color: [],
+        universe: "Beauty & the Beast",
+        added: 1.0
+    },
+    {
+        furnitureName: `Tierred Fountaint`,
+        price: 8000,
+        style: ["Traditional","Elegant"],
+        furnitureCategory: ["Outdoors","Decor"],
+        color: [],
+        universe: "Beauty & the Beast",
+        added: 1.0,
+        updated: 1.1
+    },
+    {
+        furnitureName: `Topiary Square`,
+        price: 8640,
+        style: ["Traditional","Elegant"],
+        furnitureCategory: ["Outdoors","Decor"],
+        color: [],
+        universe: "Beauty & the Beast",
+        added: 1.0
+    },
+    {
+        furnitureName: `Tufted Sofa`,
+        price: 3000,
+        style: ["Traditional","Elegant"],
+        furnitureCategory: ["Living Room","Study","Seating"],
+        color: [],
+        universe: "Beauty & the Beast",
+        added: 1.0,
+        updated: 1.1
+    },
+    {
+        furnitureName: `Velvet Ottoman`,
+        price: 2400,
+        style: ["Traditional","Cute"],
+        furnitureCategory: ["Living Room","Bedroom","Study","Seating"],
+        color: [],
+        universe: "Beauty & the Beast",
+        added: 1.0
+    },
+    {
+        furnitureName: `Wall-Mounted Candelabra`,
+        price: 2720,
+        style: ["Traditional","Elegant"],
+        furnitureCategory: ["Dining Room","Living Room","Lighting"],
+        color: [],
+        universe: "Beauty & the Beast",
+        added: 1.0,
+        updated: 1.1
+    },
+    {
+        furnitureName: `White and GOld Angelic Harp`,
+        price: 1000,
+        style: ["Traditional","Elegant"],
+        furnitureCategory: ["Study","Rec Room","Decor"],
+        color: [],
+        universe: "Beauty & the Beast",
+        added: 1.0,
+        updated: 1.1
+    },
+    {
+        furnitureName: `Woven Tapestry`,
+        price: 6800,
+        style: ["Traditional","Elegant"],
+        furnitureCategory: ["Study","Living Room","Decor"],
+        color: [],
+        universe: "Beauty & the Beast",
+        added: 1.0
+    },
+    {
+        furnitureName: `Writing Desk`,
+        price: 8800,
+        style: ["Traditional","Elegant"],
+        furnitureCategory: ["Study","Bedroom","Tables"],
+        color: [],
+        universe: "Beauty & the Beast",
+        added: 1.0
+    },
 
 ]
 
