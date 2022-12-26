7aa11742af26c3b134d0947222eea15a46dfdaa5
killingsworth.kristen <killingsworth_kristen@yahoo.com>
2022-12-25 21:03:05 -0500

    Images added to Lightyear furniture

--- seeds/furniture/lightyear.js
+++ seeds/furniture/lightyear.js
@@ -3,6 +3,7 @@ const lightyearFurniture = [
     {
         furnitureName: `Space Ranger Suit Display`,
         price: 431,
+        image: "https://dreamlightvalleywiki.com/images/thumb/f/fe/Space_Ranger_Suit_Display.png/180px-Space_Ranger_Suit_Display.png",
         obtainedBy: "Pixar Fest Star Path",
         style: ["Bold","Futuristic","Professional"],
         furnitureCategory: ["Rec Room","Study"],
@@ -13,6 +14,7 @@ const lightyearFurniture = [
     {
         furnitureName: `XL-15 Spaceship Model`,
         price: 408,
+        image: "https://dreamlightvalleywiki.com/images/thumb/2/20/XL-15_Spaceship_Model.png/180px-XL-15_Spaceship_Model.png",
         obtainedBy: "Pixar Fest Star Path",
         style: ["Bold","Futuristic"],
         furnitureCategory: ["Bedroom","Study"],
