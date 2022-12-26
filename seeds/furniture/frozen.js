62670e5ba6b912f2daff6fca02fc9e0122613db7
killingsworth.kristen <killingsworth_kristen@yahoo.com>
2022-12-25 21:10:29 -0500

    images added tto frozen furniture

--- seeds/furniture/frozen.js
+++ seeds/furniture/frozen.js
@@ -5,6 +5,7 @@ const frozenFurniture = [
     {
         furnitureName: `"Spring Reborn" Vertical Tapestry`,
         price: 2720,
+        image: "https://dreamlightvalleywiki.com/images/thumb/3/3e/%22Spring_Reborn%22_Vertical_Tapestry.png/61px-%22Spring_Reborn%22_Vertical_Tapestry.png",
         style: ["Rustic","Traditional"],
         furnitureCategory: ["Living Room","Rec Room","Study","Decor"],
         color: [],
@@ -14,6 +15,7 @@ const frozenFurniture = [
     {
         furnitureName: `Anna Statue`,
         // price: 1,
+        image: "https://dreamlightvalleywiki.com/images/thumb/5/5f/Anna_Statue.png/62px-Anna_Statue.png",
         obtainedBy: "Frozen Memories (Kristoff Level 3)",
         tracked: false,
         style: ["Elegant"],
@@ -25,6 +27,7 @@ const frozenFurniture = [
     {
         furnitureName: `Anna's Picnic Set`,
         price: 8000,
+        image: "https://dreamlightvalleywiki.com/images/thumb/7/76/Anna%27s_Picnic_Set.png/180px-Anna%27s_Picnic_Set.png",
         style: ["Traditional","Rustic","Casual"],
         furnitureCategory: ["Outdoors","Decor"],
         color: [],
@@ -34,6 +37,7 @@ const frozenFurniture = [
     {
         furnitureName: `Banner`,
         price: 3200,
+        image: "https://dreamlightvalleywiki.com/images/thumb/8/87/Banner.png/107px-Banner.png",
         obtainedBy: "Anna Friendship Level 8",
         style: ["Traditional","Elegant"],
         furnitureCategory: ["Dining Room","Living Room","Study","Decor"],
@@ -44,6 +48,7 @@ const frozenFurniture = [
     {
         furnitureName: `Birch L-shaped Bench`,
         price: 3600,
+        image: "https://dreamlightvalleywiki.com/images/a/a8/Birch_L-shaped_Bench.png",
         style: ["Elegant","Rustic"],
         furnitureCategory: ["Rec Room","Bathroom","Seating"],
         color: [],
@@ -53,6 +58,7 @@ const frozenFurniture = [
     {
         furnitureName: `Birch Toiletry Basket`,
         price: 1200,
+        image: "https://dreamlightvalleywiki.com/images/thumb/0/0c/Birch_Toiletry_Basket.png/180px-Birch_Toiletry_Basket.png",
         style: ["Elegant","Rustic"],
         furnitureCategory: ["Rec Room","Bathroom","Decor"],
         color: [],
@@ -62,6 +68,7 @@ const frozenFurniture = [
     {
         furnitureName: `Birch Tub`,
         price: 20000,
+        image: "https://dreamlightvalleywiki.com/images/4/48/Birch_Tub.png",
         style: ["Elegant","Rustic"],
         furnitureCategory: ["Rec Room","Bathroom","Decor"],
         color: [],
@@ -71,6 +78,7 @@ const frozenFurniture = [
     {
         furnitureName: `Birch Water Bucket`,
         price: 360,
+        image: "https://dreamlightvalleywiki.com/images/4/47/Birch_Water_Bucket.png",
         style: ["Elegant","Rustic"],
         furnitureCategory: ["Rec Room","Bathroom","Decor"],
         color: [],
@@ -80,6 +88,7 @@ const frozenFurniture = [
     {
         furnitureName: `Blue Robe Hanger`,
         price: 6400,
+        image: "https://dreamlightvalleywiki.com/images/thumb/0/0e/Blue_Robe_Hanger.png/180px-Blue_Robe_Hanger.png",
         style: ["Elegant","Rustic"],
         furnitureCategory: ["Rec Room","Bathroom","Decor"],
         color: [],
@@ -89,6 +98,7 @@ const frozenFurniture = [
     {
         furnitureName: `Blue Robe Shelf`,
         price: 6400,
+        image: "https://dreamlightvalleywiki.com/images/thumb/8/86/Blue_Robe_Shelf.png/107px-Blue_Robe_Shelf.png",
         style: ["Elegant","Rustic"],
         furnitureCategory: ["Rec Room","Bathroom","Decor"],
         color: [],
@@ -98,6 +108,7 @@ const frozenFurniture = [
     {
         furnitureName: `Candle`,
         price: 306,
+        image: "https://dreamlightvalleywiki.com/images/thumb/3/30/Candle.png/63px-Candle.png",
         style: ["Rustic","Traditional"],
         furnitureCategory: ["Living Room","Study","Decor"],
         color: [],
@@ -107,6 +118,7 @@ const frozenFurniture = [
     {
         furnitureName: `Chandelier`,
         price: 5440,
+        image: "https://dreamlightvalleywiki.com/images/thumb/4/40/Chandelier.png/180px-Chandelier.png",
         style: ["Traditional","Elegant"],
         furnitureCategory: ["Dining Room","Living Room","Lighting"],
         color: [],
@@ -116,6 +128,7 @@ const frozenFurniture = [
     {
         furnitureName: `Chilled Chair`,
         price: 4800,
+        image: "https://dreamlightvalleywiki.com/images/thumb/9/97/Chilled_Chair.png/100px-Chilled_Chair.png",
         style: ["Bold","Elegant"],
         furnitureCategory: ["Dining Room","Study","Seating"],
         color: [],
@@ -125,6 +138,7 @@ const frozenFurniture = [
     {
         furnitureName: `Four-Poster Bed`,
         price: 3680,
+        image: "https://dreamlightvalleywiki.com/images/thumb/7/74/Four-Poster_Bed.png/180px-Four-Poster_Bed.png",
         obtainedBy: "Anna Friendship Level 5",
         style: ["Traditional","Elegant"],
         furnitureCategory: ["Bedroom","Beds"],
@@ -135,6 +149,7 @@ const frozenFurniture = [
     {
         furnitureName: `Frost Chandelier`,
         price: 5440,
+        image: "https://dreamlightvalleywiki.com/images/thumb/7/73/Frost_Chandelier.png/118px-Frost_Chandelier.png",
         style: ["Bold","Elegant"],
         furnitureCategory: ["Bedroom","Dining Room","Living Room","Decor"],
         color: [],
@@ -144,6 +159,7 @@ const frozenFurniture = [
     {
         furnitureName: `Frozen Portal`,
         price: 6400,
+        image: "https://dreamlightvalleywiki.com/images/thumb/6/6b/Frozen_Portal.png/96px-Frozen_Portal.png",
         style: ["Bold","Elegant"],
         furnitureCategory: ["Bedroom","Dining Room","Living Room","Decor"],
         color: [],
@@ -153,6 +169,7 @@ const frozenFurniture = [
     {
         furnitureName: `Gallant Painting`,
         // price: ,
+        image: "https://dreamlightvalleywiki.com/images/thumb/4/43/Gallant_Painting.png/146px-Gallant_Painting.png",
         style: ["Traditional","Elegant"],
         furnitureCategory: ["Dining Room","Living Room","Study","Decor"],
         color: [],
@@ -162,6 +179,7 @@ const frozenFurniture = [
     {
         furnitureName: `Gold Hot Steam Rocks`,
         price: 3200,
+        image: "https://dreamlightvalleywiki.com/images/thumb/3/37/Gold_Hot_Steam_Rocks.png/180px-Gold_Hot_Steam_Rocks.png",
         style: ["Elegant","Rustic"],
         furnitureCategory: ["Bathroom","Rec Room","Decor"],
         color: [],
@@ -171,6 +189,7 @@ const frozenFurniture = [
     {
         furnitureName: `Grandfather Clock`,
         price: 6400,
+        image: "https://dreamlightvalleywiki.com/images/thumb/3/34/Grandfather_Clock.png/66px-Grandfather_Clock.png",
         style: ["Traditional","Elegant"],
         furnitureCategory: ["Dining Room","Study","Living Room","Decor"],
         color: [],
@@ -180,6 +199,7 @@ const frozenFurniture = [
     {
         furnitureName: `Green Window to the Mountains`,
         price: 20000,
+        image: "https://dreamlightvalleywiki.com/images/thumb/8/8f/Green_Window_to_the_Mountains.png/180px-Green_Window_to_the_Mountains.png",
         style: ["Elegant","Rustic"],
         furnitureCategory: ["Rec Room","Bathroom","Windows"],
         color: [],
@@ -189,6 +209,7 @@ const frozenFurniture = [
     {
         furnitureName: `Hot Cocoa Stand`,
         price: 480,
+        image: "https://dreamlightvalleywiki.com/images/thumb/0/01/Hot_Cocoa_Stand.png/117px-Hot_Cocoa_Stand.png",
         obtainedBy:"Festive Star Path",
         style: ["Tradtional","Rustic"],
         furnitureCategory: ["Outdoors","Decor"],
@@ -199,6 +220,7 @@ const frozenFurniture = [
     {
         furnitureName: `Ice Castle Vertical Tapestry`,
         price: 2720,
+        image: "https://dreamlightvalleywiki.com/images/thumb/d/df/Ice_Castle_Vertical_Tapestry.png/61px-Ice_Castle_Vertical_Tapestry.png",
         style: ["Rustic","Traditional"],
         furnitureCategory: ["Living Room","Rec Room","Study","Decor"],
         color: [],
@@ -208,6 +230,7 @@ const frozenFurniture = [
     {
         furnitureName: `Ice Glazed Couch`,
         price: 6000,
+        image: "https://dreamlightvalleywiki.com/images/thumb/0/0d/Ice_Glazed_Couch.png/180px-Ice_Glazed_Couch.png",
         style: ["Bold","Elegant"],
         furnitureCategory: ["Bedroom","Living Room","Dining Room","Seating"],
         color: [],
@@ -217,6 +240,7 @@ const frozenFurniture = [
     {
         furnitureName: `Ice Throne`,
         price: 210,
+        image: "https://dreamlightvalleywiki.com/images/thumb/c/c9/Ice_Throne.png/180px-Ice_Throne.png",
         obtainedBy: "Elsa Friendship Level 8",
         style: ["Bold","Elegant"],
         furnitureCategory: ["Study","Rec Room","Outdoors","Seating"],
@@ -227,6 +251,7 @@ const frozenFurniture = [
     {
         furnitureName: `Icicle Dining Table`,
         price: 4400,
+        image: "https://dreamlightvalleywiki.com/images/thumb/f/f7/Icicle_Dining_Table.png/180px-Icicle_Dining_Table.png",
         style: ["Bold","Elegant"],
         furnitureCategory: ["Dining Room","Tables"],
         color: [],
@@ -236,6 +261,7 @@ const frozenFurniture = [
     {
         furnitureName: `Icy Fountain`,
         price: 8000,
+        image: "https://dreamlightvalleywiki.com/images/thumb/2/2b/Icy_Fountain.png/150px-Icy_Fountain.png",
         style: ["Bold","Elegant"],
         furnitureCategory: ["Rec Room","Bathroom","Decor"],
         color: [],
@@ -245,6 +271,7 @@ const frozenFurniture = [
     {
         furnitureName: `Icy Window`,
         price: 3200,
+        image: "https://dreamlightvalleywiki.com/images/thumb/b/b4/Icy_Window.png/59px-Icy_Window.png",
         style: ["Rustic","Traditional"],
         furnitureCategory: ["Kitchen","Dining Room","Living Room","Windows"],
         color: [],
@@ -254,6 +281,7 @@ const frozenFurniture = [
     {
         furnitureName: `Kristoff's Sled`,
         price: 20000,
+        image: "https://dreamlightvalleywiki.com/images/thumb/2/22/Kristoff%27s_Sled.png/180px-Kristoff%27s_Sled.png",
         obtainedBy: "A Broken Sled (Kristoff Level 10)",
         style: ["Traditional","Rustic"],
         furnitureCategory: ["Outdoors","Decor"],
@@ -264,6 +292,7 @@ const frozenFurniture = [
     {
         furnitureName: `Library Side Table`,
         price: 1320,
+        image: "https://dreamlightvalleywiki.com/images/thumb/6/64/Library_Side_Table.png/180px-Library_Side_Table.png",
         style: ["Rustic","Traditional"],
         furnitureCategory: ["Living Room","Study","Tables"],
         color: [],
@@ -273,6 +302,7 @@ const frozenFurniture = [
     {
         furnitureName: `Long Dining Table`,
         price: 8800,
+        image: "https://dreamlightvalleywiki.com/images/thumb/1/14/Long_Dining_Table.png/180px-Long_Dining_Table.png",
         style: ["Traditional","Elegant"],
         furnitureCategory: ["Dining Room","Tables"],
         color: [],
@@ -282,6 +312,7 @@ const frozenFurniture = [
     {
         furnitureName: `Miniature Snow Castle`,
         price: 2720,
+        image: "https://dreamlightvalleywiki.com/images/thumb/7/70/Miniature_Snow_Castle.png/180px-Miniature_Snow_Castle.png",
         style: ["Elegant","Quirky"],
         furnitureCategory: ["Outdoors","Decor"],
         color: [],
@@ -291,6 +322,7 @@ const frozenFurniture = [
     {
         furnitureName: `Norwegian Spruce L-shaped Bench`,
         price: 3600,
+        image: "https://dreamlightvalleywiki.com/images/2/25/Norwegian_Spruce_L-shaped_Bench.png",
         style: ["Elegant","Rustic"],
         furnitureCategory: ["Rec Room","Bathroom","Seating"],
         color: [],
@@ -300,6 +332,7 @@ const frozenFurniture = [
     {
         furnitureName: `Norwegian Spruce Robe Hanger`,
         price: 6400,
+        image: "https://dreamlightvalleywiki.com/images/thumb/4/41/Norwegian_Spruce_Robe_Hanger.png/99px-Norwegian_Spruce_Robe_Hanger.png",
         style: ["Elegant","Rustic"],
         furnitureCategory: ["Rec Room","Bathroom","Decor"],
         color: [],
@@ -309,6 +342,7 @@ const frozenFurniture = [
     {
         furnitureName: `Norwegian Spruce Robe Shelf`,
         price: 6400,
+        image: "https://dreamlightvalleywiki.com/images/thumb/0/0e/Norwegian_Spruce_Robe_Shelf.png/107px-Norwegian_Spruce_Robe_Shelf.png",
         style: ["Elegant","Rustic"],
         furnitureCategory: ["Rec Room","Bathroom","Decor"],
         color: [],
@@ -318,6 +352,7 @@ const frozenFurniture = [
     {
         furnitureName: `Norwegian Spruce Toiletry Basket`,
         price: 1200,
+        image: "https://dreamlightvalleywiki.com/images/thumb/1/13/Norwegian_Spruce_Toiletry_Basket.png/180px-Norwegian_Spruce_Toiletry_Basket.png",
         style: ["Elegant","Rustic"],
         furnitureCategory: ["Rec Room","Bathroom","Decor"],
         color: [],
@@ -327,6 +362,7 @@ const frozenFurniture = [
     {
         furnitureName: `Norwegian Spruce Tub`,
         price: 20000,
+        image: "https://dreamlightvalleywiki.com/images/2/25/Norwegian_Spruce_Tub.png",
         obtainedBy: "Kristoff Friendship Level 8",
         style: ["Elegant","Rustic"],
         furnitureCategory: ["Rec Room","Bathroom","Decor"],
@@ -337,6 +373,7 @@ const frozenFurniture = [
     {
         furnitureName: `Norwegian Spruce Water Bucket`,
         price: 360,
+        image: "https://dreamlightvalleywiki.com/images/4/43/Norwegian_Spruce_Water_Bucket.png",
         style: ["Elegant","Rustic"],
         furnitureCategory: ["Rec Room","Bathroom","Decor"],
         color: [],
@@ -346,6 +383,7 @@ const frozenFurniture = [
     {
         furnitureName: `Norwegian Spruce Window to the Mountains`,
         price: 20000,
+        image: "https://dreamlightvalleywiki.com/images/thumb/1/1c/Norwegian_Spruce_Window_to_the_Mountains.png/180px-Norwegian_Spruce_Window_to_the_Mountains.png",
         style: ["Elegant","Rustic"],
         furnitureCategory: ["Rec Room","Bathroom","Windows"],
         color: [],
@@ -355,6 +393,7 @@ const frozenFurniture = [
     {
         furnitureName: `Ornate Dining Chair`,
         price: 2400,
+        image: "https://dreamlightvalleywiki.com/images/thumb/d/d3/Ornate_Dining_Chair.png/92px-Ornate_Dining_Chair.png",
         style: ["Traditional","Elegant"],
         furnitureCategory: ["Dining Room","Seating"],
         color: [],
@@ -364,6 +403,7 @@ const frozenFurniture = [
     {
         furnitureName: `Painted Double Door`,
         price: 3200,
+        image: "https://dreamlightvalleywiki.com/images/thumb/2/23/Painted_Double_Door.png/180px-Painted_Double_Door.png",
         obtainedBy: "Anna Friendship Level 2",
         style: ["Traditional","Elegant"],
         furnitureCategory: ["Dining Room","Bedroom","Study","Living Room", "Decor"],
@@ -374,6 +414,7 @@ const frozenFurniture = [
     {
         furnitureName: `Pillar`,
         price: 3200,
+        image: "https://dreamlightvalleywiki.com/images/thumb/1/14/Pillar.png/42px-Pillar.png",
         style: ["Bold","Elegant"],
         furnitureCategory: ["Dining Room","Living Room","Outdoors"],
         color: [],
@@ -383,6 +424,7 @@ const frozenFurniture = [
     {
         furnitureName: `Royal Bookshelf with Bell`,
         price: 6400,
+        image: "https://dreamlightvalleywiki.com/images/thumb/5/54/Royal_Bookshelf_with_Bell.png/78px-Royal_Bookshelf_with_Bell.png",
         style: ["Rustic","Traditional"],
         furnitureCategory: ["Study","Rec Room","Living Room","Decor"],
         color: [],
@@ -392,6 +434,7 @@ const frozenFurniture = [
     {
         furnitureName: `Royal Bookshelf with Greenery`,
         price: 6400,
+        image: "https://dreamlightvalleywiki.com/images/thumb/7/75/Royal_Bookshelf_with_Greenery.png/78px-Royal_Bookshelf_with_Greenery.png",
         style: ["Rustic","Traditional"],
         furnitureCategory: ["Study","Rec Room","Living Room","Decor"],
         color: [],
@@ -401,6 +444,7 @@ const frozenFurniture = [
     {
         furnitureName: `Snowflake Bed`,
         price: 9200,
+        image: "https://dreamlightvalleywiki.com/images/thumb/e/ee/Snowflake_Bed.png/180px-Snowflake_Bed.png",
         style: ["Bold","Elegant"],
         furnitureCategory: ["Bedroom","Beds"],
         color: [],
@@ -410,6 +454,7 @@ const frozenFurniture = [
     {
         furnitureName: `Swing Painting`,
         price: 3400,
+        image: "https://dreamlightvalleywiki.com/images/thumb/d/da/Swing_Painting.png/180px-Swing_Painting.png",
         style: ["Traditional","Elegant"],
         furnitureCategory: ["Dining Room","Living Room","Study","Decor"],
         color: [],
@@ -419,6 +464,7 @@ const frozenFurniture = [
     {
         furnitureName: `Triangular Stained Glass Window`,
         price: 8000,
+        image: "https://dreamlightvalleywiki.com/images/thumb/5/55/Triangular_Stained_Glass_Window.png/180px-Triangular_Stained_Glass_Window.png",
         style: ["Traditional","Elegant"],
         furnitureCategory: ["Dining Room","Bathroom","Kitchen","Living Room","Windows"],
         color: [],
@@ -428,6 +474,7 @@ const frozenFurniture = [
     {
         furnitureName: `Vintage Phonograph`,
         price: 6400,
+        image: "https://dreamlightvalleywiki.com/images/thumb/2/2e/Vintage_Phonograph.png/120px-Vintage_Phonograph.png",
         style: ["Rustic","Traditional"],
         furnitureCategory: ["Living Room","Rec Room","Study","Decor"],
         color: [],
@@ -437,6 +484,7 @@ const frozenFurniture = [
     {
         furnitureName: `Wine-Colored Loveseat`,
         price: 2400,
+        image: "https://dreamlightvalleywiki.com/images/thumb/c/cd/Wine-Colored_Loveseat.png/180px-Wine-Colored_Loveseat.png",
         obtainedBy: "Furniture pouch",
         style: ["Traditional","Rustic"],
         furnitureCategory: ["Rec Room","Study","Bedroom","Seating"],
@@ -446,6 +494,7 @@ const frozenFurniture = [
     },
     {
         furnitureName: `Yule Goat`,
+        image: "https://dreamlightvalleywiki.com/images/thumb/3/3b/Yule_Goat.png/136px-Yule_Goat.png",
         obtainedBy: "crafting",
         style: [],
         furnitureCategory: ["Decor"],
