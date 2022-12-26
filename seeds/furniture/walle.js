f5032ae3dedefcc36adb7a55937a623db81a23a4
killingsworth.kristen <killingsworth_kristen@yahoo.com>
2022-12-25 22:53:01 -0500

    images added to walle furniture

--- seeds/furniture/walle.js
+++ seeds/furniture/walle.js
@@ -5,6 +5,7 @@ const walleFurniture =[
     {
         furnitureName: `"Meal in a Cup" Dispenser`,
         price: 7360,
+        image: "https://dreamlightvalleywiki.com/images/thumb/2/2a/%22Meal_in_a_Cup%22_Dispenser.png/127px-%22Meal_in_a_Cup%22_Dispenser.png",
         style: ["Futuristic",],
         furnitureCategory: ["Kitchen","Outdoors","Decor"],
         color: [],
@@ -14,6 +15,7 @@ const walleFurniture =[
     {
         furnitureName: `Access Door`,
         price: 3200,
+        image: "https://dreamlightvalleywiki.com/images/thumb/3/34/Access_Door.png/121px-Access_Door.png",
         style: ["Futuristic"],
         furnitureCategory: ["Study","Bedroom","Living Room","Decor"],
         color: [],
@@ -23,6 +25,7 @@ const walleFurniture =[
     {
         furnitureName: `Automated Parasol`,
         price: 1200,
+        image: "https://dreamlightvalleywiki.com/images/thumb/2/25/Automated_Parasol.png/151px-Automated_Parasol.png",
         style: ["Futuristic"],
         furnitureCategory: ["Outdoors","Decor"],
         color: [],
@@ -32,6 +35,7 @@ const walleFurniture =[
     {
         furnitureName: `Bike Seat Stool`,
         price: 2400,
+        image: "https://dreamlightvalleywiki.com/images/thumb/4/40/Bike_Seat_Stool.png/140px-Bike_Seat_Stool.png",
         style: ["Rustic","Modern","Quirky"],
         furnitureCategory: ["Kitchen","Rec Room","Outdoors","Seating"],
         color: [],
@@ -42,6 +46,7 @@ const walleFurniture =[
     {
         furnitureName: `Caution Cone`,
         price: 900,
+        image: "https://dreamlightvalleywiki.com/images/thumb/f/f4/Caution_Cone.png/161px-Caution_Cone.png",
         style: ["Professional","Modern"],
         furnitureCategory: ["Outdoors","Decor"],
         color: [],
@@ -51,24 +56,27 @@ const walleFurniture =[
     {
         furnitureName: `Clamped Side Table`,
         price: 3520,
+        image: "https://dreamlightvalleywiki.com/images/f/f2/Clamped_Side_Table.png",
         style: ["Modern","Rustic"],
         furnitureCategory: ["Bedroom","Living Room","Tables"],
         color: [],
         universe: "WALL-E",
         added: 1.0
     },
-    // {
-    //     furnitureName: `Clothing Hanger`,
-    //     price: ,
-    //     style: [],
-    //     furnitureCategory: [""],
-    //     color: [],
-    //     universe: "WALL-E",
-    //     added: 1.0
-    // },
+    {
+        furnitureName: `Clothing Hanger`,
+        // price: ,
+        image: "https://dreamlightvalleywiki.com/images/thumb/a/a2/Clothing_Hanger.png/154px-Clothing_Hanger.png",
+        style: [],
+        furnitureCategory: ["Decor"],
+        color: [],
+        universe: "WALL-E",
+        added: 1.0
+    },
     {
         furnitureName: `Hanging Pans`,
         price: 3200,
+        image: "https://dreamlightvalleywiki.com/images/thumb/f/f3/Hanging_Pans.png/180px-Hanging_Pans.png",
         style: ["Modern","Rustic","Quirky"],
         furnitureCategory: ["Kitchen","Lighting"],
         color: [],
@@ -78,6 +86,7 @@ const walleFurniture =[
     {
         furnitureName: `Hoverchair`,
         price: 4800,
+        image: "https://dreamlightvalleywiki.com/images/thumb/f/f0/Hoverchair.png/149px-Hoverchair.png",
         style: ["Futuristic"],
         furnitureCategory: ["Living Room","Study","Rec Room","Seating"],
         color: [],
@@ -87,6 +96,7 @@ const walleFurniture =[
     {
         furnitureName: `Hovercouch`,
         price: 4800,
+        image: "https://dreamlightvalleywiki.com/images/thumb/5/52/Hovercouch.png/180px-Hovercouch.png",
         style: ["Futuristic"],
         furnitureCategory: ["Living Room","Seating"],
         color: [],
@@ -96,6 +106,7 @@ const walleFurniture =[
     {
         furnitureName: `Hypersleeper`,
         price: 4600,
+        image: "https://dreamlightvalleywiki.com/images/thumb/6/67/Hypersleeper.png/180px-Hypersleeper.png",
         style: ["Futuristic"],
         furnitureCategory: ["Bedroom","Beds"],
         color: [],
@@ -105,6 +116,7 @@ const walleFurniture =[
     {
         furnitureName: `Industrial Ceiling Lights`,
         price: 1020,
+        image: "https://dreamlightvalleywiki.com/images/thumb/4/4b/Industrial_Ceiling_Lights.png/165px-Industrial_Ceiling_Lights.png",
         style: ["Modern","Quirky","Rustic"],
         furnitureCategory: ["Kitchen","Rec Room","Lighting"],
         color: [],
@@ -114,6 +126,7 @@ const walleFurniture =[
     {
         furnitureName: `Jar Lamp`,
         price: 765,
+        image: "https://dreamlightvalleywiki.com/images/thumb/8/8d/Jar_Lamp.png/54px-Jar_Lamp.png",
         style: ["Modern","Quirky"],
         furnitureCategory: ["Living Room","Bedroom","Study","Lighting"],
         color: [],
@@ -123,6 +136,7 @@ const walleFurniture =[
     {
         furnitureName: `Large Cooler`,
         price: 2400,
+        image: "https://dreamlightvalleywiki.com/images/thumb/1/10/Large_Cooler.png/180px-Large_Cooler.png",
         obtainedby: "WALL-E Friendship Level 2",
         style: ["Modern","Cute"],
         furnitureCategory: ["Kitchen","Decor"],
@@ -133,6 +147,7 @@ const walleFurniture =[
     {
         furnitureName: `Leisure Banner`,
         price: 1020,
+        image: "https://dreamlightvalleywiki.com/images/thumb/a/a8/Leisure_Banner.png/58px-Leisure_Banner.png",
         style: ["Futuristic"],
         furnitureCategory: ["Outdoors","Rec Room","Decor"],
         color: [],
@@ -142,6 +157,7 @@ const walleFurniture =[
     {
         furnitureName: `Lido Deck Table`,
         price: 1320,
+        image: "https://dreamlightvalleywiki.com/images/3/31/Lido_Deck_Table.png",
         style: [],
         furnitureCategory: ["Tables"],
         color: [],
@@ -151,6 +167,7 @@ const walleFurniture =[
     {
         furnitureName: `Locker Counter`,
         price: 3520,
+        image: "https://dreamlightvalleywiki.com/images/thumb/2/23/Locker_Counter.png/180px-Locker_Counter.png",
         style: ["Rustic","Modern","Quirky"],
         furnitureCategory: ["Kitchen","Tables"],
         color: [],
@@ -160,6 +177,7 @@ const walleFurniture =[
     {
         furnitureName: `Meal in a Cup`,
         price: 2400,
+        image: "https://dreamlightvalleywiki.com/images/thumb/f/f7/Meal_in_a_Cup.png/76px-Meal_in_a_Cup.png",
         style: ["Futuristic"],
         furnitureCategory: ["Kitchen","Dining Room","Decor"],
         color: [],
@@ -169,6 +187,7 @@ const walleFurniture =[
     {
         furnitureName: `Pallet Bed`,
         price: 9200,
+        image: "https://dreamlightvalleywiki.com/images/thumb/6/69/Pallet_Bed.png/131px-Pallet_Bed.png",
         style: ["Modern","Quirky"],
         furnitureCategory: ["Bedroom","Beds"],
         color: [],
@@ -178,6 +197,7 @@ const walleFurniture =[
     {
         furnitureName: `Rusted Bench`,
         price: 360,
+        image: "https://dreamlightvalleywiki.com/images/thumb/b/b3/Rusted_Bench.png/180px-Rusted_Bench.png",
         style: ["Modern"],
         furnitureCategory: ["Outdoors","Seating"],
         color: [],
@@ -187,6 +207,7 @@ const walleFurniture =[
     {
         furnitureName: `Rusted Trashcan`,
         price: 900,
+        image: "https://dreamlightvalleywiki.com/images/thumb/b/bb/Rusted_Trashcan.png/180px-Rusted_Trashcan.png",
         style: ["Modern"],
         furnitureCategory: ["Outdoors","Decor"],
         color: [],
@@ -196,6 +217,7 @@ const walleFurniture =[
     {
         furnitureName: `Spacecraft Porthole`,
         price: 16000,
+        image: "https://dreamlightvalleywiki.com/images/thumb/e/ea/Spacecraft_Porthole.png/180px-Spacecraft_Porthole.png",
         style: ["Futuristic"],
         furnitureCategory: ["Rec Room","Bedroom","Living Room","Decor"],
         color: [],
@@ -205,6 +227,7 @@ const walleFurniture =[
     {
         furnitureName: `Sports Banner`,
         price: 1020,
+        image: "https://dreamlightvalleywiki.com/images/thumb/b/b8/Sports_Banner.png/58px-Sports_Banner.png",
         style: ["Futuristic"],
         furnitureCategory: ["Rec Room","Outdoors","Decor"],
         color: [],
@@ -214,6 +237,7 @@ const walleFurniture =[
     {
         furnitureName: `Steward Bot`,
         price: 6400,
+        image: "https://dreamlightvalleywiki.com/images/thumb/f/f2/Steward_Bot.png/112px-Steward_Bot.png",
         style: ["Futuristic"],
         furnitureCategory: ["Outdoors","Rec Room","Decor"],
         color: [],
@@ -223,6 +247,7 @@ const walleFurniture =[
     {
         furnitureName: `Suspended Shelf`,
         price: 6400,
+        image: "https://dreamlightvalleywiki.com/images/thumb/2/29/Suspended_Shelf.png/180px-Suspended_Shelf.png",
         style: ["Modern","Rustic","Quirky"],
         furnitureCategory: ["Kitchen","Decor"],
         color: [],
@@ -232,6 +257,7 @@ const walleFurniture =[
     {
         furnitureName: `Tractor Island`,
         price: 7040,
+        image: "https://dreamlightvalleywiki.com/images/thumb/3/39/Tractor_Island.png/180px-Tractor_Island.png",
         style: ["Modern","Rustic","Quirky"],
         furnitureCategory: ["Kitchen","Rec Room","Tables"],
         color: [],
@@ -241,6 +267,7 @@ const walleFurniture =[
     {
         furnitureName: `Vacant Bike Dock`,
         price: 900,
+        image: "https://dreamlightvalleywiki.com/images/thumb/6/60/Vacant_Bike_Dock.png/180px-Vacant_Bike_Dock.png",
         style: ["Casual","Modern"],
         furnitureCategory: ["Outdoors","Decor"],
         color: [],
@@ -250,6 +277,7 @@ const walleFurniture =[
     {
         furnitureName: `Valise Chair`,
         price: 2400,
+        image: "https://dreamlightvalleywiki.com/images/thumb/9/93/Valise_Chair.png/145px-Valise_Chair.png",
         style: ["Cute","Modern","Quirky"],
         furnitureCategory: ["Bedroom","Living Room","Study","Seating"],
         color: [],
@@ -259,6 +287,7 @@ const walleFurniture =[
     {
         furnitureName: `Vintage Advertising Posters`,
         price: 2720,
+        image: "https://dreamlightvalleywiki.com/images/thumb/6/6d/Vintage_Advertising_Posters.png/180px-Vintage_Advertising_Posters.png",
         obtainedby: "WALL-E Friendship Level 8",
         style: ["Modern"],
         furnitureCategory: ["Bedroom","Kitchen","Study","Decor"],
@@ -269,24 +298,27 @@ const walleFurniture =[
     {
         furnitureName: `Weather Panel`,
         price: 1020,
+        image: "https://dreamlightvalleywiki.com/images/thumb/e/e9/Weather_Panel.png/74px-Weather_Panel.png",
         style: ["Futuristic"],
         furnitureCategory: ["Decor"],
         color: [],
         universe: "WALL-E",
         added: 1.0
     },
-    // {
-    //     furnitureName: `Wild Coffee Table`,
-    //     price: ,
-    //     style: [],
-    //     furnitureCategory: [""],
-    //     color: [],
-    //     universe: "WALL-E",
-    //     added: 1.0
-    // },
+    {
+        furnitureName: `Wild Coffee Table`,
+        // price: ,
+        image: "https://dreamlightvalleywiki.com/images/thumb/6/60/Wild_Coffee_Table.png/180px-Wild_Coffee_Table.png",
+        style: [],
+        furnitureCategory: ["Tables"],
+        color: [],
+        universe: "WALL-E",
+        added: 1.0
+    },
     {
         furnitureName: `Wood Plank Seat`,
         price: 900,
+        image: "https://dreamlightvalleywiki.com/images/thumb/0/09/Wood_Plank_Seat.png/124px-Wood_Plank_Seat.png",
         style: [],
         furnitureCategory: ["Seating"],
         color: [],
@@ -296,6 +328,7 @@ const walleFurniture =[
     {
         furnitureName: `Wooden Sink`,
         price: 3200,
+        image: "https://dreamlightvalleywiki.com/images/thumb/9/92/Wooden_Sink.png/125px-Wooden_Sink.png",
         style: ["Modern","Quirky","Rustic"],
         furnitureCategory: ["Kitchen","Bathroom"],
         color: [],
@@ -305,6 +338,7 @@ const walleFurniture =[
     {
         furnitureName: `Yellow Bike`,
         price: 6400,
+        image: "https://dreamlightvalleywiki.com/images/thumb/8/84/Yellow_Bike.png/180px-Yellow_Bike.png",
         obtainedby: "WALL-E Friendship Level 5",
         style: ["Casual","Modern"],
         furnitureCategory: ["Outdoors","Decor"],
@@ -312,7 +346,17 @@ const walleFurniture =[
         universe: "WALL-E",
         added: 1.0,
         updated: 1.1
-    }
+    },
+    // {
+    //     furnitureName: ``,
+    //     price: ,
+    //     image: "",
+    //     style: [],
+    //     furnitureCategory: [""],
+    //     color: [],
+    //     universe: "WALL-E",
+    //     added: 1.3,
+    // }
 ]
 
 module.exports=walleFurniture
