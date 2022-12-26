53059f6bef6480fe02a27e2740ae4bee2f498081
killingsworth.kristen <killingsworth_kristen@yahoo.com>
2022-12-25 21:56:34 -0500

    images added to little mermaid furniture

--- seeds/furniture/littleMermaid.js
+++ seeds/furniture/littleMermaid.js
@@ -5,6 +5,7 @@ const littleMermaidFurniture = [
     {
         furnitureName: `Aquarium`,
         price: 6400,
+        image: "https://dreamlightvalleywiki.com/images/thumb/e/e1/Aquarium.png/153px-Aquarium.png",
         style: ["Casual","Quirky"],
         furnitureCategory: ["Study","Rec Room","Living Room","Decor"],
         color: [],
@@ -14,6 +15,7 @@ const littleMermaidFurniture = [
     {
         furnitureName: `Blue Pearly Pendant Light`,
         price: 5440,
+        image: "https://dreamlightvalleywiki.com/images/thumb/5/56/Blue_Pearly_Pendant_Light.png/97px-Blue_Pearly_Pendant_Light.png",
         style: ["Cute","Elegant"],
         furnitureCategory: ["Living Room","Dining Room","Bedroom","Lighting"],
         color: [],
@@ -23,6 +25,7 @@ const littleMermaidFurniture = [
     {
         furnitureName: `Blue Pearly Table Lamp`,
         price: 4080,
+        image: "https://dreamlightvalleywiki.com/images/thumb/1/11/Blue_Pearly_Table_Lamp.png/180px-Blue_Pearly_Table_Lamp.png",
         style: ["Cute","Elegant"],
         furnitureCategory: ["Living Room","Dining Room","Study","Lighting"],
         color: [],
@@ -32,6 +35,7 @@ const littleMermaidFurniture = [
     {
         furnitureName: `Blue Pearly Torchère`,
         price: 5440,
+        image: "https://dreamlightvalleywiki.com/images/thumb/0/04/Blue_Pearly_Torch%C3%A8re.png/69px-Blue_Pearly_Torch%C3%A8re.png",
         style: ["Cute","Elegant"],
         furnitureCategory: ["Living Room","Bedroom","Lighting"],
         color: [],
@@ -41,6 +45,7 @@ const littleMermaidFurniture = [
     {
         furnitureName: `Bubble Lights`,
         price: 1020,
+        image: "https://dreamlightvalleywiki.com/images/thumb/a/aa/Bubble_Lights.png/98px-Bubble_Lights.png",
         style: ["Elegant","Modern"],
         furnitureCategory: ["Bathroom","Lighting"],
         color: [],
@@ -50,6 +55,7 @@ const littleMermaidFurniture = [
     {
         furnitureName: `Coral Hanger`,
         price: 6400,
+        image: "https://dreamlightvalleywiki.com/images/thumb/d/d8/Coral_Hanger.png/86px-Coral_Hanger.png",
         style: ["Elegant","Quirky"],
         furnitureCategory: ["Living Room","Bedroom","Decor"],
         color: [],
@@ -59,6 +65,7 @@ const littleMermaidFurniture = [
     {
         furnitureName: `Coral Jewelry Stand`,
         price: 2400,
+        image: "https://dreamlightvalleywiki.com/images/thumb/6/6e/Coral_Jewelry_Stand.png/102px-Coral_Jewelry_Stand.png",
         style: ["Cute","Elegant"],
         furnitureCategory: ["Bedroom","Bathroom","Decor"],
         color: [],
@@ -68,6 +75,7 @@ const littleMermaidFurniture = [
     {
         furnitureName: `Coral Reef Lounge`,
         price: 18000,
+        image: "https://dreamlightvalleywiki.com/images/thumb/3/34/Coral_Reef_Lounge.png/180px-Coral_Reef_Lounge.png",
         obtainedby: "Ariel Friendship Level 8",
         style: ["Cute","Elegant","Quirky"],
         furnitureCategory: ["Living Room","Outdoors"],
@@ -78,6 +86,7 @@ const littleMermaidFurniture = [
     {
         furnitureName: `Eric's Miniature Boat`,
         price: 16000,
+        image: "https://dreamlightvalleywiki.com/images/thumb/4/43/Eric%27s_Miniature_Boat.png/121px-Eric%27s_Miniature_Boat.png",
         obtainedby: "The Mystical Crystal (Prince Eric Level 10)",
         style: ["Elegant","Traditional"],
         furnitureCategory: ["Outdoors","Decor"],
@@ -88,6 +97,7 @@ const littleMermaidFurniture = [
     {
         furnitureName: `Lagoon Dresser`,
         price: 6400,
+        image: "https://dreamlightvalleywiki.com/images/thumb/3/3f/Lagoon_Dresser.png/180px-Lagoon_Dresser.png",
         style: ["Cute","Elegant"],
         furnitureCategory: ["Bedroom","Storage"],
         color: [],
@@ -97,6 +107,7 @@ const littleMermaidFurniture = [
     {
         furnitureName: `Lagoon Mirror`,
         price: 3200,
+        image: "https://dreamlightvalleywiki.com/images/thumb/c/c0/Lagoon_Mirror.png/180px-Lagoon_Mirror.png",
         style: ["Cute","Elegant"],
         furnitureCategory: ["Living Room","Bedroom","Bathroom","Decor"],
         color: [],
@@ -106,6 +117,7 @@ const littleMermaidFurniture = [
     {
         furnitureName: `Lagoon Vanity`,
         price: 7040,
+        image: "https://dreamlightvalleywiki.com/images/thumb/a/a5/Lagoon_Vanity.png/106px-Lagoon_Vanity.png",
         style: ["Cute","Elegant"],
         furnitureCategory: ["Bedroom","Tables"],
         color: [],
@@ -115,6 +127,7 @@ const littleMermaidFurniture = [
     {
         furnitureName: `Maelstrom Armchair`,
         price: 4800,
+        image: "https://dreamlightvalleywiki.com/images/thumb/9/9f/Maelstrom_Armchair.png/114px-Maelstrom_Armchair.png",
         style: ["Bold"],
         furnitureCategory: ["Bedroom","Living Room","Seating"],
         color: [],
@@ -124,6 +137,7 @@ const littleMermaidFurniture = [
     {
         furnitureName: `Mirror and Mosaic Sink`,
         price: 6200,
+        image: "https://dreamlightvalleywiki.com/images/thumb/6/63/Mirror_and_Mosaic_Sink.png/104px-Mirror_and_Mosaic_Sink.png",
         style: ["Elegant","Cute","Modern"],
         furnitureCategory: ["Bathroom","Decor"],
         color: [],
@@ -133,6 +147,7 @@ const littleMermaidFurniture = [
     {
         furnitureName: `Nautilus Bedside Lamp`,
         price: 4080,
+        image: "https://dreamlightvalleywiki.com/images/thumb/2/26/Nautilus_Bedside_Lamp.png/180px-Nautilus_Bedside_Lamp.png",
         style: ["Bold"],
         furnitureCategory: ["Living Room","Bedroom","Lighting"],
         color: [],
@@ -142,6 +157,7 @@ const littleMermaidFurniture = [
     {
         furnitureName: `Ocean Decor`,
         price: 765,
+        image: "https://dreamlightvalleywiki.com/images/thumb/2/2b/Ocean_Decor.png/62px-Ocean_Decor.png",
         style: ["Casual","Cute","Modern"],
         furnitureCategory: ["Bedroom","Bathroom","Study","Decor"],
         color: [],
@@ -151,6 +167,7 @@ const littleMermaidFurniture = [
     {
         furnitureName: `Octopus Carpet`,
         price: 8000,
+        image: "https://dreamlightvalleywiki.com/images/f/fe/Octopus_Carpet.png",
         style: ["Bold"],
         furnitureCategory: ["Study","Rec Room","Living Room","Bedroom","Rugs"],
         color: [],
@@ -160,6 +177,7 @@ const littleMermaidFurniture = [
     {
         furnitureName: `Octopus Table`,
         price: 3520,
+        image: "https://dreamlightvalleywiki.com/images/thumb/8/87/Octopus_Table.png/155px-Octopus_Table.png",
         style: ["Bold"],
         furnitureCategory: ["Living Room","Bedroom","Tables"],
         color: [],
@@ -170,6 +188,7 @@ const littleMermaidFurniture = [
     {
         furnitureName: `Ornamental Kelp`,
         price: 5760,
+        image: "https://dreamlightvalleywiki.com/images/thumb/8/8b/Ornamental_Kelp.png/75px-Ornamental_Kelp.png",
         style: ["Elegant","Quirky"],
         furnitureCategory: ["Bathroom","Dining Room","Decor"],
         color: [],
@@ -179,6 +198,7 @@ const littleMermaidFurniture = [
     {
         furnitureName: `Pearl Nightstand`,
         price: 3520,
+        image: "https://dreamlightvalleywiki.com/images/thumb/f/f5/Pearl_Nightstand.png/180px-Pearl_Nightstand.png",
         style: ["Quirky"],
         furnitureCategory: ["Living Room","Bedroom","Tables"],
         color: [],
@@ -188,6 +208,7 @@ const littleMermaidFurniture = [
     {
         furnitureName: `Poor Unfortunate Bookcase`,
         price: 6400,
+        image: "https://dreamlightvalleywiki.com/images/thumb/d/dc/Poor_Unfortunate_Bookcase.png/104px-Poor_Unfortunate_Bookcase.png",
         obtainedby: "Ursula Friendship Level 8",
         style: ["Bold"],
         furnitureCategory: ["Bedroom","Living Room","Decor"],
@@ -198,6 +219,7 @@ const littleMermaidFurniture = [
     {
         furnitureName: `Porthole`,
         price: 6400,
+        image: "https://dreamlightvalleywiki.com/images/thumb/f/f7/Porthole.png/150px-Porthole.png",
         style: ["Quirky","Bold"],
         furnitureCategory: ["Bathroom","Rec Room","Decor"],
         color: [],
@@ -206,6 +228,7 @@ const littleMermaidFurniture = [
     },
     {
         furnitureName: `Prince Eric Statue`,
+        image: "https://dreamlightvalleywiki.com/images/thumb/7/7b/Prince_Eric_Statue.png/68px-Prince_Eric_Statue.png",
         obtainedby: "The Missing Prince (Ariel Level 3)",
         tracked: false,
         style: ["Elegant","Traditional","Professional"],
@@ -217,6 +240,7 @@ const littleMermaidFurniture = [
     {
         furnitureName: `Purple Foot Stool`,
         price: 3200,
+        image: "https://dreamlightvalleywiki.com/images/thumb/b/be/Purple_Foot_Stool.png/180px-Purple_Foot_Stool.png",
         style: ["Bold"],
         furnitureCategory: ["Study","Living Room","Bedroom","Seating"],
         color: [],
@@ -226,6 +250,7 @@ const littleMermaidFurniture = [
     {
         furnitureName: `Queen of the Sea Mirror`,
         price: 6400,
+        image: "https://dreamlightvalleywiki.com/images/thumb/9/98/Queen_of_the_Sea_Mirror.png/74px-Queen_of_the_Sea_Mirror.png",
         style: ["Bold"],
         furnitureCategory: ["Bedroom","Living Room","Decor"],
         color: [],
@@ -235,6 +260,7 @@ const littleMermaidFurniture = [
     {
         furnitureName: `Scallop Slipper Chair`,
         price: 10000,
+        image: "https://dreamlightvalleywiki.com/images/thumb/6/67/Scallop_Slipper_Chair.png/180px-Scallop_Slipper_Chair.png",
         style: ["Cute","Elegant"],
         furnitureCategory: ["Bedroom","Rec Room","Living Room","Seating"],
         color: [],
@@ -244,6 +270,7 @@ const littleMermaidFurniture = [
     {
         furnitureName: `Scallop Stool`,
         price: 4800,
+        image: "https://dreamlightvalleywiki.com/images/thumb/8/81/Scallop_Stool.png/180px-Scallop_Stool.png",
         style: ["Cute","Elegant"],
         furnitureCategory: ["Living Room","Rec Room","Study","Seating"],
         color: [],
@@ -253,6 +280,7 @@ const littleMermaidFurniture = [
     {
         furnitureName: `Scallop Tulip Chair`,
         price: 4800,
+        image: "https://dreamlightvalleywiki.com/images/thumb/6/6b/Scallop_Tulip_Chair.png/130px-Scallop_Tulip_Chair.png",
         style: ["Cute","Elegant"],
         furnitureCategory: ["Living Room","Rec Room","Seating"],
         color: [],
@@ -262,6 +290,7 @@ const littleMermaidFurniture = [
     {
         furnitureName: `Seashell Bed`,
         price: 9200,
+        image: "https://dreamlightvalleywiki.com/images/thumb/6/64/Seashell_Bed.png/168px-Seashell_Bed.png",
         style: ["Quirky","Cute"],
         furnitureCategory: ["Bedroom","Beds"],
         color: [],
@@ -272,6 +301,7 @@ const littleMermaidFurniture = [
     {
         furnitureName: `Shell Bathmat`,
         price: 3200,
+        image: "https://dreamlightvalleywiki.com/images/thumb/3/34/Shell_Bathmat.png/180px-Shell_Bathmat.png",
         style: ["Cute","Quirky","Modern"],
         furnitureCategory: ["Bathroom","Rec Room","Bedroom","Rugs"],
         color: [],
@@ -281,6 +311,7 @@ const littleMermaidFurniture = [
     {
         furnitureName: `Shell Chair`,
         price: 2400,
+        image: "https://dreamlightvalleywiki.com/images/thumb/d/d9/Shell_Chair.png/166px-Shell_Chair.png",
         style: ["Elegant","Cute"],
         furnitureCategory: ["Bedroom","Study","Living Room","Seating"],
         color: [],
@@ -290,6 +321,7 @@ const littleMermaidFurniture = [
     {
         furnitureName: `Shell Couch and Starfish Pillow`,
         // price: ,
+        image: "https://dreamlightvalleywiki.com/images/thumb/f/f9/Shell_Couch_and_Starfish_Pillow.png/180px-Shell_Couch_and_Starfish_Pillow.png",
         style: [""],
         furnitureCategory: ["Seating"],
         color: [],
@@ -300,6 +332,7 @@ const littleMermaidFurniture = [
     {
         furnitureName: `Starfish Coffee Table`,
         price: 7040,
+        image: "https://dreamlightvalleywiki.com/images/thumb/d/d7/Starfish_Coffee_Table.png/180px-Starfish_Coffee_Table.png",
         style: ["Cute","Elegant"],
         furnitureCategory: ["Living Room","Tables"],
         color: [],
@@ -309,6 +342,7 @@ const littleMermaidFurniture = [
     {
         furnitureName: `Starfish Side Table`,
         price: 17600,
+        image: "https://dreamlightvalleywiki.com/images/thumb/5/55/Starfish_Side_Table.png/180px-Starfish_Side_Table.png",
         style: ["Cute","Elegant"],
         furnitureCategory: ["Living Room","Tables"],
         color: [],
@@ -316,8 +350,9 @@ const littleMermaidFurniture = [
         added: 1.0
     },
     {
-        furnitureName: `Tentacle Pattern FLoor Lamp`,
+        furnitureName: `Tentacle Pattern Floor Lamp`,
         price: 2720,
+        image: "https://dreamlightvalleywiki.com/images/thumb/0/06/Tentacle_Pattern_Floor_Lamp.png/56px-Tentacle_Pattern_Floor_Lamp.png",
         style: ["Bold"],
         furnitureCategory: ["Living Room","Bedroom","Lighting"],
         color: [],
@@ -327,6 +362,7 @@ const littleMermaidFurniture = [
     {
         furnitureName: `Toilet`,
         price: 6400,
+        image: "https://dreamlightvalleywiki.com/images/d/d5/Toilet.png",
         style: ["Cute","Quirky","Modern"],
         furnitureCategory: ["Bathroom","Decor"],
         color: [],
@@ -336,6 +372,7 @@ const littleMermaidFurniture = [
     {
         furnitureName: `Undersea Seat`,
         price: 4800,
+        image: "https://dreamlightvalleywiki.com/images/1/13/Undersea_Seat.png",
         style: ["Bold","Quirky"],
         furnitureCategory: ["Study","Bedroom","Outdoors","Seating"],
         color: [],
@@ -345,6 +382,7 @@ const littleMermaidFurniture = [
     {
         furnitureName: `Ursula's Throne of the Abyss`,
         price: 12000,
+        image: "https://dreamlightvalleywiki.com/images/thumb/d/d3/Ursula%27s_Throne_of_the_Abyss.png/146px-Ursula%27s_Throne_of_the_Abyss.png",
         style: ["Bold"],
         furnitureCategory: ["Outdoors","Rec Room","Study","Seating"],
         color: [],
@@ -354,6 +392,7 @@ const littleMermaidFurniture = [
     {
         furnitureName: `Ursula's Vanity`,
         price: 7040,
+        image: "https://dreamlightvalleywiki.com/images/thumb/a/af/Ursula%27s_Vanity.png/102px-Ursula%27s_Vanity.png",
         style: ["Bold","Quirky"],
         furnitureCategory: ["Bedroom","Tables"],
         color: [],
@@ -363,12 +402,23 @@ const littleMermaidFurniture = [
     {
         furnitureName: `Whirl Bath`,
         price: 16000,
+        image: "https://dreamlightvalleywiki.com/images/thumb/4/49/Whirl_Bath.png/133px-Whirl_Bath.png",
         style: ["Modern","Elegant"],
         furnitureCategory: ["Bathroom","Decor"],
         color: [],
         universe:"The Little Mermaid",
         added: 1.0
     },
+    // {
+    //     furnitureName: ``,
+    //     price: ,
+    //     image: "",
+    //     style: [],
+    //     furnitureCategory: [""],
+    //     color: [],
+    //     universe:"The Little Mermaid",
+    //     added: 1.3
+    // },
 ]
 
 module.exports=littleMermaidFurniture
