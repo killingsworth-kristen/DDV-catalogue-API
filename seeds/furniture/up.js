344d676df02f16c78614923e12ecf5a5711815e3
killingsworth.kristen <killingsworth_kristen@yahoo.com>
2022-12-25 21:01:30 -0500

    Images added to Up furniture

--- seeds/furniture/up.js
+++ seeds/furniture/up.js
@@ -3,6 +3,7 @@ const upFurniture = [
     {
         furnitureName: `"Adventure is Out There!" Balloon Basket`,
         price: 720,
+        image: "https://dreamlightvalleywiki.com/images/thumb/9/9f/%22Adventure_Is_Out_There%21%22_Balloon_Basket.png/180px-%22Adventure_Is_Out_There%21%22_Balloon_Basket.png",
         obtainedBy: "Pixar Fest Star Path",
         style: ["Cute","Quirky"],
         furnitureCategory: ["Outdoors"],
@@ -13,6 +14,7 @@ const upFurniture = [
     {
         furnitureName: `Fredricksen Fireplace`,
         price: 480,
+        image: "https://dreamlightvalleywiki.com/images/thumb/9/94/Fredricksen_Fireplace.png/180px-Fredricksen_Fireplace.png",
         obtainedBy: "Pixar Fest Star Path",
         style: ["Casual","Cute","Rustic"],
         furnitureCategory: ["Dining Room","Living Room","Study"],
