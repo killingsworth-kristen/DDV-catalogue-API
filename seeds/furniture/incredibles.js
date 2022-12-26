478e0885d7d98dea201240b585902159be106291
killingsworth.kristen <killingsworth_kristen@yahoo.com>
2022-12-25 20:43:26 -0500

    Images added to incredibles furniture

--- seeds/furniture/incredibles.js
+++ seeds/furniture/incredibles.js
@@ -3,6 +3,7 @@ const incrediblesFurniture = [
     {
         furnitureName: `Elasticycle`,
         price: 430,
+        image: "https://dreamlightvalleywiki.com/images/thumb/3/3e/Elasticycle.png/180px-Elasticycle.png",
         obtainedBy: "Pixar Fest Star Path",
         style: ["Bold","Futuristic","Sporty"],
         furnitureCategory: ["Outdoors"],
@@ -13,6 +14,7 @@ const incrediblesFurniture = [
     {
         furnitureName: `Mr. Incredible Retro Suit Display`,
         price: 480,
+        image: "https://dreamlightvalleywiki.com/images/thumb/9/9f/Mr._Incredible_Retro_Suit_Display.png/180px-Mr._Incredible_Retro_Suit_Display.png",
         obtainedBy: "Pixar Fest Star Path",
         style: ["Professional","Quirky"],
         furnitureCategory: ["Study"],
