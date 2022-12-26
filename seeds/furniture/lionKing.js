378e28056c1c0893a02dc5ea787aa525146ed1b4
killingsworth.kristen <killingsworth_kristen@yahoo.com>
2022-12-25 21:50:25 -0500

    images added to lion king furniture

--- seeds/furniture/lionKing.js
+++ seeds/furniture/lionKing.js
@@ -6,6 +6,7 @@ const lionKingFurniture = [
     {
         furnitureName: `Branch Partition`,
         // price: ,
+        image: "https://dreamlightvalleywiki.com/images/thumb/4/42/Branch_Partition.png/87px-Branch_Partition.png",
         style: ["Rustic","Traditional"],
         furnitureCategory: ["Bedroom","Dining Room","Study","Decor"],
         color: [],
@@ -15,6 +16,7 @@ const lionKingFurniture = [
     {
         furnitureName: `Carved Wood Bed`,
         price: 23000,
+        image: "https://dreamlightvalleywiki.com/images/thumb/d/d6/Carved_Wood_Bed.png/168px-Carved_Wood_Bed.png",
         style: ["Bold","Elegant","Rustic"],
         furnitureCategory: ["Bedroom"],
         color: [],
@@ -24,6 +26,7 @@ const lionKingFurniture = [
     {
         furnitureName: `Carved Wood Bench`,
         price: 2400,
+        image: "https://dreamlightvalleywiki.com/images/thumb/2/29/Carved_Wood_Bench.png/180px-Carved_Wood_Bench.png",
         style: ["Bold","Rustic","Traditional"],
         furnitureCategory: ["Dining Room","Outdoors","Rec Room"],
         color: [],
@@ -33,6 +36,7 @@ const lionKingFurniture = [
     {
         furnitureName: `Carved Wood Table`,
         price: 8800,
+        image: "https://dreamlightvalleywiki.com/images/thumb/3/3e/Carved_Wood_Table.png/180px-Carved_Wood_Table.png",
         style: ["Bold","Rustic","Traditional"],
         furnitureCategory: ["Dining Room","Outdoors"],
         color: [],
@@ -42,6 +46,7 @@ const lionKingFurniture = [
     {
         furnitureName: `Carved Wooden Side Table`,
         price: 3520,
+        image: "https://dreamlightvalleywiki.com/images/thumb/8/8f/Carved_Wooden_Side_Table.png/180px-Carved_Wooden_Side_Table.png",
         style: ["Bold","Rustic","Traditional"],
         furnitureCategory: ["Dining Room","Study"],
         color: [],
@@ -51,6 +56,7 @@ const lionKingFurniture = [
     {
         furnitureName: `Carved Wooden Sofa`,
         price: 6000,
+        image: "https://dreamlightvalleywiki.com/images/thumb/d/d9/Carved_Wooden_Sofa.png/180px-Carved_Wooden_Sofa.png",
         style: ["Bold","Rustic","Traditional"],
         furnitureCategory: ["Bedroom","Living Room"],
         color: [],
@@ -60,6 +66,7 @@ const lionKingFurniture = [
     {
         furnitureName: `Clay Vase`,
         price: 2040,
+        image: "https://dreamlightvalleywiki.com/images/thumb/4/49/Clay_Vase.png/162px-Clay_Vase.png",
         style: ["Bold","Rustic","Traditional"],
         furnitureCategory: ["Decor","Dining Room","Study"],
         color: [],
@@ -69,6 +76,7 @@ const lionKingFurniture = [
     {
         furnitureName: `Cub-Emblazoned Dresser`,
         price: 6400,
+        image: "https://dreamlightvalleywiki.com/images/thumb/3/32/Cub-Emblazoned_Dresser.png/180px-Cub-Emblazoned_Dresser.png",
         style: ["Bold","Elegant","Rustic"],
         furnitureCategory: ["Bedroom","Living Room"],
         color: [],
@@ -79,6 +87,7 @@ const lionKingFurniture = [
     {
         furnitureName: `Fallen Log Perch`,
         price: 3000,
+        image: "https://dreamlightvalleywiki.com/images/thumb/5/5a/Fallen_Log_Perch.png/180px-Fallen_Log_Perch.png",
         style: ["Rustic"],
         furnitureCategory: ["Outdoors"],
         color: [],
@@ -88,6 +97,7 @@ const lionKingFurniture = [
     {
         furnitureName: `Floral Mirror`,
         price: 5440,
+        image: "https://dreamlightvalleywiki.com/images/thumb/4/47/Floral_Mirror.png/100px-Floral_Mirror.png",
         style: ["Bold","Elegant","Rustic"],
         furnitureCategory: ["Bathroom","Bedroom"],
         color: [],
@@ -97,6 +107,7 @@ const lionKingFurniture = [
     {
         furnitureName: `Giraffe Lamp`,
         price: 5440,
+        image: "https://dreamlightvalleywiki.com/images/thumb/5/58/Giraffe_Lamp.png/99px-Giraffe_Lamp.png",
         style: ["Bold","Elegant","Rustic"],
         furnitureCategory: ["Bedroom","Living Room"],
         color: [],
@@ -106,6 +117,7 @@ const lionKingFurniture = [
     {
         furnitureName: `Leaf Pile Rug`,
         price: 600,
+        image: "https://dreamlightvalleywiki.com/images/thumb/5/56/Leaf_Pile_Rug.png/180px-Leaf_Pile_Rug.png",
         style: ["Rustic"],
         furnitureCategory: ["Outdoors"],
         color: [],
@@ -115,6 +127,7 @@ const lionKingFurniture = [
     {
         furnitureName: `Lion Wall Sculpture`,
         price: 20400,
+        image: "https://dreamlightvalleywiki.com/images/thumb/4/40/Lion_Wall_Sculpture.png/180px-Lion_Wall_Sculpture.png",
         obtainedBy: "Scar Friendship Level 5",
         style: ["Bold","Rustic","Traditional"],
         furnitureCategory: ["Dining Room","Decor"],
@@ -125,6 +138,7 @@ const lionKingFurniture = [
     {
         furnitureName: `Nightstand`,
         price: 3520,
+        image: "https://dreamlightvalleywiki.com/images/thumb/9/93/Nightstand.png/180px-Nightstand.png",
         style: ["Bold","Elegant","Rustic"],
         furnitureCategory: ["Bedroom","Living Room"],
         color: [],
@@ -134,6 +148,7 @@ const lionKingFurniture = [
     {
         furnitureName: `Paw Print Rock Slab`,
         price: 13600,
+        image: "https://dreamlightvalleywiki.com/images/d/dc/Paw_Print_Rock_Slab.png",
         obtainedBy: "Scar Friendship Level 8",
         style: ["Bold","Quirky","Rustic"],
         furnitureCategory: ["Bedroom","Living Room","Outdoors","Rec Room"],
@@ -144,6 +159,7 @@ const lionKingFurniture = [
     {
         furnitureName: `Rafiki's Bowl`,
         price: 900,
+        image: "https://dreamlightvalleywiki.com/images/thumb/e/e3/Rafiki%27s_Bowl.png/171px-Rafiki%27s_Bowl.png",
         style: ["Rustic"],
         furnitureCategory: ["Bathroom","Kitchen","Outdoors"],
         color: [],
@@ -153,6 +169,7 @@ const lionKingFurniture = [
     {
         furnitureName: `Scar's Lounging Rock`,
         price: 4600,
+        image: "https://dreamlightvalleywiki.com/images/thumb/c/c1/Scar%27s_Lounging_Rock.png/180px-Scar%27s_Lounging_Rock.png",
         style: ["Rustic"],
         furnitureCategory: ["Outdoors"],
         color: [],
@@ -162,6 +179,7 @@ const lionKingFurniture = [
     {
         furnitureName: `Trunk Column`,
         price: 6400,
+        image: "https://dreamlightvalleywiki.com/images/thumb/e/e0/Trunk_Column.png/62px-Trunk_Column.png",
         style: ["Bold","Elegant","Rustic"],
         furnitureCategory: ["Bedroom","Living Room"],
         color: [],
@@ -171,6 +189,7 @@ const lionKingFurniture = [
     {
         furnitureName: `Vine Wall`,
         price: 5760,
+        image: "https://dreamlightvalleywiki.com/images/thumb/f/f8/Vine_Wall.png/109px-Vine_Wall.png",
         obtainedBy: "Furniture Pouch",
         style: ["Rustic"],
         furnitureCategory: ["Outdoor"],
@@ -182,6 +201,7 @@ const lionKingFurniture = [
         furnitureName: `Volcanic Stove`,
         // unlikely this is correct
         price: 1,
+        image: "https://dreamlightvalleywiki.com/images/2/2b/Volcanic_Stove.png",
         obtainedBy: "The Making of a Monarch (Scar Level 10)",
         style: ["Bold","Rustic"],
         furnitureCategory: ["Outdoors"],
@@ -192,6 +212,7 @@ const lionKingFurniture = [
     {
         furnitureName: `Waterfall`,
         price: 8000,
+        image: "https://dreamlightvalleywiki.com/images/thumb/a/a9/Waterfall.png/106px-Waterfall.png",
         style: ["Rustic"],
         furnitureCategory: ["Outdoors"],
         color: [],
@@ -201,6 +222,7 @@ const lionKingFurniture = [
     {
         furnitureName: `Watering Hole`,
         price: 20000,
+        image: "https://dreamlightvalleywiki.com/images/thumb/9/9c/Watering_Hole.png/180px-Watering_Hole.png",
         style: ["Rustic"],
         furnitureCategory: ["Outdoors"],
         color: [],
@@ -210,13 +232,24 @@ const lionKingFurniture = [
     {
         furnitureName: `Wildebeest Remains`,
         price: 16000,
+        image: "https://dreamlightvalleywiki.com/images/thumb/2/23/Wildebeest_Remains.png/180px-Wildebeest_Remains.png",
         obtainedBy: "Scar Friendship Level 2",
         style: ["Bold","Rustic"],
         furnitureCategory: ["Outdoors"],
         color: [],
         universe: "Lion King",
         added: 1.1
-    }
+    },
+    // {
+    //     furnitureName: ``,
+    //     price: ,
+    //     image: "",
+    //     style: [],
+    //     furnitureCategory: [""],
+    //     color: [],
+    //     universe: "Lion King",
+    //     added: 1.3
+    // },
 ]
 
 module.exports=lionKingFurniture
