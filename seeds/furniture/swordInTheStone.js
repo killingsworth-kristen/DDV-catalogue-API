54cff1a29347b85fcb823ea505c8b78a773e9b12
killingsworth.kristen <killingsworth_kristen@yahoo.com>
2022-12-25 22:25:52 -0500

    images added to sword and stone furniture

--- seeds/furniture/swordInTheStone.js
+++ seeds/furniture/swordInTheStone.js
@@ -4,8 +4,8 @@ const SwordStoneFurniture =[
     {
         furnitureName: `Bag of Tricks`,
         price: 3200,
-        obtainedBy: "Merlin Friendship Level 2",
         image: "https://dreamlightvalleywiki.com/images/thumb/e/e4/Bag_of_Tricks.png/180px-Bag_of_Tricks.png",
+        obtainedBy: "Merlin Friendship Level 2",
         style: ["Quirky","Traditional"],
         furnitureCategory: ["Study"],
         color: [],
@@ -15,6 +15,7 @@ const SwordStoneFurniture =[
     {
         furnitureName: `Big Pile of Books`,
         price: 6400,
+        image: "https://dreamlightvalleywiki.com/images/thumb/0/00/Big_Pile_of_Books.png/134px-Big_Pile_of_Books.png",
         tracked: false,
         style: ["Quirky","Traditional","Simple","Playful","Delicate","Wondrous"],
         furnitureCategory: ["Study"],
@@ -25,6 +26,7 @@ const SwordStoneFurniture =[
     {
         furnitureName: `Book Pile`,
         price: 900,
+        image: "https://dreamlightvalleywiki.com/images/thumb/e/e6/Book_Pile.png/180px-Book_Pile.png",
         style: ["Professional","Traditional"],
         furnitureCategory: ["Bedroom","Study"],
         color: [],
@@ -34,6 +36,7 @@ const SwordStoneFurniture =[
     {
         furnitureName: `Book Piles`,
         price: 3200,
+        image: "https://dreamlightvalleywiki.com/images/2/2f/Book_Piles.png",
         tracked: false,
         style: ["Quirky","Traditional"],
         furnitureCategory: ["Study"],
@@ -44,6 +47,7 @@ const SwordStoneFurniture =[
     {
         furnitureName: `Line of Books`,
         price: 480,
+        image: "https://dreamlightvalleywiki.com/images/thumb/5/58/Line_of_Books.png/180px-Line_of_Books.png",
         style: [],
         furnitureCategory: ["Decor"],
         color: [],
@@ -53,6 +57,7 @@ const SwordStoneFurniture =[
     {
         furnitureName: `Melted Candles`,
         price: 360,
+        image: "https://dreamlightvalleywiki.com/images/thumb/3/34/Melted_Candles.png/165px-Melted_Candles.png",
         style: ["Professional","Traditional"],
         furnitureCategory: ["Bedroom","Study"],
         color: [],
@@ -63,6 +68,7 @@ const SwordStoneFurniture =[
     {
         furnitureName: `Mortar 'n' Pestle`,
         price: 360,
+        image: "https://dreamlightvalleywiki.com/images/0/01/Mortar_%27n%27_Pestle.png",
         style: ["Professional","Quirky","Rustic"],
         furnitureCategory: ["Study"],
         color: [],
@@ -72,6 +78,7 @@ const SwordStoneFurniture =[
     {
         furnitureName: `Old Armchair`,
         price: 900,
+        image: "https://dreamlightvalleywiki.com/images/thumb/a/a5/Old_Armchair.png/180px-Old_Armchair.png",
         tracked: false,
         style: ["Casual","Quirky"],
         furnitureCategory: ["Living Room","Rec Room","Study"],
@@ -82,6 +89,7 @@ const SwordStoneFurniture =[
     {
         furnitureName: `Open Book`,
         price: 1200,
+        image: "https://dreamlightvalleywiki.com/images/thumb/5/59/Open_Book.png/180px-Open_Book.png",
         style: ["Professional","Traditional"],
         furnitureCategory: ["Bedroom","Study"],
         color: [],
@@ -91,6 +99,7 @@ const SwordStoneFurniture =[
     {
         furnitureName: `Orntate Curtain`,
         price: 1200,
+        image: "https://dreamlightvalleywiki.com/images/thumb/c/c1/Ornate_Curtain.png/88px-Ornate_Curtain.png",
         style: [],
         furnitureCategory: ["Living Room","Study"],
         color: [],
@@ -100,6 +109,7 @@ const SwordStoneFurniture =[
     {
         furnitureName: `Phial Holder`,
         price: 1200,
+        image: "https://dreamlightvalleywiki.com/images/thumb/2/22/Phial_Holder.png/180px-Phial_Holder.png",
         obtainedBy: "Merlin Friendship Level 5",
         style: ["Professional","Quirky","Traditional"],
         furnitureCategory: ["Rec Room","Study"],
@@ -110,6 +120,7 @@ const SwordStoneFurniture =[
     {
         furnitureName: `Potion Cupboard`,
         price: 6400,
+        image: "https://dreamlightvalleywiki.com/images/thumb/3/32/Potion_Cupboard.png/180px-Potion_Cupboard.png",
         obtainedBy: "Merlin Friendship Level 8",
         style: ["Quirky","Rustic","Traditional"],
         furnitureCategory: ["Rec Room","Study"],
@@ -120,6 +131,7 @@ const SwordStoneFurniture =[
     {
         furnitureName: `Round-Bottomed Flask and Holder`,
         price: 900,
+        image: "https://dreamlightvalleywiki.com/images/thumb/9/9c/Round-Bottomed_Flask_and_Holder.png/180px-Round-Bottomed_Flask_and_Holder.png",
         style: ["Professional","Quirky"],
         furnitureCategory: ["Rec Room","Study"],
         color: [],
@@ -129,12 +141,23 @@ const SwordStoneFurniture =[
     {
         furnitureName: `Small Book Pile`,
         price: 360,
+        image: "https://dreamlightvalleywiki.com/images/thumb/2/26/Small_Book_Pile.png/180px-Small_Book_Pile.png",
         style: ["Professional","Traditional"],
         furnitureCategory: ["Bedroom","Study"],
         color: [],
         universe: "The Sword and the Stone",
         added: 1.0
-    }
+    },
+    // {
+    //     furnitureName: ``,
+    //     price: ,
+    //     image: "",
+    //     style: [],
+    //     furnitureCategory: [""],
+    //     color: [],
+    //     universe: "The Sword and the Stone",
+    //     added: 1.3
+    // }
 ]
 
 module.exports=SwordStoneFurniture
