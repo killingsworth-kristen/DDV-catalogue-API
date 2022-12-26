8a7eaffaea4fbf76ceec30f21ab38b0a42370f8e
killingsworth.kristen <killingsworth_kristen@yahoo.com>
2022-12-25 22:03:35 -0500

    images added to moana furniture

--- seeds/furniture/moana.js
+++ seeds/furniture/moana.js
@@ -5,6 +5,7 @@ const moanaFurniture = [
     {
         furnitureName: `Beach Banquet`,
         price: 20000,
+        image: "https://dreamlightvalleywiki.com/images/thumb/6/6c/Beach_Banquet.png/180px-Beach_Banquet.png",
         style: ["Casual","Elegant","Rustic"],
         furnitureCategory: ["Outdoors","Decor"],
         color: [],
@@ -14,6 +15,7 @@ const moanaFurniture = [
     {
         furnitureName: `Beadlet Anemones`,
         price: 2160,
+        image: "https://dreamlightvalleywiki.com/images/thumb/0/01/Beadlet_Anemones.png/180px-Beadlet_Anemones.png",
         style: ["Bold","Quirky"],
         furnitureCategory: ["Outdoors","Decor"],
         color: [],
@@ -23,6 +25,7 @@ const moanaFurniture = [
     {
         furnitureName: `Bioluminescent Palm`,
         price: 7200,
+        image: "https://dreamlightvalleywiki.com/images/thumb/c/c6/Bioluminescent_Palm.png/180px-Bioluminescent_Palm.png",
         style: ["Bold","Quirky"],
         furnitureCategory: ["Outdoors","Decor"],
         color: [],
@@ -32,6 +35,7 @@ const moanaFurniture = [
     {
         furnitureName: `Canopied Couch`,
         price: 4800,
+        image: "https://dreamlightvalleywiki.com/images/thumb/a/a9/Canopied_Couch.png/180px-Canopied_Couch.png",
         obtainedBy: "Furniture Pouch",
         style: ["Casual","Cute"],
         furnitureCategory: ["Outdoors","Seating"],
@@ -42,6 +46,7 @@ const moanaFurniture = [
     {
         furnitureName: `Circular Rocky Pool`,
         price: 24000,
+        image: "https://dreamlightvalleywiki.com/images/thumb/1/1b/Circular_Rocky_Pool.png/180px-Circular_Rocky_Pool.png",
         style: ["Casual","Elegant","Rustic"],
         furnitureCategory: ["Outdoors","Decor"],
         color: [],
@@ -51,6 +56,7 @@ const moanaFurniture = [
     {
         furnitureName: `Coconut Beverage`,
         price: 900,
+        image: "https://dreamlightvalleywiki.com/images/thumb/f/f0/Coconut_Beverage.png/146px-Coconut_Beverage.png",
         style: ["Casual","Cute"],
         furnitureCategory: ["Kitchen","Outdoors","Decor"],
         color: [],
@@ -60,6 +66,7 @@ const moanaFurniture = [
     {
         furnitureName: `Coconut Setting`,
         price: 900,
+        image: "https://dreamlightvalleywiki.com/images/e/e3/Coconut_Setting.png",
         style: ["Rustic","Traditional"],
         furnitureCategory: ["Dining Room","Kitchen","Outdoors","Decor"],
         color: [],
@@ -69,6 +76,7 @@ const moanaFurniture = [
     {
         furnitureName: `Conch Shell`,
         price: 900,
+        image: "https://dreamlightvalleywiki.com/images/thumb/e/ed/Conch_Shell.png/180px-Conch_Shell.png",
         obtainedBy: "Moana Friendship Level 8",
         style: ["Elegant","Rustic","Traditional"],
         furnitureCategory: ["Bathroom","Outdoors","Study","Decor"],
@@ -79,6 +87,7 @@ const moanaFurniture = [
     {
         furnitureName: `Cooking Fire Pit`,
         price: 6400,
+        image: "https://dreamlightvalleywiki.com/images/thumb/9/9a/Cooking_Fire_Pit.png/180px-Cooking_Fire_Pit.png",
         obtainedBy: "Moana Friendship Level 5",
         style: ["Rustic","Traditional"],
         furnitureCategory: ["Outdoors","Decor"],
@@ -89,6 +98,7 @@ const moanaFurniture = [
     {
         furnitureName: `Coral, Shells, and Rock Pillar`,
         price: 6400,
+        image: "https://dreamlightvalleywiki.com/images/thumb/1/13/Coral%2C_Shells_and_Rock_Pillar.png/180px-Coral%2C_Shells_and_Rock_Pillar.png",
         style: ["Bold","Quirky"],
         furnitureCategory: ["Outdoors","Decor"],
         color: [],
@@ -98,6 +108,7 @@ const moanaFurniture = [
     {
         furnitureName: `Corkscrew Anemone`,
         price: 2800,
+        image: "https://dreamlightvalleywiki.com/images/thumb/6/6e/Corkscrew_Anemone.png/164px-Corkscrew_Anemone.png",
         style: ["Bold","Quirky"],
         furnitureCategory: ["Outdoors","Decor"],
         color: [],
@@ -107,6 +118,7 @@ const moanaFurniture = [
     {
         furnitureName: `Engraved Stone Partition`,
         // price: not available,
+        image: "https://dreamlightvalleywiki.com/images/thumb/2/24/Engraved_Stone_Partition.png/129px-Engraved_Stone_Partition.png",
         obtainedBy: "Furniture Pouch",
         style: [],
         furnitureCategory: ["Decor"],
@@ -117,6 +129,7 @@ const moanaFurniture = [
     {
         furnitureName: `Fan Coral`,
         price: 2880,
+        image: "https://dreamlightvalleywiki.com/images/thumb/5/50/Fan_Coral.png/180px-Fan_Coral.png",
         style: ["Bold","Quirky"],
         furnitureCategory: ["Outdoors","Decor"],
         color: [],
@@ -126,6 +139,7 @@ const moanaFurniture = [
     {
         furnitureName: `Giant Clam`,
         price: 6800,
+        image: "https://dreamlightvalleywiki.com/images/thumb/a/a4/Giant_Clam.png/180px-Giant_Clam.png",
         style: ["Bold","Quirky"],
         furnitureCategory: ["Outdoors","Decor"],
         color: [],
@@ -135,6 +149,7 @@ const moanaFurniture = [
     {
         furnitureName: `Handwoven Palm Basket`,
         price: 900,
+        image: "https://dreamlightvalleywiki.com/images/thumb/8/82/Handwoven_Palm_Basket.png/162px-Handwoven_Palm_Basket.png",
         style: ["Rustic","Traditional"],
         furnitureCategory: ["Kitchen","Outdoors","Decor"],
         color: [],
@@ -144,6 +159,7 @@ const moanaFurniture = [
     {
         furnitureName: `Hanging Wicker Light`,
         price: 2720,
+        image: "https://dreamlightvalleywiki.com/images/thumb/0/0b/Hanging_Wicker_Light.png/87px-Hanging_Wicker_Light.png",
         style: ["Modern","Rustic"],
         furnitureCategory: ["Dining Room","Kitchen","Living Room","Lighting"],
         color: [],
@@ -153,6 +169,7 @@ const moanaFurniture = [
     {
         furnitureName: `Heat Vent`,
         price: 3200,
+        image: "https://dreamlightvalleywiki.com/images/thumb/3/3a/Heat_Vent.png/180px-Heat_Vent.png",
         style: ["Bold","Quirky"],
         furnitureCategory: ["Outdoors","Decor"],
         color: [],
@@ -162,6 +179,7 @@ const moanaFurniture = [
     {
         furnitureName: `Island Drawings`,
         price: 2720,
+        image: "https://dreamlightvalleywiki.com/images/thumb/b/b0/Island_Drawings.png/180px-Island_Drawings.png",
         obtainedBy: "Maui Friendship Level 5",
         style: ["Cute","Rustic","Traditional"],
         furnitureCategory: ["Kitchen","Rec Room","Decor"],
@@ -172,6 +190,7 @@ const moanaFurniture = [
     {
         furnitureName: `Maui's #1 Fan`,
         // price: 1,
+        image: "https://dreamlightvalleywiki.com/images/thumb/f/fe/Maui%27s_num_1_Fan.png/180px-Maui%27s_num_1_Fan.png",
         obtainedBy: "The Heart of Dreamlight Valley (Maui Level 10)",
         style: ["Bold","Traditional"],
         furnitureCategory: ["Outdoors","Decor"],
@@ -182,6 +201,7 @@ const moanaFurniture = [
     {
         furnitureName: `Medium Fern Coral`,
         price: 2880,
+        image: "https://dreamlightvalleywiki.com/images/thumb/e/ed/Medium_Fern_Coral.png/180px-Medium_Fern_Coral.png",
         style: ["Bold","Quirky"],
         furnitureCategory: ["OUtdoors","Deor"],
         color: [],
@@ -191,6 +211,7 @@ const moanaFurniture = [
     {
         furnitureName: `Monster Tapestry`,
         price: 2720,
+        image: "https://dreamlightvalleywiki.com/images/thumb/9/98/Monster_Tapestry.png/132px-Monster_Tapestry.png",
         style: ["Rustic","Traditional"],
         furnitureCategory: ["Bedroom","Living Room","Study"],
         color: [],
@@ -200,6 +221,7 @@ const moanaFurniture = [
     {
         furnitureName: `Ornamental Palm Fronds`,
         price: 2160,
+        image: "https://dreamlightvalleywiki.com/images/thumb/0/0c/Ornamental_Palm_Fronds.png/172px-Ornamental_Palm_Fronds.png",
         style: ["Elegant","Modern"],
         furnitureCategory: ["Bathroom","Kitchen","Living Room","Decor"],
         color: [],
@@ -209,6 +231,7 @@ const moanaFurniture = [
     {
         furnitureName: `Round Side Table`,
         price: 3520,
+        image: "https://dreamlightvalleywiki.com/images/thumb/0/0b/Round_Side_Table.png/180px-Round_Side_Table.png",
         style: ["Casual","Elegant","Rustic"],
         furnitureCategory: ["Living Room","Outdoors","Tables"],
         color: [],
@@ -218,6 +241,7 @@ const moanaFurniture = [
     {
         furnitureName: `Sea Snail Shell`,
         price: 2040,
+        image: "https://dreamlightvalleywiki.com/images/thumb/6/6b/Sea_Snail_Shell.png/109px-Sea_Snail_Shell.png",
         style: ["Bold","Quirky"],
         furnitureCategory: ["Outdoors","Decor"],
         color: [],
@@ -227,6 +251,7 @@ const moanaFurniture = [
     {
         furnitureName: `Small Fern Coral`,
         price: 2880,
+        image: "https://dreamlightvalleywiki.com/images/thumb/c/c1/Small_Fern_Coral.png/180px-Small_Fern_Coral.png",
         style: ["Bold","Quirky"],
         furnitureCategory: ["Outdoors","Decor"],
         color: [],
@@ -236,6 +261,7 @@ const moanaFurniture = [
     {
         furnitureName: `Stone Sink`,
         price: 6400,
+        image: "https://dreamlightvalleywiki.com/images/thumb/5/5f/Stone_Sink.png/180px-Stone_Sink.png",
         style: ["Elegant","Modern","Rustic"],
         furnitureCategory: ["Kitchen","Tables"],
         color: [],
@@ -245,6 +271,7 @@ const moanaFurniture = [
     {
         furnitureName: `Tall Torch`,
         price: 2040,
+        image: "https://dreamlightvalleywiki.com/images/thumb/5/52/Tall_Torch.png/63px-Tall_Torch.png",
         style: ["Rustic","Traditional"],
         furnitureCategory: ["Outdoors","Lighting"],
         color: [],
@@ -254,6 +281,7 @@ const moanaFurniture = [
     {
         furnitureName: `Te Kā Tapestry`,
         price: 2720,
+        image: "https://dreamlightvalleywiki.com/images/thumb/6/6e/Te_K%C4%81_Tapestry.png/139px-Te_K%C4%81_Tapestry.png",
         style: ["Rustic","Traditional"],
         furnitureCategory: ["Bedroom","Living Room","Study","Decor"],
         color: [],
@@ -264,6 +292,7 @@ const moanaFurniture = [
     {
         furnitureName: `Three-Panel Windows`,
         price: 4800,
+        image: "https://dreamlightvalleywiki.com/images/thumb/c/ce/Three-Panel_Windows.png/180px-Three-Panel_Windows.png",
         style: ["Casual","Elegant","Modern"],
         furnitureCategory: ["Dining Room","Kitchen","Living Room","Decor"],
         color: [],
@@ -273,6 +302,7 @@ const moanaFurniture = [
     {
         furnitureName: `Tropical Fruit Bowl`,
         price: 2400,
+        image: "https://dreamlightvalleywiki.com/images/thumb/6/6c/Tropical_Fruit_Bowl.png/180px-Tropical_Fruit_Bowl.png",
         obtainedBy: "Maui Friendship Level 8",
         style: ["Casual","Elegant","Modern"],
         furnitureCategory: ["Kitchen","Decor"],
@@ -283,6 +313,7 @@ const moanaFurniture = [
     {
         furnitureName: `Tropical Lounge Chair`,
         price: 2400,
+        image: "https://dreamlightvalleywiki.com/images/thumb/b/ba/Tropical_Lounge_Chair.png/180px-Tropical_Lounge_Chair.png",
         style: ["Casual","Cute"],
         furnitureCategory: ["Outdoors","Seating"],
         color: [],
@@ -292,13 +323,23 @@ const moanaFurniture = [
     {
         furnitureName: `Wicker Bar Stool`,
         price: 1800,
+        image: "https://dreamlightvalleywiki.com/images/thumb/f/f6/Wicker_Bar_Stool.png/123px-Wicker_Bar_Stool.png",
         style: ["Elegant","Modern","Rustic"],
         furnitureCategory: ["Kitchen","Outdoors","Seating"],
         color: [],
         universe:"Moana",
         added: 1.0
     },
-
+    // {
+    //     furnitureName: ``,
+    //     price: ,
+    //     image: "",
+    //     style: [],
+    //     furnitureCategory: [""],
+    //     color: [],
+    //     universe:"Moana",
+    //     added: 1.3
+    // },
 ]
 
 module.exports=moanaFurniture
