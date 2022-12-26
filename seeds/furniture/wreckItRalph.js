6722be4333eab1139842cb0c13e5cafa1ec78ac6
killingsworth.kristen <killingsworth_kristen@yahoo.com>
2022-12-25 22:58:52 -0500

    images added to wreck it raplph fruniture

--- seeds/furniture/wreckItRalph.js
+++ seeds/furniture/wreckItRalph.js
@@ -5,7 +5,7 @@ const wreckItRaplhFurniture = [
     {
         furnitureName: `Abstract Chandelier`,
         price: 1020,
-        obtainedBy: "Scrooge's Store",
+        image: "https://dreamlightvalleywiki.com/images/thumb/7/72/Abstract_Chandelier.png/83px-Abstract_Chandelier.png",
         style: ["Professional","Modern","Quirky"],
         furnitureCategory: ["Study","Bathroom","Dining Room","Lighting"],
         color: [],
@@ -15,7 +15,7 @@ const wreckItRaplhFurniture = [
     {
         furnitureName: `Abstract Neon Sign`,
         price: 2720,
-        obtainedBy: "Scrooge's Store",
+        image: "https://dreamlightvalleywiki.com/images/f/f7/Abstract_Neon_Sign.png",
         style: ["Modern","Casual"],
         furnitureCategory: ["Rec Room","Bedroom","Living Room","Decor"],
         color: [],
@@ -25,7 +25,7 @@ const wreckItRaplhFurniture = [
     {
         furnitureName: `Accent Chair`,
         price: 2400,
-        obtainedBy: "Scrooge's Store",
+        image: "https://dreamlightvalleywiki.com/images/thumb/5/53/Accent_Chair.png/135px-Accent_Chair.png",
         style: ["Professional","Modern","Quirky"],
         furnitureCategory: ["Study","Living Room","Seating"],
         color: [],
@@ -35,7 +35,7 @@ const wreckItRaplhFurniture = [
     {
         furnitureName: `Air Hockey Table`,
         price: 6400,
-        obtainedBy: "Scrooge's Store",
+        image: "https://dreamlightvalleywiki.com/images/thumb/b/b0/Air_Hockey_Table.png/180px-Air_Hockey_Table.png",
         style: ["Modern","Casual"],
         furnitureCategory: ["Rec Room","Decor"],
         color: [],
@@ -45,7 +45,7 @@ const wreckItRaplhFurniture = [
     {
         furnitureName: `Awesome Online Awards`,
         price: 3200,
-        obtainedBy: "Scrooge's Store",
+        image: "https://dreamlightvalleywiki.com/images/thumb/8/88/Awesome_Online_Awards.png/178px-Awesome_Online_Awards.png",
         style: ["Professional","Modern","Quirky"],
         furnitureCategory: ["Study","Bedroom","Rec Room","Decor"],
         color: [],
@@ -55,7 +55,7 @@ const wreckItRaplhFurniture = [
     {
         furnitureName: `Bamboo Shoots`,
         price: 2880,
-        obtainedBy: "Scrooge's Store",
+        image: "https://dreamlightvalleywiki.com/images/thumb/c/cc/Bamboo_Shoots.png/68px-Bamboo_Shoots.png",
         style: ["Modern"],
         furnitureCategory: ["Living Room","Bathroom","Study","Decor"],
         color: [],
@@ -65,7 +65,7 @@ const wreckItRaplhFurniture = [
     {
         furnitureName: `Candy Bed`,
         price: 9200,
-        obtainedBy: "Scrooge's Store",
+        image: "https://dreamlightvalleywiki.com/images/thumb/c/c2/Candy_Bed.png/180px-Candy_Bed.png",
         style: ["Modern","Cute"],
         furnitureCategory: ["Bedroom","Beds"],
         color: [],
@@ -76,7 +76,7 @@ const wreckItRaplhFurniture = [
     {
         furnitureName: `Coin-op Game`,
         price: 210,
-        obtainedBy: "Scrooge's Store",
+        image: "https://dreamlightvalleywiki.com/images/thumb/0/02/Coin-op_Game.png/180px-Coin-op_Game.png",
         style: ["Modern","Casual"],
         furnitureCategory: ["Rec Room","Decor"],
         color: [],
@@ -86,7 +86,7 @@ const wreckItRaplhFurniture = [
     {
         furnitureName: `Counter Seat`,
         price: 900,
-        obtainedBy: "Scrooge's Store",
+        image: "https://dreamlightvalleywiki.com/images/thumb/4/4b/Counter_Seat.png/149px-Counter_Seat.png",
         style: ["Modern"],
         furnitureCategory: ["Living Room","Study","Seating"],
         color: [],
@@ -96,7 +96,7 @@ const wreckItRaplhFurniture = [
     {
         furnitureName: `Curvy Couch`,
         price: 6000,
-        obtainedBy: "Scrooge's Store",
+        image: "https://dreamlightvalleywiki.com/images/thumb/7/72/Curvy_Couch.png/180px-Curvy_Couch.png",
         style: ["Professional","Modern","Quirky"],
         furnitureCategory: ["Study","Living Room","Seating"],
         color: [],
@@ -106,7 +106,7 @@ const wreckItRaplhFurniture = [
     {
         furnitureName: `Desktop Computer`,
         price: 288,
-        obtainedBy: "Scrooge's Store",
+        image: "https://dreamlightvalleywiki.com/images/thumb/9/97/Desktop_Computer.png/180px-Desktop_Computer.png",
         style: ["Modern","Cute"],
         furnitureCategory: ["Bedroom","Study","Electronics"],
         color: [],
@@ -116,7 +116,7 @@ const wreckItRaplhFurniture = [
     {
         furnitureName: `Donut Chair`,
         price: 4800,
-        obtainedBy: "Scrooge's Store",
+        image: "https://dreamlightvalleywiki.com/images/thumb/3/38/Donut_Chair.png/180px-Donut_Chair.png",
         style: ["Modern","Cute"],
         furnitureCategory: ["Bedroom","Living Room","Rec Room","Seating"],
         color: [],
@@ -126,7 +126,7 @@ const wreckItRaplhFurniture = [
     {
         furnitureName: `Drink Cabinet`,
         price: 3200,
-        obtainedBy: "Scrooge's Store",
+        image: "https://dreamlightvalleywiki.com/images/thumb/4/40/Drink_Cabinet.png/130px-Drink_Cabinet.png",
         style: ["Modern"],
         furnitureCategory: ["Living Room","Study","Decor"],
         color: [],
@@ -136,7 +136,7 @@ const wreckItRaplhFurniture = [
     {
         furnitureName: `Exec Desk`,
         price: 8800,
-        obtainedBy: "Scrooge's Store",
+        image: "https://dreamlightvalleywiki.com/images/thumb/e/e2/Exec_Desk.png/180px-Exec_Desk.png",
         style: ["Professional","Modern","Quirky"],
         furnitureCategory: ["Study","Tables"],
         color: [],
@@ -146,7 +146,7 @@ const wreckItRaplhFurniture = [
     {
         furnitureName: `Fancy Desk Chair`,
         price: 2400,
-        obtainedBy: "Scrooge's Store",
+        image: "https://dreamlightvalleywiki.com/images/thumb/a/af/Fancy_Desk_Chair.png/95px-Fancy_Desk_Chair.png",
         style: ["Professional","Modern","Quirky"],
         furnitureCategory: ["Study","Dining Room","Seating"],
         color: [],
@@ -156,7 +156,7 @@ const wreckItRaplhFurniture = [
     {
         furnitureName: `Food Stall Counter`,
         price: 20000,
-        obtainedBy: "Scrooge's Store",
+        image: "https://dreamlightvalleywiki.com/images/thumb/0/0c/Food_Stall_Counter.png/180px-Food_Stall_Counter.png",
         style: ["Modern","Casual"],
         furnitureCategory: ["Rec Room","Kitchen","Decor"],
         color: [],
@@ -166,7 +166,7 @@ const wreckItRaplhFurniture = [
     {
         furnitureName: `Freestanding Fireplace`,
         price: 6400,
-        obtainedBy: "Scrooge's Store",
+        image: "https://dreamlightvalleywiki.com/images/thumb/9/95/Freestanding_Fireplace.png/49px-Freestanding_Fireplace.png",
         style: ["Modern","Bold"],
         furnitureCategory: ["Living Room","Study","Dining Room","Decor"],
         color: [],
@@ -176,7 +176,7 @@ const wreckItRaplhFurniture = [
     {
         furnitureName: `Game Machine`,
         price: 16000,
-        obtainedBy: "Scrooge's Store",
+        image: "https://dreamlightvalleywiki.com/images/thumb/f/fa/Game_Machine.png/180px-Game_Machine.png",
         style: [],
         furnitureCategory: ["Decor"],
         color: [],
@@ -186,7 +186,7 @@ const wreckItRaplhFurniture = [
     {
         furnitureName: `Gamer Desk`,
         price: 3520,
-        obtainedBy: "Scrooge's Store",
+        image: "https://dreamlightvalleywiki.com/images/thumb/f/fa/Gamer_Desk.png/180px-Gamer_Desk.png",
         style: ["Modern","Cute"],
         furnitureCategory: ["Bedroom","Study","Tables"],
         color: [],
@@ -196,7 +196,7 @@ const wreckItRaplhFurniture = [
     {
         furnitureName: `Geometric Coffee Table`,
         price: 1320,
-        obtainedBy: "Scrooge's Store",
+        image: "https://dreamlightvalleywiki.com/images/thumb/e/e0/Geometric_Coffee_Table.png/180px-Geometric_Coffee_Table.png",
         style: ["Professional","Modern","Quirky"],
         furnitureCategory: ["Study","Living Room","Tables"],
         color: [],
@@ -206,7 +206,7 @@ const wreckItRaplhFurniture = [
     {
         furnitureName: `Glass Shelves`,
         price: 3200,
-        obtainedBy: "Scrooge's Store",
+        image: "https://dreamlightvalleywiki.com/images/thumb/5/59/Glass_Shelves.png/180px-Glass_Shelves.png",
         style: ["Modern"],
         furnitureCategory: ["Living Room","Study","Decor"],
         color: [],
@@ -216,7 +216,7 @@ const wreckItRaplhFurniture = [
     {
         furnitureName: `Holographic Bar Stool`,
         price: 1800,
-        obtainedBy: "Scrooge's Store",
+        image: "https://dreamlightvalleywiki.com/images/thumb/c/cf/Holographic_Bar_Stool.png/180px-Holographic_Bar_Stool.png",
         style: ["Modern","Casual"],
         furnitureCategory: ["Rec Room","Kitchen","Seating"],
         color: [],
@@ -226,7 +226,7 @@ const wreckItRaplhFurniture = [
     {
         furnitureName: `Indoor Vertical Fountain`,
         price: 6400,
-        obtainedBy: "Scrooge's Store",
+        image: "https://dreamlightvalleywiki.com/images/thumb/4/48/Indoor_Vertical_Fountain.png/85px-Indoor_Vertical_Fountain.png",
         style: ["Bold","Modern"],
         furnitureCategory: ["Bathroom","Living Room","Study","Decor"],
         color: [],
@@ -236,7 +236,7 @@ const wreckItRaplhFurniture = [
     {
         furnitureName: `L-Counter`,
         price: 4400,
-        obtainedBy: "Scrooge's Store",
+        image: "https://dreamlightvalleywiki.com/images/thumb/c/c5/L-Counter.png/180px-L-Counter.png",
         style: ["Modern"],
         furnitureCategory: ["Living Room","Study","Tables"],
         color: [],
@@ -246,7 +246,7 @@ const wreckItRaplhFurniture = [
     {
         furnitureName: `Modern Wall Sconce`,
         price: 765,
-        obtainedBy: "Scrooge's Store",
+        image: "https://dreamlightvalleywiki.com/images/thumb/f/f4/Modern_Wall_Sconce.png/180px-Modern_Wall_Sconce.png",
         style: ["Modern"],
         furnitureCategory: ["Living Room","Study","Dining Room","Lighting"],
         color: [],
@@ -256,7 +256,7 @@ const wreckItRaplhFurniture = [
     {
         furnitureName: `Nightlight`,
         price: 2040,
-        obtainedBy: "Scrooge's Store",
+        image: "https://dreamlightvalleywiki.com/images/thumb/7/71/Nightlight.png/180px-Nightlight.png",
         style: ["Modern","Cute"],
         furnitureCategory: ["Bedroom","Lighting"],
         color: [],
@@ -266,7 +266,7 @@ const wreckItRaplhFurniture = [
     {
         furnitureName: `Octagonal Mirror`,
         price: 3200,
-        obtainedBy: "Scrooge's Store",
+        image: "https://dreamlightvalleywiki.com/images/thumb/d/d5/Octagonal_Mirror.png/159px-Octagonal_Mirror.png",
         style: ["Professional","Modern","Quirky"],
         furnitureCategory: ["Study","Bathroom","Bedroom","Decor"],
         color: [],
@@ -276,7 +276,7 @@ const wreckItRaplhFurniture = [
     {
         furnitureName: `Octagonal Side Table`,
         price: 3520,
-        obtainedBy: "Scrooge's Store",
+        image: "https://dreamlightvalleywiki.com/images/thumb/6/60/Octagonal_Side_Table.png/130px-Octagonal_Side_Table.png",
         style: ["Prfessional","Modern","Quirky"],
         furnitureCategory: ["Study","Living Room","Bedroom","Tables"],
         color: [],
@@ -286,7 +286,7 @@ const wreckItRaplhFurniture = [
     {
         furnitureName: `Pink Sweet Tooth Rug`,
         price: 1200,
-        obtainedBy: "Scrooge's Store",
+        image: "https://dreamlightvalleywiki.com/images/thumb/6/65/Pink_Sweet_Tooth_Rug.png/180px-Pink_Sweet_Tooth_Rug.png",
         style: ["Cute","Casual"],
         furnitureCategory: ["Bathroom","Bedroom","Study","Rugs"],
         color: [],
@@ -296,7 +296,7 @@ const wreckItRaplhFurniture = [
     {
         furnitureName: `Pixelated Window`,
         price: 3200,
-        obtainedBy: "Scrooge's Store",
+        image: "https://dreamlightvalleywiki.com/images/thumb/8/88/Pixelated_Window.png/73px-Pixelated_Window.png",
         style: ["Modern"],
         furnitureCategory: ["Living Room","Study","Bedroom","Windows"],
         color: [],
@@ -306,7 +306,7 @@ const wreckItRaplhFurniture = [
     {
         furnitureName: `Shelving Unit`,
         price: 3200,
-        obtainedBy: "Scrooge's Store",
+        image: "https://dreamlightvalleywiki.com/images/thumb/2/22/Shelving_Unit.png/154px-Shelving_Unit.png",
         style: ["Modern"],
         furnitureCategory: ["Living Room","Study","Bedroom","Decor"],
         color: [],
@@ -316,7 +316,7 @@ const wreckItRaplhFurniture = [
     {
         furnitureName: `Sugar Rush Plushie Shelf`,
         price: 6400,
-        obtainedBy: "Scrooge's Store",
+        image: "https://dreamlightvalleywiki.com/images/thumb/5/5a/Sugar_Rush_Plushie_Shelf.png/137px-Sugar_Rush_Plushie_Shelf.png",
         style: ["Modern","Cute"],
         furnitureCategory: ["Bedroom","Decor","Wall"],
         color: [],
@@ -326,7 +326,7 @@ const wreckItRaplhFurniture = [
     {
         furnitureName: `Vanellope Portraits`,
         price: 2720,
-        obtainedBy: "Scrooge's Store",
+        image: "https://dreamlightvalleywiki.com/images/thumb/7/7a/Vanellope_Portraits.png/144px-Vanellope_Portraits.png",
         style: ["Modern"],
         furnitureCategory: ["Living Room","Study","Dining Room","Decor"],
         color: [],
@@ -336,7 +336,7 @@ const wreckItRaplhFurniture = [
     {
         furnitureName: `Virtual Desktop`,
         price: 5760,
-        obtainedBy: "Scrooge's Store",
+        image: "https://dreamlightvalleywiki.com/images/thumb/c/cf/Virtual_Desktop.png/180px-Virtual_Desktop.png",
         style: ["Modern","Professional","Quirky"],
         furnitureCategory: ["Study","Bedroom","Decor"],
         color: [],
@@ -347,13 +347,23 @@ const wreckItRaplhFurniture = [
     {
         furnitureName: `Winner's Gamer Chair`,
         price: 4800,
-        obtainedBy: "Scrooge's Store",
+        image: "https://dreamlightvalleywiki.com/images/thumb/5/50/Winner%27s_Gamer_Chair.png/106px-Winner%27s_Gamer_Chair.png",
         style: ["Modern","Cute"],
         furnitureCategory: ["Bedroom","Study","Seating"],
         color: [],
         universe:"Wreck it Ralph",
         added: 1.0
     },
+    // {
+    //     furnitureName: ``,
+    //     price: ,
+    //     image: "",
+    //     style: [],
+    //     furnitureCategory: [""],
+    //     color: [],
+    //     universe:"Wreck it Ralph",
+    //     added: 1.3
+    // },
 ]
 
 module.exports=wreckItRaplhFurniture
