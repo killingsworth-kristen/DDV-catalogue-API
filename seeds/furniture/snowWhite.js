591a483d10376e30d57642fc42b5e7535cc8f5e1
killingsworth.kristen <killingsworth_kristen@yahoo.com>
2022-12-25 20:59:32 -0500

    images added to snow white furniture

--- seeds/furniture/snowWhite.js
+++ seeds/furniture/snowWhite.js
@@ -3,6 +3,7 @@ const snowWhiteFurniture = [
     {
         furnitureName: `Magic Mirror (On the Wall)`,
         price: 408,
+        image: "https://dreamlightvalleywiki.com/images/thumb/c/c6/Magic_Mirror_%28On_the_Wall%29.png/113px-Magic_Mirror_%28On_the_Wall%29.png",
         obtainedBy: "Villain's Star Path",
         style: ["Bold","Elegant"],
         furnitureCategory: ["Bedroom","Study"],
