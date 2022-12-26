2d6e5045db4b90425422a8292bbe3fc2b7c2b66f
killingsworth.kristen <killingsworth_kristen@yahoo.com>
2022-12-25 20:42:44 -0500

    images added to hercules furniture

--- seeds/furniture/hercules.js
+++ seeds/furniture/hercules.js
@@ -3,6 +3,7 @@ const herculesFurniture = [
     {
         furnitureName: `Well of Souls`,
         price: 300,
+        image: "https://dreamlightvalleywiki.com/images/thumb/9/96/Well_of_Souls.png/180px-Well_of_Souls.png",
         obtainedBy: "Villain's Star Path",
         style: ["Quirky"],
         furnitureCategory: ["Outdoors","Study"],
