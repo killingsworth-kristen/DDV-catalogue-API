19d884ae62da990d2cffba488d0468c633f33788
killingsworth.kristen <killingsworth_kristen@yahoo.com>
2022-12-25 22:32:43 -0500

    images added to tangled furniture

--- seeds/furniture/tangled.js
+++ seeds/furniture/tangled.js
@@ -3,8 +3,9 @@ const tangledFurniture = [
     {
         furnitureName: `Barrel Table`,
         price: 3520,
+        images: "https://dreamlightvalleywiki.com/images/thumb/1/12/Barrel_Table.png/180px-Barrel_Table.png",
         style: ["Bold","Rustic"],
-        furnitureCategory: ["Bedroom","Living Room","Study"],
+        furnitureCategory: ["Bedroom","Living Room","Study","Tables"],
         color: [],
         universe:"Tangled",
         added: 1.0
@@ -12,8 +13,9 @@ const tangledFurniture = [
     {
         furnitureName: `Bear Armchair`,
         price: 2400,
+        images: "https://dreamlightvalleywiki.com/images/thumb/a/aa/Bear_Armchair.png/111px-Bear_Armchair.png",
         style: ["Bold"],
-        furnitureCategory: ["Dining Room","Living Room","Rec Room","Study"],
+        furnitureCategory: ["Dining Room","Living Room","Rec Room","Study","Seating"],
         color: [],
         universe:"Tangled",
         added: 1.0
@@ -21,8 +23,9 @@ const tangledFurniture = [
     {
         furnitureName: `Cozy Couch`,
         price: 3000,
+        images: "https://dreamlightvalleywiki.com/images/thumb/1/1f/Cozy_Couch.png/180px-Cozy_Couch.png",
         style: ["Casual","Cute","Quirky"],
-        furnitureCategory: ["Bedroom","Living Room","Rec Room"],
+        furnitureCategory: ["Bedroom","Living Room","Rec Room","Seating"],
         color: [],
         universe:"Tangled",
         added: 1.0
@@ -30,8 +33,9 @@ const tangledFurniture = [
     {
         furnitureName: `Day & Night Paintings`,
         price: 1275,
+        images: "https://dreamlightvalleywiki.com/images/thumb/8/87/Day_%26_Night_Paintings.png/163px-Day_%26_Night_Paintings.png",
         style: ["Casual","Cute","Quirky"],
-        furnitureCategory: ["Rec Room","Study"],
+        furnitureCategory: ["Rec Room","Study","Decor"],
         color: [],
         universe:"Tangled",
         added: 1.0
@@ -39,8 +43,9 @@ const tangledFurniture = [
     {
         furnitureName: `Easel`,
         price: 2720,
+        images: "https://dreamlightvalleywiki.com/images/thumb/a/a7/Easel.png/83px-Easel.png",
         style: ["Casual","Cute","Quirky"],
-        furnitureCategory: ["Rec Room","Study"],
+        furnitureCategory: ["Rec Room","Study","Decor"],
         color: [],
         universe:"Tangled",
         added: 1.0
@@ -48,8 +53,9 @@ const tangledFurniture = [
     {
         furnitureName: `Guitar`,
         price: 2400,
+        images: "https://dreamlightvalleywiki.com/images/thumb/3/3f/Guitar.png/61px-Guitar.png",
         style: ["Casual","Cute","Quirky"],
-        furnitureCategory: ["Rec Room","Study"],
+        furnitureCategory: ["Rec Room","Study","Decor"],
         color: [],
         universe:"Tangled",
         added: 1.0
@@ -57,8 +63,9 @@ const tangledFurniture = [
     {
         furnitureName: `Helmet Lamp`,
         price: 2720,
+        images: "https://dreamlightvalleywiki.com/images/f/f8/Helmet_Lamp.png",
         style: ["Bold","Rustic"],
-        furnitureCategory: ["Living Room","Rec Room","Study"],
+        furnitureCategory: ["Living Room","Rec Room","Study","Lighting"],
         color: [],
         universe:"Tangled",
         added: 1.0
@@ -66,8 +73,9 @@ const tangledFurniture = [
     {
         furnitureName: `Homey Rug`,
         price: 4800,
+        images: "https://dreamlightvalleywiki.com/images/thumb/3/3b/Homey_Rug.png/180px-Homey_Rug.png",
         style: ["Bold","Rustic"],
-        furnitureCategory: ["Bedroom","Living Room","Study"],
+        furnitureCategory: ["Bedroom","Living Room","Study","Rugs"],
         color: [],
         universe:"Tangled",
         added: 1.0
@@ -75,8 +83,9 @@ const tangledFurniture = [
     {
         furnitureName: `Horned Tavern Fireplace`,
         price: 600,
+        images: "https://dreamlightvalleywiki.com/images/thumb/1/1b/Horned_Tavern_Fireplace.png/109px-Horned_Tavern_Fireplace.png",
         style: ["Bold","Rustic"],
-        furnitureCategory: ["Dining Room","Living Room","Rec Room"],
+        furnitureCategory: ["Dining Room","Living Room","Rec Room","Decor"],
         color: [],
         universe:"Tangled",
         added: 1.0
@@ -84,8 +93,9 @@ const tangledFurniture = [
     {
         furnitureName: `Lantern Garland`,
         price: 2720,
+        images: "https://dreamlightvalleywiki.com/images/thumb/8/87/Lantern_Garland.png/180px-Lantern_Garland.png",
         style: ["Casual","Cute","Quirky"],
-        furnitureCategory: ["Bedroom","Rec Room"],
+        furnitureCategory: ["Bedroom","Rec Room","Lighting"],
         color: [],
         universe:"Tangled",
         added: 1.0
@@ -93,8 +103,9 @@ const tangledFurniture = [
     {
         furnitureName: `Lounge Pillows`,
         price: 8000,
+        images: "https://dreamlightvalleywiki.com/images/1/10/Lounge_Pillows.png",
         style: ["Casual","Cute","Quirky"],
-        furnitureCategory: ["Bedroom","Living Room","Rec Room"],
+        furnitureCategory: ["Bedroom","Living Room","Rec Room","Decor"],
         color: [],
         universe:"Tangled",
         added: 1.0
@@ -102,9 +113,10 @@ const tangledFurniture = [
     {
         furnitureName: `Mother Gothel's Standing Mirror`,
         price: 16000,
+        images: "https://dreamlightvalleywiki.com/images/thumb/f/f1/Mother_Gothel%27s_Standing_Mirror.png/99px-Mother_Gothel%27s_Standing_Mirror.png",
         obtainedBy: "Village Project: Restoring the Sunstone (Mother Gothel Level 10)",
         style: ["Elegant","Traditional"],
-        furnitureCategory: ["Bathroom","Bedroom"],
+        furnitureCategory: ["Bathroom","Bedroom","Decor"],
         color: [],
         universe:"Tangled",
         added: 1.0
@@ -112,9 +124,9 @@ const tangledFurniture = [
     {
         furnitureName: `Painting Table`,
         // needs pricing data
-        obtainedBy: "Scrooge's Store",
+        images: "https://dreamlightvalleywiki.com/images/thumb/8/85/Painting_Table.png/180px-Painting_Table.png",
         style: ["Casual","Cutre","Quirky"],
-        furnitureCategory: ["Rec Room","Study"],
+        furnitureCategory: ["Rec Room","Study","Crafting Station"],
         color: [],
         universe:"Tangled",
         added: 1.0
@@ -122,8 +134,9 @@ const tangledFurniture = [
     {
         furnitureName: `Plain Bed`,
         price: 4600,
+        images: "https://dreamlightvalleywiki.com/images/thumb/4/4e/Plain_Bed.png/180px-Plain_Bed.png",
         style: ["Bold","Rustic"],
-        furnitureCategory: ["Bedroom"],
+        furnitureCategory: ["Bedroom","Beds"],
         color: [],
         universe:"Tangled",
         added: 1.0
@@ -131,8 +144,9 @@ const tangledFurniture = [
     {
         furnitureName: `Provincial Window`,
         price: 3200,
+        images: "https://dreamlightvalleywiki.com/images/thumb/6/6b/Provincial_Window.png/129px-Provincial_Window.png",
         style: ["Bold","Rustic"],
-        furnitureCategory: ["Bedroom","Kitchen","Living Room"],
+        furnitureCategory: ["Bedroom","Kitchen","Living Room","Windows"],
         color: [],
         universe:"Tangled",
         added: 1.0
@@ -140,8 +154,9 @@ const tangledFurniture = [
     {
         furnitureName: `Pub Sign`,
         price: 2720,
+        images: "https://dreamlightvalleywiki.com/images/thumb/d/d5/Pub_Sign.png/180px-Pub_Sign.png",
         style: ["Cute","Quirky","Rustic"],
-        furnitureCategory: ["Bathroom","Kitchen","Rec Room"],
+        furnitureCategory: ["Bathroom","Kitchen","Rec Room","Decor"],
         color: [],
         universe:"Tangled",
         added: 1.0
@@ -149,8 +164,9 @@ const tangledFurniture = [
     {
         furnitureName: `Rustic Chandelier`,
         price: 2720,
+        images: "https://dreamlightvalleywiki.com/images/thumb/b/b2/Rustic_Chandelier.png/180px-Rustic_Chandelier.png",
         style: ["Bold","Rustic"],
-        furnitureCategory: ["Dining Room","Kitchen","Living Room"],
+        furnitureCategory: ["Dining Room","Kitchen","Living Room","Lighting"],
         color: [],
         universe:"Tangled",
         added: 1.0
@@ -158,8 +174,9 @@ const tangledFurniture = [
     {
         furnitureName: `Simple Stool`,
         price: 1800,
+        images: "https://dreamlightvalleywiki.com/images/thumb/e/e5/Simple_Stool.png/134px-Simple_Stool.png",
         style: ["Bold","Rustic"],
-        furnitureCategory: ["Dining Room","Living Room","Study"],
+        furnitureCategory: ["Dining Room","Living Room","Study","Seating"],
         color: [],
         universe:"Tangled",
         added: 1.0
@@ -167,12 +184,24 @@ const tangledFurniture = [
     {
         furnitureName: `Spray-Painted Window`,
         price: 4800,
+        images: "https://dreamlightvalleywiki.com/images/thumb/3/31/Spray-Painted_Window.png/121px-Spray-Painted_Window.png",
         style: ["Casual","Cute","Quirky"],
-        furnitureCategory: ["Rec Room","Study"],
+        furnitureCategory: ["Rec Room","Study","Windows"],
         color: [],
         universe:"Tangled",
         added: 1.0
-    }
+    },
+    // {
+    //     furnitureName: ``,
+    //     price: ,
+    //     images: "",
+    //     style: [],
+    //     furnitureCategory: [""],
+    //     color: [],
+    //     universe:"Tangled",
+    //     added: 1.3
+    // },
+
 ]
 
 module.exports=tangledFurniture
