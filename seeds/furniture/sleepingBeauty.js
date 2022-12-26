437d1cafc691b12ad4f23f4d79eac6846929205d
killingsworth.kristen <killingsworth_kristen@yahoo.com>
2022-12-25 21:00:01 -0500

    Images added to sleeping beauty furniture

--- seeds/furniture/sleepingBeauty.js
+++ seeds/furniture/sleepingBeauty.js
@@ -3,6 +3,7 @@ const sleepingBeautyFurniture = [
     {
         furnitureName: `Maleficent's Throne`,
         price: 450,
+        image: "https://dreamlightvalleywiki.com/images/thumb/c/ce/Maleficent%27s_Throne.png/142px-Maleficent%27s_Throne.png",
         obtainedBy: "Villain's Star Path",
         style: ["Bold","Elegant"],
         furnitureCategory: ["Living Room","Study"],
