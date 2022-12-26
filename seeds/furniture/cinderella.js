ff5c93c5206a4dab7d5ce14ac5d028e30e7d4e77
killingsworth.kristen <killingsworth_kristen@yahoo.com>
2022-12-25 20:50:13 -0500

    Cinderella images added to furniture

--- seeds/furniture/cinderella.js
+++ seeds/furniture/cinderella.js
@@ -5,6 +5,7 @@ const cinderllaFurniture = [
     {
         furnitureName: `Azure and Gold Arched Window`,
         // price: ,
+        image: "https://dreamlightvalleywiki.com/images/thumb/d/d1/Azure_and_Gold_Arched_Window.png/180px-Azure_and_Gold_Arched_Window.png",
         style: [],
         obtainedby: "Founder's Pack: Ultimate Edition",
         furnitureCategory: ["Windows"],
@@ -15,6 +16,7 @@ const cinderllaFurniture = [
     {
         furnitureName: `Ballroom Candelabra`,
         price: 5440,
+        image: "https://dreamlightvalleywiki.com/images/thumb/7/76/Ballroom_Candelabra.png/180px-Ballroom_Candelabra.png",
         style: ["Elegant","Traditional"],
         furnitureCategory: ["Study","Dining Room","Lighting"],
         color: [],
@@ -24,6 +26,7 @@ const cinderllaFurniture = [
     {
         furnitureName: `Bookcase`,
         // price: ,
+        image: "https://dreamlightvalleywiki.com/images/thumb/5/52/Bookcase.png/86px-Bookcase.png",
         style: ["Elegant"],
         furnitureCategory: ["Living Room","Study","Decor"],
         color: [],
@@ -33,6 +36,7 @@ const cinderllaFurniture = [
     {
         furnitureName: `Carpet`,
         price: 1500,
+        image: "https://dreamlightvalleywiki.com/images/thumb/5/54/Carpet.png/180px-Carpet.png",
         obtainedby: "Founder's Pack: Ultimate Edition",
         style: [],
         furnitureCategory: ["Rugs"],
@@ -43,6 +47,7 @@ const cinderllaFurniture = [
     {
         furnitureName: `Castle Painting`,
         price: 5440,
+        image: "https://dreamlightvalleywiki.com/images/thumb/d/dc/Castle_Painting.png/180px-Castle_Painting.png",
         obtainedby: "Founder's Pack: Ultimate Edition",
         style: [],
         furnitureCategory: ["Decor"],
@@ -53,6 +58,7 @@ const cinderllaFurniture = [
     {
         furnitureName: `Craft Desk`,
         price: 1000,
+        image: "https://dreamlightvalleywiki.com/images/thumb/c/c8/Craft_Desk.png/180px-Craft_Desk.png",
         style: ["Elegant","Professional","Traditional"],
         furnitureCategory: ["Study","Rec Room","Tables"],
         color: [],
@@ -62,6 +68,7 @@ const cinderllaFurniture = [
     {
         furnitureName: `Enchanting Nightstand`,
         price: 3520,
+        image: "https://dreamlightvalleywiki.com/images/thumb/5/5f/Enchanting_Nightstand.png/180px-Enchanting_Nightstand.png",
         style: ["Elegant","Traditional"],
         furnitureCategory: ["Bedroom","Tables"],
         color: [],
@@ -71,6 +78,7 @@ const cinderllaFurniture = [
     {
         furnitureName: `Fabric Shelf`,
         price: 6400,
+        image: "https://dreamlightvalleywiki.com/images/thumb/b/b9/Fabric_Shelf.png/100px-Fabric_Shelf.png",
         style: ["Elegant","Professional","Traditional"],
         furnitureCategory: ["Rec Room","Study"],
         color: [],
@@ -80,6 +88,7 @@ const cinderllaFurniture = [
     {
         furnitureName: `Glass Slipper Bed`,
         price: 23000,
+        image: "https://dreamlightvalleywiki.com/images/thumb/2/22/Glass_Slipper_Bed.png/180px-Glass_Slipper_Bed.png",
         style: ["Elegant","Traditional"],
         furnitureCategory: ["Bedroom","Beds"],
         color: [],
@@ -89,6 +98,7 @@ const cinderllaFurniture = [
     {
         furnitureName: `Golden Arched Window`,
         price: 6400,
+        image: "https://dreamlightvalleywiki.com/images/thumb/7/79/Golden_Arched_Window.png/180px-Golden_Arched_Window.png",
         obtainedby: "Founder's Pack: Ultimate Edition",
         style: [],
         furnitureCategory: ["Decor"],
@@ -99,6 +109,7 @@ const cinderllaFurniture = [
     {
         furnitureName: `Magnificent Chandelier`,
         price: 5440,
+        image: "https://dreamlightvalleywiki.com/images/thumb/d/d1/Magnificent_Chandelier.png/180px-Magnificent_Chandelier.png",
         obtainedby: "Founder's Pack: Ultimate Edition",
         style: [],
         furnitureCategory: ["Decor"],
@@ -109,6 +120,7 @@ const cinderllaFurniture = [
     {
         furnitureName: `Mannequin`,
         price: 4800,
+        image: "https://dreamlightvalleywiki.com/images/thumb/9/9a/Mannequin.png/180px-Mannequin.png",
         style: ["Elegant","Professional"],
         furnitureCategory: ["Rec Room","Study","Bedroom","Decor"],
         color: [],
@@ -118,6 +130,7 @@ const cinderllaFurniture = [
     {
         furnitureName: `Marble Coffee Table`,
         price: 3520,
+        image: "https://dreamlightvalleywiki.com/images/thumb/5/5b/Marble_Coffee_Table.png/180px-Marble_Coffee_Table.png",
         style: ["Elegant","Traditional"],
         furnitureCategory: ["Study","Living Room","Tables"],
         color: [],
@@ -127,6 +140,7 @@ const cinderllaFurniture = [
     {
         furnitureName: `Marble Column`,
         price: 3200,
+        image: "https://dreamlightvalleywiki.com/images/thumb/a/a6/Marble_Column.png/180px-Marble_Column.png",
         obtainedby: "Founder's Pack: Ultimate Edition",
         style: [],
         furnitureCategory: ["Decor"],
@@ -137,6 +151,7 @@ const cinderllaFurniture = [
     {
         furnitureName: `Marble Fireplace`,
         // price: ,
+        image: "https://dreamlightvalleywiki.com/images/thumb/1/12/Marble_Fireplace.png/180px-Marble_Fireplace.png",
         obtainedby: "Furniture Pouch",
         style: [],
         furnitureCategory: ["Decor"],
@@ -147,6 +162,7 @@ const cinderllaFurniture = [
     {
         furnitureName: `Marble Side Table`,
         price: 3520,
+        image: "https://dreamlightvalleywiki.com/images/thumb/5/53/Marble_Side_Table.png/180px-Marble_Side_Table.png",
         style: ["Elegant","Traditional"],
         furnitureCategory: ["Living Room","Study","Tables"],
         color: [],
@@ -157,6 +173,7 @@ const cinderllaFurniture = [
     {
         furnitureName: `Mice Vanity`,
         // price: ,
+        image: "https://dreamlightvalleywiki.com/images/thumb/3/38/Mice_Vanity.png/126px-Mice_Vanity.png",
         obtainedby: "Furniture Pouch",
         style: [],
         furnitureCategory: ["Tables"],
@@ -167,6 +184,7 @@ const cinderllaFurniture = [
     {
         furnitureName: `Mirror of Gilded Dreams`,
         price: 5440,
+        image: "https://dreamlightvalleywiki.com/images/thumb/6/65/Mirror_of_Gilded_Dreams.png/85px-Mirror_of_Gilded_Dreams.png",
         style: ["Elegant","Traditional"],
         furnitureCategory: ["Bathroom","Bedroom","Study","Decor"],
         color: [],
@@ -176,6 +194,7 @@ const cinderllaFurniture = [
     {
         furnitureName: `On-the-Dot Clock`,
         price: 16000,
+        image: "https://dreamlightvalleywiki.com/images/thumb/3/3c/On-the-Dot_Clock.png/180px-On-the-Dot_Clock.png",
         obtainedby: "Founder's Pack: Ultimate Edition",
         style: [],
         furnitureCategory: ["Decor"],
@@ -186,6 +205,7 @@ const cinderllaFurniture = [
     {
         furnitureName: `Painted Wardrobe`,
         price: 6400,
+        image: "https://dreamlightvalleywiki.com/images/thumb/8/80/Painted_Wardrobe.png/180px-Painted_Wardrobe.png",
         style: ["Elegant","Traditional"],
         furnitureCategory: ["Bedroom","Rec Room","Bathroom","Decor"],
         color: [],
@@ -195,6 +215,7 @@ const cinderllaFurniture = [
     {
         furnitureName: `Pearly Grand Piano`,
         price: 16000,
+        image: "https://dreamlightvalleywiki.com/images/thumb/8/86/Pearly_Grand_Piano.png/143px-Pearly_Grand_Piano.png",
         style: ["Elegant","Traditional"],
         furnitureCategory: ["Living Room","Rec Room","Study","Decor"],
         color: [],
@@ -204,6 +225,7 @@ const cinderllaFurniture = [
     {
         furnitureName: `Pearly Piano Bench`,
         price: 2400,
+        image: "https://dreamlightvalleywiki.com/images/thumb/8/8d/Pearly_Piano_Bench.png/180px-Pearly_Piano_Bench.png",
         style: ["Elegant","Traditional"],
         furnitureCategory: ["Living Room","Study","Seating"],
         color: [],
@@ -213,6 +235,7 @@ const cinderllaFurniture = [
     {
         furnitureName: `Pumpkin Lamp`,
         price: 2040,
+        image: "https://dreamlightvalleywiki.com/images/thumb/1/14/Pumpkin_Lamp.png/100px-Pumpkin_Lamp.png",
         style: ["Elegant","Traditional"],
         furnitureCategory: ["Bedroom","Living Room","Lighting"],
         color: [],
@@ -222,6 +245,7 @@ const cinderllaFurniture = [
     {
         furnitureName: `Pumpkin Ottoman`,
         price: 4800,
+        image: "https://dreamlightvalleywiki.com/images/6/69/Pumpkin_Ottoman.png",
         style: ["Elegant","Traditional"],
         furnitureCategory: ["Bedroom","Living Room","Seating"],
         color: [],
@@ -231,6 +255,7 @@ const cinderllaFurniture = [
     {
         furnitureName: `Regal Desk`,
         price: 8800,
+        image: "https://dreamlightvalleywiki.com/images/thumb/b/bd/Regal_Desk.png/180px-Regal_Desk.png",
         obtainedby: "Founders Pack: Ultimate Edition",
         style: [],
         furnitureCategory: ["Tables"],
@@ -241,6 +266,7 @@ const cinderllaFurniture = [
     {
         furnitureName: `Regal Side Table`,
         price: 7040,
+        image: "https://dreamlightvalleywiki.com/images/thumb/9/9c/Regal_Side_Table.png/180px-Regal_Side_Table.png",
         obtainedby: "Founders Pack: Ultimate Edition",
         style: [],
         furnitureCategory: ["Tables"],
@@ -251,6 +277,7 @@ const cinderllaFurniture = [
     {
         furnitureName: `Royal Banner`,
         price: 2720,
+        image: "https://dreamlightvalleywiki.com/images/thumb/b/b9/Royal_Banner.png/180px-Royal_Banner.png",
         obtainedby: "Founders Pack: Ultimate Edition",
         style: [],
         furnitureCategory: ["Decor"],
@@ -261,6 +288,7 @@ const cinderllaFurniture = [
     {
         furnitureName: `Royal Grand Piano`,
         price: 16000,
+        image: "https://dreamlightvalleywiki.com/images/thumb/3/35/Royal_Grand_Piano.png/180px-Royal_Grand_Piano.png",
         obtainedby: "Founders Pack: Ultimate Edition",
         style: [],
         furnitureCategory: ["Decor"],
@@ -271,6 +299,7 @@ const cinderllaFurniture = [
     {
         furnitureName: `Royal Piano Bench`,
         price: 2400,
+        image: "https://dreamlightvalleywiki.com/images/thumb/8/8e/Royal_Piano_Bench.png/180px-Royal_Piano_Bench.png",
         obtainedby: "Founders Pack: Ultimate Edition",
         style: [],
         furnitureCategory: ["Seating"],
@@ -281,6 +310,7 @@ const cinderllaFurniture = [
     {
         furnitureName: `Satin-Draped Window`,
         // price: ,
+        image: "https://dreamlightvalleywiki.com/images/thumb/6/63/Satin-Draped_Window.png/127px-Satin-Draped_Window.png",
         obtainedby: "Furniture Pouch",
         style: [],
         furnitureCategory: ["Windows"],
@@ -291,6 +321,8 @@ const cinderllaFurniture = [
     {
         furnitureName: `Sewing Machine`,
         price: 320,
+        image: "https://dreamlightvalleywiki.com/images/thumb/e/e0/Sewing_Machine.png/180px-Sewing_Machine.png",
+        image: "",
         style: ["Elegant","Traditional","Professional"],
         furnitureCategory: ["Rec Room","Study","Decor"],
         color: [],
@@ -300,6 +332,7 @@ const cinderllaFurniture = [
     {
         furnitureName: `Spool Seat`,
         price: 4800,
+        image: "https://dreamlightvalleywiki.com/images/thumb/5/55/Spool_Seat.png/150px-Spool_Seat.png",
         style: ["Elegant","Professional","Quirky"],
         furnitureCategory: ["Rec Room","Study","Seating"],
         color: [],
@@ -309,6 +342,7 @@ const cinderllaFurniture = [
     {
         furnitureName: `Throne`,
         price: 2400,
+        image: "https://dreamlightvalleywiki.com/images/thumb/3/35/Throne.png/180px-Throne.png",
         obtainedby: "Founders Pack: Ultimate Edition",
         style: [],
         furnitureCategory: ["Seating"],
@@ -319,6 +353,7 @@ const cinderllaFurniture = [
     {
         furnitureName: `Tufted Armchair`,
         price: 2400,
+        image: "https://dreamlightvalleywiki.com/images/thumb/a/a8/Tufted_Armchair.png/180px-Tufted_Armchair.png",
         style: ["Elegant","Traditional"],
         furnitureCategory: ["Living Room","Study","Seating"],
         color: [],
@@ -328,6 +363,7 @@ const cinderllaFurniture = [
     {
         furnitureName: `Tufted Chaise`,
         price: 3000,
+        image: "https://dreamlightvalleywiki.com/images/thumb/c/ce/Tufted_Chaise.png/180px-Tufted_Chaise.png",
         style: ["Elegant","Traditional"],
         furnitureCategory: ["Living Room","Study","Seating"],
         color: [],
@@ -337,6 +373,7 @@ const cinderllaFurniture = [
     {
         furnitureName: `Tufted Couch`,
         price: 3000,
+        image: "https://dreamlightvalleywiki.com/images/thumb/3/32/Tufted_Couch.png/180px-Tufted_Couch.png",
         style: ["Elegant","Traditional"],
         furnitureCategory: ["Living Room","Study","Seating"],
         color: [],
@@ -346,12 +383,22 @@ const cinderllaFurniture = [
     {
         furnitureName: `Velvet Carpet`,
         price: 4000,
+        image: "https://dreamlightvalleywiki.com/images/thumb/0/01/Velvet_Carpet.png/180px-Velvet_Carpet.png",
         style: ["Elegant","Traditional"],
         furnitureCategory: ["Rec Room","Study","Rugs"],
         color: [],
         universe: "Cinderella",
         added: 1.0
     },
+    // {
+    //     furnitureName: ``,
+    //     price: ,
+    //     image: "",
+    //     furnitureCategory: [""],
+    //     color: [],
+    //     universe: "Cinderella",
+    //     added: 1.3
+    // },
 ]
 
 module.exports=cinderllaFurniture
