7fbcbbc3dd376c70ef1490f0e1ed2371ad08935f
killingsworth.kristen <killingsworth_kristen@yahoo.com>
2022-12-25 20:42:16 -0500

    Images added to coco furniture

--- seeds/furniture/coco.js
+++ seeds/furniture/coco.js
@@ -3,6 +3,7 @@ const cocoFurniture = [
     {
         furnitureName: `Burro Piñata`,
         price: 480,
+        image: "https://dreamlightvalleywiki.com/images/thumb/3/33/Burro_Pi%C3%B1ata.png/180px-Burro_Pi%C3%B1ata.png",
         obtainedBy: "Pixar Fest Star Path",
         style: ["Cute","Modern"],
         furnitureCategory: ["Outdoors","Rec Room"],
@@ -13,6 +14,7 @@ const cocoFurniture = [
     {
         furnitureName: `Miguel's Guitar`,
         price: 480,
+        image: "https://dreamlightvalleywiki.com/images/thumb/7/76/Miguel%27s_Guitar.png/180px-Miguel%27s_Guitar.png",
         obtainedBy: "Pixar Fest Star Path",
         style: ["Bold","Traditional"],
         furnitureCategory: ["Outdoors","Rec Room","Study"],
@@ -23,6 +25,7 @@ const cocoFurniture = [
     {
         furnitureName: `Papel Picados`,
         price: 408,
+        image: "https://dreamlightvalleywiki.com/images/thumb/e/e7/Papel_Picados.png/180px-Papel_Picados.png",
         obtainedBy: "Pixar Fest Star Path",
         style: ["Cute","Traditional"],
         furnitureCategory: ["Outdoors"],
