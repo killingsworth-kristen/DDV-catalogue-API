7692a6398adfc2c3248dc8fd4a275d3c012561e3
Kristen Killingsworth <killingsworth_kristen@yahoo.com>
2023-01-24 05:56:28 -0500

    hercules images changed to paths

--- seeds/furniture/hercules.js
+++ seeds/furniture/hercules.js
@@ -1,16 +1,28 @@
+// 1 item as of 1/24/23
 
 const herculesFurniture = [
     {
         furnitureName: `Well of Souls`,
         price: 300,
-        image: "https://dreamlightvalleywiki.com/images/thumb/9/96/Well_of_Souls.png/180px-Well_of_Souls.png",
+        image: "/images/furniture/hercules/Well_of_Souls.png",
         obtainedBy: "Villain's Star Path",
         style: ["Quirky"],
         furnitureCategory: ["Outdoors","Study"],
         color: [],
         universe: "Hercules",
         added: 1.1
-    }
+    },
+    // {
+    //     furnitureName: ``,
+    //     price: ,
+    //     image: "/images/furniture/hercules/",
+    //     obtainedBy: "",
+    //     style: [""],
+    //     furnitureCategory: [""],
+    //     color: [],
+    //     universe: "Hercules",
+    //     added: 1.3
+    // }
 ]
 
 module.exports=herculesFurniture
